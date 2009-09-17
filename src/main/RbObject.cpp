29cf8d38ae1fe58ef413e03b6e572bda71d49b3b
Fredrik Ronquist <fredrik.ronquist@nrm.se>
2009-09-17 20:41:13 +0000

    Modified RbObject; added const qualifiers etc.

--- src/main/RbObject.cpp
+++ src/main/RbObject.cpp
@@ -1,30 +1,41 @@
+/**
+ * @file
+ * This file contains the partial implementation of RbObject,
+ * which is the REvBayes abstract base class for all objects.
+ *
+ * @brief Partial implementation of RbObject
+ *
+ * (c) Copyright 2009-
+ * @date Last modified: $Date: 2009-08-23 15:04:35 +0200 (Sön, 23 Aug 2009) $
+ * @author The REvBayes development core team
+ * @license GPL version 3
+ * @since Version 1.0, 2009-09-09
+ *
+ * $Id: RbDataType.h 9 2009-08-23 13:04:35Z ronquist $
+ */
 
+
+#include "RbObject.h"
 #include <string>
 #include <ostream>
-#include "RbObject.h"
 
 
-RbObject::RbObject(void) {
+const std::string& RbObject::getName() const {
 
-}  //!< default constructor
-
-RbObject::~RbObject() {
-
-}  //! Virtual destructor because of virtual functions
-
-
-std::string RbObject::getName(void) {
 	return name;
 }
 
-void RbObject::setName(std::string n) {
+void RbObject::print(std::ostream &c) const {
+
+	c<<toString();
+}
+
+void RbObject::setName(std::string& n) {
+
 	name = n;
 }
 
-std::string RbObject::toString(void){
+std::string RbObject::toString() const {
+
 	return name;
 }
-
-void RbObject::print(std::ostream &c) {
-	c<<toString();
-} //!< Print this object
--- src/main/RbObject.h
+++ src/main/RbObject.h
@@ -1,29 +1,49 @@
+/**
+ * @file
+ * This file contains the declaration of RbObject, which is
+ * the REvBayes abstrct base class for all objects.
+ *
+ * @brief Declaration of RbObject
+ *
+ * (c) Copyright 2009-
+ * @date Last modified: $Date$
+ * @author The REvBayes development core team
+ * @license GPL version 3
+ * @since Version 1.0, 2009-09-09
+ *
+ * $Id$
+ */
+
+
 #ifndef RbObject_H
 #define RbObject_H
 
 #include <string>
 #include <ostream>
-#include "../utils/RbDumpState.h"
+
+/* Forward declaration */
+class RbDumpState;
 
 class RbObject{
-public:
-	RbObject(void);  //!< default constructor
-    virtual ~RbObject();  //! Virtual destructor because of virtual functions
 
-    virtual RbObject* clone(void) = 0;    //!< create a cope of this object. Used to call the copy constructor of the derived class
+    public:
+        virtual ~RbObject() {}  //! Virtual destructor because of virtual functions
 
-    std::string         getName(void);              //!< Get name of object
-    void                setName(std::string n);     //!< Set name of object
-	virtual bool        operator==(RbObject o)=0;   //!< Comparison
-    virtual std::string toString(void);             //!< Information about object for debugging, printing etc
-    virtual void        print(std::ostream& c);     //!< Print this object for debugging, printing etc
-	virtual void        dump(std::ostream& c)=0;    //!< Dump to ostream c
-	virtual void        resurrect(const RbDumpState& x)=0;  //!< Resurrect from dumped state
+        virtual RbObject*   clone() const = 0;                      //!< Make copy
+	    virtual void        dump(std::ostream& c) const = 0;        //!< Dump to ostream c
+        const std::string&  getName() const;                        //!< Get name of object
+	    virtual bool        operator==(RbObject& o) const = 0;     //!< Comparison
+        virtual void        print(std::ostream& c) const;           //!< Print this object
+	    virtual void        resurrect(const RbDumpState& x) = 0;    //!< Resurrect from dumped state
+        void                setName(std::string& n);                //!< Set name of object
+        virtual std::string toString() const;                       //!< Information about object
 
-protected:
+    protected:
+	        RbObject() {}   //!< Make it impossible to create objects
 
-private:
-	std::string name;
+    private:
+	    std::string name;   //!< Name of object
 
 };
+
 #endif
