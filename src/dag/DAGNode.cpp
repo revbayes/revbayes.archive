d2bb8b176cf7ccdb002518e12c5c500692f3615d
Fredrik Ronquist <fredrik.ronquist@nrm.se>
2009-11-18 00:03:56 +0000

    Updated DAGNode. Does not compile yet.

--- src/dag/DAGNode.cpp
+++ src/dag/DAGNode.cpp
@@ -1,8 +1,7 @@
 /**
  * @file
  * This file contains the implementation of DAGNode, which is the base
- * class for nodes in the model DAG as well as for the nodes in a
- * syntax tree.
+ * class for nodes in the model DAG.
  *
  * @brief Implementation of DAGNode
  *
@@ -19,41 +18,62 @@
  */
 
 #include "DAGNode.h"
-#include "RbDataType.h"
+#include "RbObject.h"
+
 #include <iostream>
 
-using namespace std;
 
 /**
- * @brief DAGNode constructor
+ * @brief DAGNode default constructor
+ *
+ * This constructor creates an empty DAGNode.
+ *
+ */
+DAGNode::DAGNode()
+    : storedValue(NULL), value(NULL), changed(false), touched(false),
+      children(), parents() {
+}
+
+
+/**
+ * @brief DAGNode constructor from value
  *
  * This constructor creates a DAGNode with a variable attached to the node.
  *
- * @param dt      the variable of the dag node
+ * @param val   The value of the DAG node
+ *
  */
-DAGNode::DAGNode(RbDataType *dt)
-    : touched(true) {
-
-	value = dt;
-	storedValue = dt->copy();
+DAGNode::DAGNode(RbObject *val)
+    : storedValue(NULL), value(val), changed(false), touched(false),
+      children(), parents() {
 }
 
 /**
  * @brief DAGNode copy constructor
  *
- * This constructor creates a DAGNode as a clone of another DAGNode.
+ * This constructor creates a DAGNode as a copy of another
+ * DAGNode.
  *
- * @param d      the dag node to clone.
+ * @param d     The DAG node to clone.
+ *
  */
-DAGNode::DAGNode(DAGNode &d) {
+DAGNode::DAGNode(const DAGNode &d)
+    :   storedValue(d.storedValue.clone()), value(value.clone()),
+        changed(d.changed), touched(d.touched),
+        children(), parents() {
 
-	//TODO implement this
+    for (set<DAGNode*>::iterator i=d.children.begin(); i!=d.children.end(); i++)
+            children.insert((*i)->clone());
+
+    for (set<DAGNode*>::iterator i=parents.begin(); i!=parents.end(); i++)
+            parents.insert((*i)->clone());
 }
+
 
 /**
  * @brief DAGNode desctructor
  *
- * This is the standard destructor for any dag node.
+ * This is the standard destructor for all DAG nodes.
  *
  */
 DAGNode::~DAGNode(void) {
@@ -64,51 +84,161 @@ DAGNode::~DAGNode(void) {
 		delete storedValue;
 }
 
-//TODO not sure what this does
+
+/**
+ * @brief Clone a DAGNode
+ *
+ * This is the clone function for DAGNode objects.
+ *
+ */
+DAGNode* DAGNode::clone(void) const {
+
+	return (new DAGNode(*this));
+}
+
+
+/**
+ * @brief Tell affected DAG nodes to keep current value
+ *
+ * This function calls all affected DAG nodes so that they
+ * have a chance to keep the current value and discard the
+ * previous value.
+ *
+ */
 void DAGNode::keepAffected() {
 
     if (changed) {
-        for (set<DAGNode*>::iterator c=children.begin(); c!=children.end(); c++)
-            (*c)->keepAffected();
+        for (set<DAGNode*>::iterator i=children.begin(); i!=children.end(); i++)
+            (*i)->keepAffected();
         keep();
     }
 }
 
+
+/**
+ * @brief Compare DAG nodes
+ *
+ * To be the same, the DAG nodes must have the same parents and children,
+ * as well as the same value and storedValue. It is the pointers to these
+ * objects we need to compare, not their current values.
+ *
+ */
+bool DAGNode::operator==(const DAGNode& d) const {
+
+	if (value != d.value || storedValue != d.storedValue)
+        return false;
+
+    if (changed != d.changed || touched != d.touched)
+        return false;
+
+    if (children.size() != d.children.size() || parents.size() != d.parents.size())
+        return false;
+
+    for (set<DAGNode*>::iterator i=d.children.begin(); i!=d.children.end(); i++)
+        if (children.find(*i) == children.end())
+            return false;
+
+    for (set<DAGNode*>::iterator i=d.parents.begin(); i!=d.parents.end(); i++)
+        if (parents.find(*i) == parents.end())
+            return false;
+
+    return true;
+}
+
+
+/**
+ * @brief Print DAG node
+ *
+ * This function prints the content of the DAG node.
+ *
+ * @param o     The ostream for printing.
+ *
+ */
+void DAGNode::print(std::ostream& o) const {
+
+	o << "DAGNode: ";
+    //   o << getClass()->printValueVector(o);
+    o << std::endl;
+
+    o << "value:" << std::endl;
+    if (!value)
+        o << "null" << std::endl;
+    else
+        value->print(o);
+    
+    o << "storedValue:" << std::endl;
+    if (!storedValue)
+        o << "null" << std::endl;
+    else
+        storedValue->print(o);
+
+    printParents(o);
+    printChildren(o);
+}
+
+
+/**
+ * @brief Print children
+ *
+ * This function prints the children DAG nodes to an ostream with a header
+ * for each child.
+ *
+ * @param o     The ostream for printing.
+ *
+ */
 void DAGNode::printChildren(std::ostream& o) const {
 
 	if ( children.empty() )	
 		{
-		o << std::endl;
+		o << "No children" << std::endl;
 		return;
 		}
-	for (std::set<DAGNode*>::iterator c=children.begin(); c != children.end(); c++)
-		o << (*c) << " ";
+
+	for (int count=1, std::set<DAGNode*>::iterator i=children.begin(); i != children.end(); i++, count++) {
+		o << "children[" << count << "]:" << std::endl;
+        o << (*i) << std::endl;
+    }
 
 	o << std::endl;
 }
 
+
+/**
+ * @brief Print parents
+ *
+ * This function prints the parent DAG nodes to an ostream with a header
+ * for each parent.
+ *
+ * @param o     The ostream for printing.
+ *
+ */
 void DAGNode::printParents(std::ostream& o) const {
 
 	if ( parents.empty() )
 		{
-		o << std::endl;
+		o << "No parents" << std::endl;
 		return;
 		}
-	for (std::set<DAGNode*>::iterator p=parents.begin(); p != parents.end(); p++)
-		o << (*p) << " ";
+
+	for (int count=1, std::set<DAGNode*>::iterator i=parents.begin(); i != parents.end(); i++, count++) {
+		o << "parents[" << count << "]:" << std::endl;
+        o << (*i) << std::endl;
+    }
 
 	o << std::endl;
 }
 
+
 /**
  * @brief Restore the old value
  *
- * This function restores the old value of the dag node. This might be wanted during the MCMC if a value was proposed but rejected.
+ * This function restores the old value of the DAG node. It is typically called
+ * during MCMC sampling when a proposed value is rejected.
  *
  */
 void DAGNode::restore() {
 
-    RbDataType* temp;
+    RbObject* temp;
     
     if (changed) {
         temp        = value;
@@ -119,32 +249,35 @@ void DAGNode::restore() {
     keep();     // Sets touched and changed to false
 }
 
+
 /**
  * @brief Restore affected nodes.
  *
- * This function calls all nodes which are affected by this dag node and restores them.
+ * This function calls all nodes that are affected by this DAG node and restores
+ * them.
  *
  */
 void DAGNode::restoreAffected() {
 
     if (changed) {
-        for (set<DAGNode*>::iterator c=children.begin(); c!=children.end(); c++)
-            (*c)->restoreAffected();
+        for (set<DAGNode*>::iterator i=children.begin(); i!=children.end(); i++)
+            (*i)->restoreAffected();
         restore();
     }
 }
 
+
 /**
  * @brief Thouch affected nodes.
  *
- * This function touches all affected nodes, i.e. marks them as changed.
+ * This function touches all affected DAG nodes, i.e. marks them as changed.
  *
  */
 void DAGNode::touchAffected() {
 
     if (!touched) {
         touch();
-        for (set<DAGNode*>::iterator c=children.begin(); c!=children.end(); c++)
-            (*c)->touchAffected();
+        for (set<DAGNode*>::iterator i=children.begin(); i!=children.end(); i++)
+            (*i)->touchAffected();
     }
 }
--- src/dag/DAGNode.h
+++ src/dag/DAGNode.h
@@ -20,72 +20,82 @@
 #ifndef DAGNode_H
 #define DAGNode_H
 
-#include "../datatypes/RbDataType.h"
 #include "../main/RbObject.h"
 #include <string>
 #include <set>
 
-using namespace std;
 
-/*! This is the abstract base class for nodes in a model graph, a DAG. It is
- *  also the base class for the nodes in a syntax tree.
+/**
+ * @brief DAGNode, abstract base class for nodes in a model graph (DAG)
  *
- *  A DAGNode has the ability to restore itself to a previous state to facilitate
- *  MCMC sampling. A touched flag is used to mark a node for later recalculation.
- *  The node will then be recalculated when its getValue method is called. Note
- *  that only some nodes need to recalculate their value; others have a fixed value
- *  and the base class behavior of just returning the value unconditionally on
- *  a call to getValue is appropriate. When getValue is called, the old value
- *  should be kept in storedValue in all derived classes that have a value that
- *  can change.  A subsequent call to restore will restore the value of the node to
- *  its previous value; a call to keep will simply unmark the node by setting
- *  the touched flag to false.
+ * This is the abstract base class for nodes in a model graph, a DAG (Directed
+ * Acyclic Graph). DAGNode objects fall into three types, 
  *
- *  To facilitate operation on sections of DAGs, this class also implements a
- *  touchAffected function, which touches all affected children nodes until it reaches
- *  a node that says no additional children will be affected; this would typically
- *  be a stochastic node.
+ * A DAGNode has the ability to restore itself to a previous state to facilitate
+ * MCMC sampling. A touched flag is used to mark a node for later recalculation.
+ * The node will then be recalculated when its getValue method is called. When it
+ * is recalculated, the changed flag is set to true so that subsequent calls to
+ * get value knows that the work has been done.
  *
- *  Similarly, keep and restore have recursive versions that deal with all affected
- *  nodes in the graph (as determined by their touched flags).
+ * Note that only some nodes need to recalculate their value; others have a fixed
+ * value and the base class behavior of just returning the value unconditionally
+ * on a call to getValue is appropriate. When getValue is called, the old value
+ * should be kept in storedValue in all derived classes that have a value that
+ * can change.  A subsequent call to restore will restore the value of the node to
+ * its previous value; a call to keep will simply unmark the node by setting
+ * the touched flag to false.
+ *
+ * To facilitate operation on sections of DAGs, this class also implements a
+ * touchAffected function, which touches all affected children nodes until it
+ * reaches a node that says no additional children will be affected; this would
+ * typically be a stochastic node. Similarly, keep and restore have recursive
+ * versions that deal with all affected nodes in the graph (as determined by their
+ * touched flags). Normally, it is the recursive versions that should be called
+ * during an MCMC run.
+ *
+ * The language is structured such that all named objects will be folded into
+ * DAGNode objects. Syntax elements produced by the parser are also converted to
+ * unnamed DAGNode objects when representing dynamically evaluated expressions.
  */
 class DAGNode : public RbObject {
 
-public:
-	virtual ~DAGNode(void);
+    public:
+	        virtual ~DAGNode(void);     //!< Destructor
 
-	void                addChildNode(DAGNode* c) { children.insert(c); }    //!< Add child node
-	set<DAGNode*>&      getChildrenNodes(void) { return children; } //!< Get children nodes
-	set<DAGNode*>&      getParentNodes(void) { return parents; }    //!< Get parent nodes
-    virtual RbDataType* getValue() { return value; }            //!< Get value; base class just returns value
-	virtual bool        isChanged(void) { return changed; }     //!< Has the node recalculated its value?
-    bool                isTouched() { return touched; }         //!< Is the node marked for recalculation?
-    void                keep() { touched = changed = false; }   //!< Keep current value of node
-    void                keepAffected();                         //!< Keep value of affected nodes recursively
-	void                printChildren(std::ostream& c) const;   //!< Print children nodes
-	void                printParents(std::ostream& c) const;    //!< Print parent nodes
-	void                removeChildNode(DAGNode* c) { children.erase(c); }  //!< Remove a child node
-    void                restore();                              //!< Restore node to previous value
-    void                restoreAffected();                      //!< Restore value of affected nodes recursively
-    void                touch() { touched = true; }             //!< Mark node for recalculation
-    void                touchAffected();                        //!< Mark affected nodes recursively
+	    void                addChildNode(DAGNode* c) { children.insert(c); }    //!< Add child node
+        DAGNode*            clone(const DAGNode& d) const;                      //!< Clone this node
+	    set<DAGNode*>&      getChildrenNodes(void) { return children; }         //!< Get children nodes
+	    set<DAGNode*>&      getParentNodes(void) { return parents; }            //!< Get parent nodes
+        virtual RbObject*   getValue() = 0;                             //!< Get value
+	    virtual bool        isChanged(void) const { return changed; }   //!< Has the node recalculated its value?
+        bool                isTouched() const { return touched; }       //!< Is the node marked for recalculation?
+        void                keep() { touched = changed = false; }   //!< Keep current value of node
+        void                keepAffected();                         //!< Keep value of affected nodes recursively
+        bool                operator==(const DAGNode& d) const;     //!< Compare two DAG nodes
+        void                print(std::ostream& o) const;           //!< Print this DAG node
+	    void                printChildren(std::ostream& o) const;   //!< Print children DAG nodes
+	    void                printParents(std::ostream& o) const;    //!< Print parent DAG nodes
+	    void                removeChildNode(DAGNode* c) { children.erase(c); }  //!< Remove a child node
+        void                restore();                              //!< Restore node to previous value
+        void                restoreAffected();                      //!< Restore affected nodes recursively
+        void                touch() { touched = true; }             //!< Mark node for recalculation
+        void                touchAffected();                        //!< Mark affected nodes recursively
 	
+    protected:
+            // The constructors are protected because this is an abstract class.
+            // Only instances from derived classes are allowed.
+            DAGNode();                  //!< Default constructor
+            DAGNode(RbObject* val);     //!< Constructor from value
+            DAGNode(const DAGnode& d);  //!< Copy constructor
 
-protected:
-    // The constructors are protected because this is a abstract class. Only instances from derived classes are allowed!
-    DAGNode() : value(NULL), storedValue(NULL), changed(false), touched(false) {}   //!< Default constructor
-    DAGNode(RbDataType *dt);
-    //DAGNode(RbDataType *dt, std::set<DAGNode*> &p, std::set<DAGNode*> &c);
-    DAGNode(DAGNode &d);
+	    RbObject*       storedValue;    //!< Holds the previous value
+        RbObject*       value;          //!< Holds the current value
 
-	RbDataType*         storedValue;    //!< Holds the previous value
-    RbDataType*         value;          //!< Holds the current value
+	    bool            changed;        //!< True if value has been recalculated
+	    bool            touched;        //!< Marks node for recalculation
 
-	bool                changed;        //!< True if value has been recalculated
-	bool                touched;        //!< Marks node for recalculation
-
-	set<DAGNode*>       children;       //!< Set of children nodes
-	set<DAGNode*>       parents;        //!< Set of parent nodes
+	    set<DAGNode*>   children;       //!< Set of children nodes
+	    set<DAGNode*>   parents;        //!< Set of parent nodes
 };
 
 #endif
