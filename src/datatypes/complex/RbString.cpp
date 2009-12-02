1b9a92c085f3985a3b6eb5cdd302d3f0faea0d44
Fredrik Ronquist <fredrik.ronquist@nrm.se>
2009-12-02 12:48:39 +0000

    Implemented RbString (except dump/resurrect and assignment ops).

--- src/datatypes/complex/RbString.cpp
+++ src/datatypes/complex/RbString.cpp
@@ -1,43 +1,74 @@
-/*!
- * \file
+/**
+ * @file
  * This file contains the implementation of RbString, which is
  * a REvBayes wrapper around a regular string.
  *
- * \brief Implementation of RbString
+ * @brief Implementation of RbString
  *
  * (c) Copyright 2009-
- * \date Last modified: $Date$
- * \author The REvBayes development core team
- * \license GPL version 3
+ * @date Last modified: $Date$
+ * @author The REvBayes development core team
+ * @license GPL version 3
  *
  * $Id$
  */
 
 #include "RbString.h"
-#include <iostream>
 
-using namespace std;
 
-RbString::RbString(const string& v) 
-    : RbDataType("RbString"), value(v) {
+/** Constructor from string */
+RbString::RbString(const std::string& v) 
+    : RbComplex(), value(v) {
 }
 
+
+/** Copy constructor */
 RbString::RbString(const RbString& s) :
-    RbDataType(s.typeName), value(s.value) {
+    RbComplex(), value(s.value) {
 }
 
-RbString* RbString::copy(void) const {
 
-	RbString *x = new RbString( *this );
-	return x;
+/** Clone function */
+RbObject* RbString::clone() const {
+
+	return (RbObject*)(new RbString(*this));
 }
 
-void RbString::print(void) const {
 
-	cout << value << endl;
+/** Equals comparison */
+bool RbString::equals(const RbObject* obj) const {
+
+    const RbString* p = dynamic_cast<const RbString*>(obj);
+    if (p == NULL)
+        return false;
+
+	return (p->value == value);
 }
 
-void RbString::print(ostream &c) const {
 
-	c << value << endl;
+/** Print value */
+void RbString::printValue(std::ostream& o) const {
+
+	o << value << std::endl;
 }
+
+
+/** Complete info on object */
+std::string RbString::toString() const {
+
+	return "RbString; value = '" + value + "'";
+}
+
+
+/** Type conversion to string */
+RbString::operator std::string() const {
+
+    return value;
+}
+
+/** Add strings together */
+RbString RbString::operator+(const RbString& s) const {
+
+    return RbString(value+s.value);
+}
+
--- src/datatypes/complex/RbString.h
+++ src/datatypes/complex/RbString.h
@@ -1,14 +1,14 @@
-/*!
- * \file
+/**
+ * @file
  * This file contains the declaration of RbString, which is
  * a REvBayes wrapper around a regular string.
  *
- * \brief Declaration of RbString
+ * @brief Declaration of RbString
  *
  * (c) Copyright 2009-
- * \date Last modified: $Date$
- * \author The REvBayes development core team
- * \license GPL version 3
+ * @date Last modified: $Date$
+ * @author The REvBayes development core team
+ * @license GPL version 3
  *
  * $Id$
  */
@@ -18,32 +18,37 @@
 
 #include "RbComplex.h"
 
-class RbString : public RbComplex{
+#include <string>
 
-public:
+class RbDumpState;
 
+
+class RbString : public RbComplex {
+
+    public:
         static const StringVector   rbClass;            //!< Static class attribute
 
-	    RbString(const string& v);      //!< Constructor from string
-	    RbString(const RbString& s);    //!< Copy constructor
+	        RbString(const std::string& v);      //!< Constructor from string
+	        RbString(const RbString& s);    //!< Copy constructor
 
-	    RbObject*                  clone(void);           //!< Copy
-	    bool                       equals(const RbObject* obj) const;              //!< Equals comparison
-	    const StringVector&        getClass() const { return rbClass; }            //!< Get class
-	    void                       printValue(std::ostream& o) const;              //!< Print value (for user)
-	    std::string                toString(void) const;                           //!< General info on object
+        // Basic utility functions
+	    RbObject*               clone() const;                          //!< Copy
+	    bool                    equals(const RbObject* obj) const;      //!< Equals comparison
+	    const StringVector&     getClass() const { return rbClass; }    //!< Get class
+	    void                    printValue(std::ostream& o) const;      //!< Print value (for user)
+	    std::string             toString(void) const;                   //!< General info on object
 
 	    // overloaded operators
-	    RbObject&                  operator=(const RbObject& o);
-	    RbString&                  operator=(const RbString& o);
-	    RbString&                  operator+(const RbString& s) const;
-                                   operator(std::string)();
+	    RbObject&               operator=(const RbObject& o);
+	    RbString&               operator=(const RbString& o);
+	    RbString                operator+(const RbString& s) const;
+                                operator std::string() const;
 
-        void                       dump(std::ostream& c);              //!< Dump to ostream c
-        void                       resurrect(const RbDumpState& x);  //!< Resurrect from dumped state
+        void                    dump(std::ostream& c);                   //!< Dump to ostream c
+        void                    resurrect(const RbDumpState& x);         //!< Resurrect from dumped state
 
-private:
-	string  value;      //!< Value member
+    private:
+	    std::string             value;      //!< Value member
 };
 
 #endif
