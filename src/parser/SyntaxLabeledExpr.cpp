31b4e73970320d1fd7cb1ee1ffb0f6392d13d72e
Fredrik Ronquist <fredrik.ronquist@nrm.se>
2009-12-03 16:28:08 +0000

    Completed SyntaxLabeledExpr

--- src/parser/SyntaxLabeledExpr.cpp
+++ src/parser/SyntaxLabeledExpr.cpp
@@ -1,34 +1,114 @@
-/*!
- * \file
+/**
+ * @file
  * This file contains the implementation of SyntaxLabeledExpr, which is
- * used to hold labeled expressions in the syntax tree.
+ * used to hold labeled expressions in the syntax tree. Labeled exp-
+ * ressions are used in functions calls for arguments and in functon
+ * definitions for argument labels and default values (formal arguments).
  *
- * \brief Implementation of SyntaxLabeledExpr
+ * @brief Implementation of SyntaxLabeledExpr
  *
  * (c) Copyright 2009- under GPL version 3
- * \date Last modified: $Date$
- * \author Fredrik Ronquist and the REvBayes core team
- * \license GPL version 3
+ * @date Last modified: $Date$
+ * @author The RevBayes core development team
+ * @license GPL version 3
  *
  * $Id$
  */
 
-#include "RbString.h"
+#include "ConstantNode.h"
+#include "DAGNode.h"
+#include "DeterministicNode.h"
+#include "RbException.h"
+#include "RbObject.h"
 #include "SyntaxLabeledExpr.h"
+#include "Workspace.h"
+
 #include <sstream>
 
-using namespace std;
 
-SyntaxLabeledExpr::SyntaxLabeledExpr(const string& id, SyntaxElement* expr) :
-    SyntaxElement(), label(id), expression(expr) {
-
-    parents.insert(expression);
+/** Constructor from label and expression */
+SyntaxLabeledExpr::SyntaxLabeledExpr(RbString* id, SyntaxElement* expr)
+    : SyntaxElement(), label(id), expression(expr) {
 }
 
-/** Print some info about the labeled expression */
-void SyntaxLabeledExpr::print (ostream& c) const {
 
-    c << "SyntaxLabeledExpr: label = " << label << " --  value = ";
-    expression->print(c);
-    c << endl;
+/** Deep copy constructor */
+SyntaxLabeledExpr::SyntaxLabeledExpr(const SyntaxLabeledExpr& x)
+    : SyntaxElement(x) {
+
+    label      = new RbString(*(x.label));
+    expression = x.expression->clone();
 }
+
+
+/** Destructor deletes members */
+SyntaxLabeledExpr::~SyntaxLabeledExpr() {
+    
+    delete label;
+    delete expression;
+}
+
+
+/** Return brief info about object */
+std::string SyntaxLabeledExpr::briefInfo () const {
+
+    std::ostringstream   o;
+
+    if (expression == NULL)
+        o << "SyntaxLabeledExpr:  '" << std::string(*label) << "' = <empty>";
+    else if (expression->isConstExpr()) {
+        o << "SyntaxLabeledExpr:  '" << std::string(*label) << "' = ";
+        expression->getValue()->printValue(o);
+    }
+    else
+        o << "SyntaxLabeledExpr:  '" << std::string(*label) << "' = <value>";
+
+    return o.str();
+}
+
+
+/** Clone syntax element */
+SyntaxElement* SyntaxLabeledExpr::clone () const {
+
+    return (SyntaxElement*)(new SyntaxLabeledExpr(*this));
+}
+
+
+/** Equals comparison */
+bool SyntaxLabeledExpr::equals(const SyntaxElement* elem) const {
+
+	const SyntaxLabeledExpr* p = dynamic_cast<const SyntaxLabeledExpr*>(elem);
+    if (p == NULL)
+        return false;
+
+    bool result = true;
+    result = result && label->equals(p->label);
+    result = result && expression->equals(p->expression);
+
+    return result;
+}
+
+
+/** Convert element to DAG node (not applicable so return NULL) */
+DAGNode* SyntaxLabeledExpr::getDAGNode(Environment* env) const {
+
+    return NULL;
+}
+
+
+/** Get semantic value (not applicable so return NULL) */
+RbObject* SyntaxLabeledExpr::getValue(Environment* env) {
+
+    return NULL;
+}
+
+
+/** Print info about the syntax element */
+void SyntaxLabeledExpr::print(std::ostream& o) const {
+
+    o << "SyntaxLabeledExpr:" << std::endl;
+    o << "label      = " << label->toString() << std::endl;
+    o << "expression = " << expression->briefInfo() << std::endl;
+}
+
+
