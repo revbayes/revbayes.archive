4814e08356d242f9575b6eb541a81db55dbb20a7
Fredrik Ronquist <fredrik.ronquist@nrm.se>
2009-12-11 20:01:40 +0000

    Added for condition

--- src/parser/SyntaxForCondition.h
+++ src/parser/SyntaxForCondition.h
@@ -1,56 +1,50 @@
-/*!
- * \file
- * This file contains the declaration of SyntaxElement, which is
- * the base class used to hold elements (nodes) in the syntax tree.
+/**
+ * @file
+ * This file contains the declaration of SyntaxForCondition, which is
+ * used to hold expressions for the loop in a for statement.
  *
- * \brief Declaration of SyntaxElement
+ * @brief Declaration of SyntaxForCondition
  *
- * (c) Copyright 2009-
- * \date Last modified: $Date$
- * \author Fredrik Ronquist and the REvBayes core team
- * \license GPL version 3.0
+ * (c) Copyright 2009- under GPL version 3
+ * @date Last modified: $Date$
+ * @author The RevBayes core development team
+ * @license GPL version 3
  *
  * $Id$
  */
 
-#ifndef SyntaxElement_H
-#define SyntaxElement_H
+#ifndef SyntaxForCondition_H
+#define SyntaxForCondition_H
 
-#include "MbObject.h"
+#include "SyntaxElement.h"
 
-using namespace std;
+#include <iostream>
+#include <list>
 
-/*! This is the abstract base class for nodes in the syntax tree.
- *
- *  The syntax tree is built up by syntax elements. The syntax elements either
- *  have one or more operands, which are themselves syntax elements, or they
- *  have no operands and simply a predefined result vector of type RbObject. In
- *  the former case, the elements correspond to interior nodes in the syntax tree
- *  and in the latter case, they correspond to terminal nodes.
- *
- *  If you call getResult on an interior element and the result has not been filled in,
- *  the syntax element will be executed (causing recursive execution of the subtree
- *  rooted on that element) before the result is returned.
- *
- *  If you call getResult on a terminal element, the predefined result is simply returned.
- *  A syntax element also has the ability to restore itself to a previous state, to speed
- *  up accept and reject steps for deterministic nodes in a model DAG.
- */
-class SyntaxElement {
+class ArgumentRule;
+class RbString;
+
+class SyntaxForCondition : public SyntaxElement {
 
     public:
-            SyntaxElement();         //!< Default constructor
-	        ~SyntaxElement();        //!< Destructor; delete operands and result
+            SyntaxForCondition(RbString* identifier, SyntaxElement* inExpr);    //!< Standard constructor
+            SyntaxForCondition(const SyntaxForCondition& x);                    //!< Copy constructor
+	        virtual ~SyntaxForCondition();                                      //!< Destructor
 
-        virtual bool        check() const = 0;              //!< Check syntax
-        virtual RbObject*   getResult() = 0;                //!< Return result
-        virtual void        print(ostream &c) const = 0;    //!< Print content
-        virtual void        restore() { swap(); }           //!< Restore stored value (children not called in default implementation)
+        // Basic utility functions
+        std::string     briefInfo() const;                          //!< Brief info about object
+        SyntaxElement*  clone() const;                              //!< Clone object
+        bool            equals(const SyntaxElement* elem) const;    //!< Equals comparison
+        void            print(std::ostream& o) const;               //!< Print info about object
+
+        // Regular functions
+        DAGNode*        getDAGNode(Frame* frame=NULL) const;        //!< Convert to DAG node
+        RbObject*       getValue(Frame* frame=NULL) const;          //!< Get semantic value
 
     protected:
-        RbObject           *result;         //!< The result of executing the element; preset for terminal elements
-        RbObject           *storedResult;   //!< Stored result from previous execution of the element
-        void                swap() { RbObject *temp = result; result = storedResult; storedResult = temp; }  //!< Restore stored value
+        RbString*       varName;            //!< The name of the variable
+        SyntaxElement*  inExpression;       //!< The expression resulting in a vector of values
 };
 
 #endif
+
