96073c3d19aa3a755af8a2f0294e69b4e6ef4475
John Huelsenbeck <johnh@berkeley.edu>
2010-08-19 09:27:46 +0000

    Added VectorRealPos

---
+++ src/datatypes/complex/VectorRealPos.cpp
@@ -0,0 +1,11 @@
+/*
+ *  VectorRealPos.cpp
+ *  revbayes_xcode
+ *
+ *  Created by John Huelsenbeck on 8/19/10.
+ *  Copyright 2010 University of California, Berkeley. All rights reserved.
+ *
+ */
+
+#include "VectorRealPos.h"
+
