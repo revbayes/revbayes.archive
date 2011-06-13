5ac720a8c7cde8041db35df924b96fa2cd552c5a
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2011-06-13 10:14:50 +0000

    Actually deleting the tree files Fredrik mentioned.

--- src/core/datatypes/complex/TreeTimeConstrained.h
+++
@@ -1,9 +0,0 @@
-/*
- *  TreeTimeConstrained.h
- *  revbayes_xcode
- *
- *  Created by John Huelsenbeck on 8/24/10.
- *  Copyright 2010 University of California, Berkeley. All rights reserved.
- *
- */
-
