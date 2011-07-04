1bcd6d9eee576b9118572f1b21b442e2b1359141
John Huelsenbeck <johnh@berkeley.edu>
2011-07-04 19:57:27 +0000

    I now instantiate the workspace in the main.mm file for the GUI...and it now works

--- src/gui/main.mm
+++ src/gui/main.mm
@@ -1,8 +1,19 @@
+#include "Func_source.h"
+#include "Help.h"
+#include "RbSettings.h"
+#include "Workspace.h"
+#include "UserInterface.h"
+#include <string>
 #import <Cocoa/Cocoa.h>
 
 
 
 int main(int argc, char *argv[]) {
+
+    RbSettings::userSettings().initializeUserSettings();
+    Help::getHelp().initializeHelp("~/help/");
+    Workspace::globalWorkspace().initializeGlobalWorkspace();
+    Workspace::globalWorkspace().addFunction( "source", new Func_source() ); 
 
     return NSApplicationMain( argc, (const char **) argv );
 }
