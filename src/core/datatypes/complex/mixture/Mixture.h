47195c55ce1678f4617c54942f2a7090016958b2
Bastien Boussau <boussau@gmail.com>
2011-09-16 23:47:45 +0000

    Made some additions of a priori useful functions to Mixture.h.

--- src/core/datatypes/complex/mixture/Mixture.h
+++ src/core/datatypes/complex/mixture/Mixture.h
@@ -49,19 +49,23 @@ public:
     // Mixture functions
     size_t                          getNumberOfClasses();                                                   //!< Get the number of classes in the mixture
     void                            setNumberOfClasses(size_t numClasses);                                  //!< Set the number of classes in the mixture
+    void                            addClass();                                                             //!< Add a new class to the mixture
+    void                            removeClass();                                                          //!< Remove a class from the mixture
     Vector                          getVectorOfParameters();                                                //!< Get the vector of parameter values associated to the classes of the mixture
     void                            setVectorOfParameters(Vector parameters);                               //!< Set the vector of parameter values associated to the classes of the mixture
-    Vector                          getAllocationVector();                                                  //!< Get the allocation vector associating class indices to elements
+    void                            setParameterValue();                                                    //!< Set the value of a parameter associated to a particular class
+    VectorNatural                   getAllocationVector();                                                  //!< Get the allocation vector associating class indices to elements
     void                            setAllocationVector(Vector allocationVector);                           //!< Set the allocation vector associating class indices to elements
-
-    
+    void                            allocateElement (int elementId, int classId);                           //!< Change the class of a particular element
+    VectorRealPos                   getClassProbabilities();                                                //!< Get the vector containing class probabilities
+    void                            setClassProbabilities();                                                //!< Set the vector containing class probabilities
     
 private:
-    static const TypeSpec           typeSpec_;
-    VectorNatural                   allocationVector_;
-    Vector                          parameters_;
-    Vector                          numberOfElementsInClasses_;
-    
+    static const TypeSpec           typeSpec_;                                                              
+    VectorNatural                   allocationVector_;                                                      //!< Vector allocating elements to cluster indices
+    Vector                          parameters_;                                                            //!< Vector of size the number of classes and containing parameters associated to the classes
+    Vector                          numberOfElementsInClasses_;                                             //!< Vector giving the number of elements in each class
+    VectorRealPos                   classProbabilities_;                                                    //!< Vector giving class probabilities
 };
 
 #endif
