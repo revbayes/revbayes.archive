49ffa718cacca50871c6794595096cbdc738210d
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2011-11-01 14:38:28 +0000

    Fixed a memory leak. Now there are all gone and it is working fine. Even now cyclic smart pointers anymore :)

--- src/parser/parser/grammar.tab.h
+++ src/parser/parser/grammar.tab.h
@@ -124,7 +124,7 @@
 
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
 typedef union YYSTYPE
-#line 93 "./grammar.y"
+#line 92 "./grammar.y"
 {
     char*                                           string;
     double                                          realValue;
