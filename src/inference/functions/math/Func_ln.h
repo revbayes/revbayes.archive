6221bb75dc9eaadf41eef9611add4b090256de19
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2012-04-25 21:12:57 +0000

    Moving the ln function to the inference directory

---
+++ src/inference/functions/math/Func_ln.h
@@ -0,0 +1,56 @@
+/**
+ * @file
+ * This file contains the declaration of Func_ln, which 
+ * calculates the natural logarithm.
+ *
+ * @brief Declaration of Func_ln
+ *
+ * (c) Copyright 2009- under GPL version 3
+ * @date Last modified: $Date$
+ * @author The RevBayes Development Core Team
+ * @license GPL version 3
+ * @version 1.0
+ *
+ * $Id$
+ */
+
+#ifndef Func_ln_H
+#define Func_ln_H
+
+#include "RbFunction.h"
+#include "Real.h"
+
+#include <map>
+#include <string>
+#include <vector>
+
+class DAGNode;
+
+
+class Func_ln :  public RbFunction {
+    
+public:
+    // Basic utility functions
+    Func_ln*                    clone(void) const;                                          //!< Clone the object
+    static const std::string&   getClassName(void);                                         //!< Get class name
+    static const TypeSpec&      getClassTypeSpec(void);                                     //!< Get class type spec
+    const TypeSpec&             getTypeSpec(void) const;                                    //!< Get language type of the object
+    
+    // Regular functions
+    const ArgumentRules&        getArgumentRules(void) const;                               //!< Get argument rules
+    const TypeSpec&             getReturnType(void) const;                                  //!< Get type of return value
+
+protected:
+    const RbLanguageObject&     executeFunction(void);                                      //!< Execute function
+    void                        setArgumentVariable(const std::string& name, const Variable* var);
+
+private:
+    Real                        value;
+    
+    // Arguments
+    RbConstVariablePtr          x;
+    
+};
+
+#endif
+
