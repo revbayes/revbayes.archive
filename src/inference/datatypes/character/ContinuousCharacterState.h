c2669c6f1923a44d026f863d1b41a12d2af57544
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2012-05-24 10:15:37 +0000

    new directory

---
+++ src/inference/datatypes/character/ContinuousCharacterState.h
@@ -0,0 +1,57 @@
+/**
+ * @file
+ * This file contains the declaration of ContinuousCharacterState, which is
+ * the class for the continuous data types in RevBayes.
+ * 
+ * Currently, continuous characters can neither be gaps nor be missing or ambiguous.
+ *
+ * @brief Declaration of ContinuousCharacterState
+ *
+ * (c) Copyright 2009-
+ * @date Last modified: $Date: 2012-05-24 09:58:04 +0200 (Thu, 24 May 2012) $
+ * @author The RevBayes Development Core Team
+ * @license GPL version 3
+ *
+ * $Id: ContinuousCharacterState.h 1568 2012-05-24 07:58:04Z hoehna $
+ */
+
+#ifndef ContinuousCharacterState_H
+#define ContinuousCharacterState_H
+
+#include "Character.h"
+#include <ostream>
+#include <vector>
+
+
+
+class ContinuousCharacterState : public CharacterState {
+    
+public:
+    ContinuousCharacterState(void);                                                         //!< Default constructor
+    ContinuousCharacterState(const ContinuousCharacterState& s);                            //!< Copy constructor
+    ContinuousCharacterState(const double x);                                               //!< Constructor with mean value
+    ContinuousCharacterState(const double x, const double v);                               //!< Constructor with mean and variance values
+
+    bool                            operator==(const Character& x) const;                   //!< Equality
+    bool                            operator!=(const Character& x) const;                   //!< Inequality
+        
+    // character observation functions
+    double                          getMean(void) const;                                    //!< Get the continuous observation
+    size_t                          getNumberOfStates(void) const;                          //!< Get the number states for a character (return 0 in base class)
+//    double                          getValue(void) const;                                   //!< Get the continuous observation
+    std::string                     getStringValue(void) const;                             //!< Get a representation of the character as a string
+    double                          getVariance(void) const;                                //!< Get the continuous observation
+    bool                            isGapState(void) const;                                 //!< Get whether this is a gapped character state
+    bool                            isMissingOrAmbiguous(void) const;                       //!< Is the character missing or ambiguous
+    void                            setGapState(bool tf);                                   //!< Set whether this is a gapped character
+    void                            setMean(double x);                                      //!< Set the continuous observation
+    void                            setVariance(const double x);                            //!< Set the continuous observation
+    
+private:
+    double                          mean;                                                   //!< Mean value (or just the value)
+    double                          variance;                                               //!< Variance (most likely 0)
+    
+};
+
+#endif
+
---
+++ src/inference/datatypes/character/DiscreteCharacterState.h
@@ -0,0 +1,51 @@
+/**
+ * @file
+ * This file contains the declaration of DiscreteCharacterState, which is
+ * the abstract base class for character observations, such as DNA, RNA, 
+ * etc. data types in RevBayes.
+ *
+ * @brief Declaration of DiscreteCharacterState
+ *
+ * (c) Copyright 2009-
+ * @date Last modified: $Date: 2012-05-24 09:58:04 +0200 (Thu, 24 May 2012) $
+ * @author The RevBayes Development Core Team
+ * @license GPL version 3
+ *
+ * $Id: DiscreteCharacterState.h 1568 2012-05-24 07:58:04Z hoehna $
+ */
+
+#ifndef DiscreteCharacterState_H
+#define DiscreteCharacterState_H
+
+#include "Character.h"
+#include "RbVector.h"
+#include <ostream>
+#include <vector>
+
+
+class DiscreteCharacterState : public Character {
+    
+public:
+    virtual bool                            operator==(const Character& x) const = 0;           //!< Equality
+    virtual bool                            operator!=(const Character& x) const = 0;           //!< Inequality
+    virtual void                            operator++();                                       //!< Increment
+    virtual void                            operator--();                                       //!< Decrement
+ 
+    // Discrete character observation functions
+    virtual void                            addState(size_t pos) = 0;                           //!< Add a character state to the set of character states
+    virtual short                           getNumberObservedStates(void) const;                //!< How many states are observed for the character
+    virtual const std::string&              getStateLabels(void) const = 0;                     //!< Get valid state labels
+    virtual std::string                     getStringValue(void) const = 0;                     //!< Get a representation of the character as a string
+    virtual size_t                          getNumberOfStates(void) const = 0;                  //!< Get the number of discrete states for the character
+//    virtual char                            getState(void) const = 0;                           //!< Get the discrete observation
+    virtual bool                            isGapState(void) const = 0;                         //!< Get whether this is a gapped character state
+    virtual bool                            isMissingOrAmbiguous(void) const = 0;               //!< Is the character missing or ambiguous
+    virtual void                            setState(size_t pos, bool val) = 0;                 //!< Set the discrete observation
+    virtual void                            setIsGapState(bool tf) = 0;                         //!< Set whether this is a gapped character
+    
+protected:
+    DiscreteCharacterState();                                                                   //!< Constructor
+};
+
+#endif
+
