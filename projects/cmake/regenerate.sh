e320cb00f7c01613b1b27a32676d10580ccbf5bf
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2013-10-09 22:10:51 +0000

    updating compilable projects 3

--- projects/cmake/regenerate.sh
+++ projects/cmake/regenerate.sh
@@ -23,32 +23,29 @@ set(PROJECT_SOURCE_DIR ${CMAKE_SOURCE_DIR}/../../src)
 
 # TODO Split these up based on sub-package dependency
 include_directories(' > $HERE/CMakeLists.txt
-find core revlanguage test -type d | grep -v "svn" | sed 's|^|    ${PROJECT_SOURCE_DIR}/|g' >> $HERE/CMakeLists.txt
+find core revlanguage -type d | grep -v "svn" | sed 's|^|    ${PROJECT_SOURCE_DIR}/|g' >> $HERE/CMakeLists.txt
 echo ')
 
 add_subdirectory(core)   # Split into much smaller libraries
 add_subdirectory(revlanguage)
-add_subdirectory(test)
 
-add_executable(rb ${PROJECT_SOURCE_DIR}/main.cpp)
-target_link_libraries(rb rb-parser rb-core rb-test)
+add_executable(rb ${PROJECT_SOURCE_DIR}/revlanguage/main.cpp)
+target_link_libraries(rb rb-parser rb-core)
 ' >> $HERE/CMakeLists.txt
 
-
+if [ ! -d $HERE/core ]; then
+mkdir $HERE/core
+fi
 echo 'set(CORE_FILES' > $HERE/core/CMakeLists.txt
-find core | sed 's|^|${PROJECT_SOURCE_DIR}/|g' >> $HERE/core/CMakeLists.txt
+find core | grep -v "svn" | sed 's|^|${PROJECT_SOURCE_DIR}/|g' >> $HERE/core/CMakeLists.txt
 echo ')
 add_library(rb-core ${CORE_FILES})'  >> $HERE/core/CMakeLists.txt
 
-
+if [ ! -d $HERE/revlanguage ]; then
+mkdir $HERE/revlanguage
+fi
 echo 'set(PARSER_FILES' > $HERE/revlanguage/CMakeLists.txt
-find revlanguage | sed 's|^|${PROJECT_SOURCE_DIR}/|g' >> $HERE/revlanguage/CMakeLists.txt
+find revlanguage | grep -v "svn" | sed 's|^|${PROJECT_SOURCE_DIR}/|g' >> $HERE/revlanguage/CMakeLists.txt
 echo ')
 add_library(rb-parser ${PARSER_FILES})'  >> $HERE/revlanguage/CMakeLists.txt
-
-
-echo 'set(TEST_FILES' > $HERE/test/CMakeLists.txt
-find test | sed 's|^|${PROJECT_SOURCE_DIR}/|g' >> $HERE/test/CMakeLists.txt
-echo ')
-add_library(rb-test ${TEST_FILES})'  >> $HERE/test/CMakeLists.txt
 
