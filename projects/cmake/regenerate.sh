f69412e8f7545339bb7215bb5dea7b16fa70539d
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2013-12-13 11:45:31 +0100

    Updating the cmake regenerate.sh

--- projects/cmake/regenerate.sh
+++ projects/cmake/regenerate.sh
@@ -27,8 +27,8 @@ set(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/CMake ${CMAKE_MODULE_PATH})
 set(PROJECT_SOURCE_DIR ${CMAKE_SOURCE_DIR}/../../src)
 
 # TODO Split these up based on sub-package dependency
-include_directories(' > $HERE/CMakeLists.txt
-find core revlanguage -type d | grep -v "svn" | sed 's|^|    ${PROJECT_SOURCE_DIR}/|g' >> $HERE/CMakeLists.txt
+include_directories(' > "$HERE/CMakeLists.txt"
+find core revlanguage -type d | grep -v "svn" | sed 's|^|    ${PROJECT_SOURCE_DIR}/|g' >> "$HERE/CMakeLists.txt"
 echo ')
 
 add_subdirectory(core)   # Split into much smaller libraries
@@ -38,19 +38,19 @@ add_executable(rb ${PROJECT_SOURCE_DIR}/revlanguage/main.cpp)
 target_link_libraries(rb rb-parser rb-core)
 ' >> $HERE/CMakeLists.txt
 
-if [ ! -d $HERE/core ]; then
-mkdir $HERE/core
+if [ ! -d "$HERE/core" ]; then
+mkdir "$HERE/core"
 fi
-echo 'set(CORE_FILES' > $HERE/core/CMakeLists.txt
-find core | grep -v "svn" | sed 's|^|${PROJECT_SOURCE_DIR}/|g' >> $HERE/core/CMakeLists.txt
+echo 'set(CORE_FILES' > "$HERE/core/CMakeLists.txt"
+find core | grep -v "svn" | sed 's|^|${PROJECT_SOURCE_DIR}/|g' >> "$HERE/core/CMakeLists.txt"
 echo ')
-add_library(rb-core ${CORE_FILES})'  >> $HERE/core/CMakeLists.txt
+add_library(rb-core ${CORE_FILES})'  >> "$HERE/core/CMakeLists.txt"
 
-if [ ! -d $HERE/revlanguage ]; then
-mkdir $HERE/revlanguage
+if [ ! -d "$HERE/revlanguage" ]; then
+mkdir "$HERE/revlanguage"
 fi
-echo 'set(PARSER_FILES' > $HERE/revlanguage/CMakeLists.txt
-find revlanguage | grep -v "svn" | sed 's|^|${PROJECT_SOURCE_DIR}/|g' >> $HERE/revlanguage/CMakeLists.txt
+echo 'set(PARSER_FILES' > "$HERE/revlanguage/CMakeLists.txt"
+find revlanguage | grep -v "svn" | sed 's|^|${PROJECT_SOURCE_DIR}/|g' >> "$HERE/revlanguage/CMakeLists.txt"
 echo ')
-add_library(rb-parser ${PARSER_FILES})'  >> $HERE/revlanguage/CMakeLists.txt
+add_library(rb-parser ${PARSER_FILES})'  >> "$HERE/revlanguage/CMakeLists.txt"
 
