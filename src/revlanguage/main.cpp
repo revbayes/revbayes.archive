335511388a856c7ba92c70d549471880819c41fa
Johan Dunfalk <johan.dunfalk@nrm.se>
2014-03-20 13:58:45 +0100

    Boost integration:
    In a kind of a sideways approach, instead of integrating boost with the whole codebase, the only code that will use boost is located within the src/ui directory.
    This way, any developer who doesn't wish to deal with boost can simply ignore this location, excluding it from the project.
    
    The general recommendation if you use xcode is to exclude src/ui to keep out of trouble. Basic functionality will/can still be maintained in src/revlanguage in the extent that is needed for debugging etc..
    
    To build all command line tools, including the boost dependent ones, just go to projects/cmake directory and execute these commands:
    ./regenerate.sh
    cmake .
    make
    
    The boost libraries are now automatically compiled and linked, and the executables are buildt in the same directory.

--- src/revlanguage/main.cpp
+++ src/revlanguage/main.cpp
@@ -17,19 +17,6 @@
 #include "functions/RlFunction.h"
 #include "WorkspaceUtils.h"
 #include "CommandLineUtils.h"
-extern "C" {
-#include "../libs/linenoise/linenoise.h" //https://github.com/tadmarshall/linenoise
-}
-
-
-/* tab completion callback */
-// todo: list object members
-// todo: parameter values: make a guess if parameter value is a file... and display that somehow 
-
-void completion(const char *buf, linenoiseCompletions *lc) {
-    
-
-}
 
 int main(int argc, const char * argv[]) {
 
@@ -43,56 +30,16 @@ int main(int argc, const char * argv[]) {
     char *prompt = default_prompt;
     int result = 0;
     std::string commandLine;
-
-#if defined (USE_LIB_LINENOISE)
-
-    char *line;
-
-    /* Set the tab completion callback.*/
-    linenoiseSetCompletionCallback(completion);
-    
-    /* Load the history at startup */
-    linenoiseHistoryLoad("history.txt");
-
-    /* The call to linenoise() will block as long as the user types something
-     * and presses enter.
-     * The typed string is returned as a malloc() allocated string by
-     * linenoise, so the user needs to free() it. */
-
-
-
-    while ((line = linenoise(prompt)) != NULL) {
-
-        linenoiseHistoryAdd(line); /* Add to the history. */
-        linenoiseHistorySave("history.txt"); /* Save the history on disk. */
-
-        //printf("echo: '%s'\n", line);
-        //printf("\r\n  \x1b[1m%s\x1b[0m \x1b[90m%s\x1b[0m\r\n", "bold", "text");
-
-        if (result == 0 || result == 2) {
-            prompt = default_prompt;
-            commandLine = line;
-        } else if (result == 1) {
-            prompt = incomplete_prompt;
-            commandLine += line;
-        }
-
-        result = RevLanguage::Parser::getParser().processCommand(commandLine, &RevLanguage::Workspace::userWorkspace());
-
-        free(line);
-    }
-#else
-
     std::string line;
 
     for (;;) {
-        
-        std::cout << prompt;
-        std::istream& retStream = getline( std::cin, line );
 
-        if ( !retStream )
-            exit( 0 );
- 
+        std::cout << prompt;
+        std::istream& retStream = getline(std::cin, line);
+
+        if (!retStream)
+            exit(0);
+
         if (result == 0 || result == 2) {
             prompt = default_prompt;
             commandLine = line;
@@ -100,11 +47,9 @@ int main(int argc, const char * argv[]) {
             prompt = incomplete_prompt;
             commandLine += line;
         }
-        
+
         result = RevLanguage::Parser::getParser().processCommand(commandLine, &RevLanguage::Workspace::userWorkspace());
     }
-    
-#endif
 
     return 0;
 
