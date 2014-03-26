1767e38a433c8db7014b9fb6e07e896b3f978b73
Nicolas Lartillot <nicolas.lartillot@gmail.com>
2014-03-27 00:17:54 +0100

    streamlined code of BrownianPhyloProcess

--- src/core/datatypes/math/CovarianceMatrix.h
+++ src/core/datatypes/math/CovarianceMatrix.h
@@ -11,4 +11,46 @@
 
 #include <iostream>
 
+#include "MatrixReal.h"
+#include "EigenSystem.h"
+
+namespace RevBayesCore {
+    
+    class EigenSystem;
+    
+    class CovarianceMatrix : public MatrixReal {
+        
+    public:
+        CovarianceMatrix(size_t n);                                                             //!< Construct rate matrix with n states
+        CovarianceMatrix(const CovarianceMatrix& m);                                                  //!< Copy constructor
+        
+        virtual                            ~CovarianceMatrix(void);                                                                 //!< Destructor
+        
+        // overloaded operators
+        CovarianceMatrix&                  operator=(const CovarianceMatrix& r);
+        
+        
+        // public methods
+        bool                                isPositive() const {return true;}
+        
+        int                                 getDim() const {return 0;}
+        
+        double                              getCorrelationCoefficient(int i, int j) const;
+        
+        virtual CovarianceMatrix*           clone(void) const;
+        
+        void                                updateMatrix(void);
+        //!< Update the rate entries of the matrix (is needed if stationarity freqs or similar have changed)
+        
+    protected:
+        
+        
+    private:
+        EigenSystem                         eigensystem;
+
+    };
+    
+    
+}
+
 #endif /* defined(__revbayes__CovarianceMatrix__) */
