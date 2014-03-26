b9c861cafefcd0db44a6d7b9f8952ca694140354
Nicolas Lartillot <nicolas.lartillot@gmail.com>
2014-03-27 00:10:39 +0100

    created classes for multivariate brownian phylogenetic processes and inverse wishart dfistribution… still a lot to do

---
+++ src/core/datatypes/math/CovarianceMatrix.cpp
@@ -0,0 +1,9 @@
+//
+//  CovarianceMatrix.cpp
+//  revbayes
+//
+//  Created by Nicolas Lartillot on 2014-03-26.
+//  Copyright (c) 2014 revbayes team. All rights reserved.
+//
+
+#include "CovarianceMatrix.h"
---
+++ src/core/datatypes/math/CovarianceMatrix.h
@@ -0,0 +1,14 @@
+//
+//  CovarianceMatrix.h
+//  revbayes
+//
+//  Created by Nicolas Lartillot on 2014-03-26.
+//  Copyright (c) 2014 revbayes team. All rights reserved.
+//
+
+#ifndef __revbayes__CovarianceMatrix__
+#define __revbayes__CovarianceMatrix__
+
+#include <iostream>
+
+#endif /* defined(__revbayes__CovarianceMatrix__) */
