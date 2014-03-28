11b4b256d0ffa25598b5c7dbbee32ee5ec30e947
Nicolas Lartillot <nicolas.lartillot@gmail.com>
2014-03-28 11:28:18 +0100

    wish art again

---
+++ src/revlanguage/datatypes/inference/moves/RlPrecisionMatrixSimpleMove.cpp
@@ -0,0 +1,9 @@
+//
+//  RlPrecisionMatrixSimpleMove.cpp
+//  revbayes
+//
+//  Created by Nicolas Lartillot on 2014-03-28.
+//  Copyright (c) 2014 revbayes team. All rights reserved.
+//
+
+#include "RlPrecisionMatrixSimpleMove.h"
---
+++ src/revlanguage/datatypes/inference/moves/RlPrecisionMatrixSimpleMove.h
@@ -0,0 +1,14 @@
+//
+//  RlPrecisionMatrixSimpleMove.h
+//  revbayes
+//
+//  Created by Nicolas Lartillot on 2014-03-28.
+//  Copyright (c) 2014 revbayes team. All rights reserved.
+//
+
+#ifndef __revbayes__RlPrecisionMatrixSimpleMove__
+#define __revbayes__RlPrecisionMatrixSimpleMove__
+
+#include <iostream>
+
+#endif /* defined(__revbayes__RlPrecisionMatrixSimpleMove__) */
