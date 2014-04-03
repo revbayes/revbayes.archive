e34ccdbbdf62d13246ba7aad4b74c039ede92048
Michael Landis <mlandis@gmail.com>
2014-04-02 20:46:26 -0700

    Re-implementation of RateMap w/o deriving from RateMatrix. Retrieved Abstract/General/BiogeographicTreeHistoryCtmc.*

---
+++ src/core/datatypes/phylogenetics/ratemap/RateMap.h
@@ -0,0 +1,52 @@
+//
+//  RateMap.h
+//  rb_mlandis
+//
+//  Created by Michael Landis on 4/2/14.
+//  Copyright (c) 2014 Michael Landis. All rights reserved.
+//
+
+#ifndef __rb_mlandis__RateMap__
+#define __rb_mlandis__RateMap__
+
+#include "CharacterState.h"
+#include "Cloneable.h"
+#include <vector>
+
+namespace RevBayesCore {
+    
+    class RateMap : public Cloneable {
+        
+    public:
+        virtual                            ~RateMap(void);                                                                 //!< Destructor
+        
+        // public methods
+        size_t                              getNumberOfStates(void) const;                      //!< Return the number of states
+        size_t                              getNumberOfCharacters(void) const;                  //!< Return the number of characters
+
+        // pure virtual methods to overwrite
+        virtual RateMap*                    clone(void) const = 0;
+        virtual double                      getRate(CharacterState* from, CharacterState* to) const = 0;
+        virtual double                      getSumOfRates(CharacterState* from) const = 0;
+        virtual double                      getTransitionProbability(CharacterState* from, CharacterState* to, double t) const = 0;
+        virtual double                      getLnTransitionProbability(CharacterState* from, CharacterState* to, double t) const = 0;
+        
+    protected:
+        // prevent instantiation
+        RateMap(const RateMap& m);                                                              //!< Copy constructor
+        RateMap(size_t ns, size_t nc);                                                          //!< Construct rate matrix with n states
+        RateMap&                            operator=(const RateMap& r);
+        
+        // protected members available for derived classes
+        size_t                              numStates;                                          //!< The number of character states
+        size_t                              numCharacters;                                      //!< The number of characters
+        bool                                needsUpdate;
+        
+    };
+    
+    // Global functions using the class
+    std::ostream&                       operator<<(std::ostream& o, const RateMap& x);                                           //!< Overloaded output operator
+    
+}
+
+#endif /* defined(__rb_mlandis__RateMap__) */
---
+++ src/core/datatypes/phylogenetics/ratemap/RateMap_Biogeography.cpp
@@ -0,0 +1,81 @@
+//
+//  RateMap_Biogeography.cpp
+//  rb_mlandis
+//
+//  Created by Michael Landis on 4/2/14.
+//  Copyright (c) 2014 Michael Landis. All rights reserved.
+//
+
+#include "RateMap_Biogeography.h"
+
+using namespace RevBayesCore;
+
+RateMap_Biogeography::RateMap_Biogeography(size_t ns, size_t nc) : RateMap(ns, nc)
+{
+    
+}
+
+RateMap_Biogeography::RateMap_Biogeography(const RateMap_Biogeography& m) : RateMap( m )
+{
+    
+}
+
+RateMap_Biogeography::~RateMap_Biogeography(void)
+{
+    
+}
+
+RateMap_Biogeography& RateMap_Biogeography::operator=(const RateMap_Biogeography& r)
+{
+    if (this != &r)
+    {
+        RateMap::operator=( r );
+    }
+    return *this;
+}
+
+RateMap_Biogeography* RateMap_Biogeography::clone(void) const
+{
+    return new RateMap_Biogeography( *this );
+}
+
+double RateMap_Biogeography::getRate(CharacterState* from, CharacterState* to) const
+{
+    return 1.0;
+}
+
+double RateMap_Biogeography::getSumOfRates(CharacterState* from) const
+{
+    return 1.0;
+}
+
+double RateMap_Biogeography::getTransitionProbability(CharacterState* from, CharacterState* to, double t) const
+{
+    return 1.0;
+}
+
+double RateMap_Biogeography::getLnTransitionProbability(CharacterState* from, CharacterState* to, double t) const
+{
+    return 0.0;
+}
+
+
+const std::vector<double>& RateMap_Biogeography::getGainLossRates(void) const
+{
+    return gainLossRates;
+}
+
+void RateMap_Biogeography::setGainLossRates(const std::vector<double> &r)
+{
+    gainLossRates = r;
+}
+
+double RateMap_Biogeography::getDistancePower(void) const
+{
+    return distancePower;
+}
+
+void RateMap_Biogeography::setDistancePower(double d)
+{
+    distancePower = d;
+}
