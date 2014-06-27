b6591aadccfd2e40c59ab032737c3825634a6a03
Fredrik Ronquist <fredrik.ronquist@nrm.se>
2014-06-27 02:13:50 -0700

    Made function class names in functions/inference/convergence conformant to naming standards

--- src/revlanguage/functions/inference/convergence/Func_beca.h
+++ src/revlanguage/functions/inference/convergence/Func_beca.h
@@ -1,6 +1,6 @@
 /**
  * @file
- * This file contains the declaration of BurninEstimationConvergenceAssessmentFunction, which is
+ * This file contains the declaration of Func_beca, which is
  * the function used estimate the optimal burnin on the criteria:
  *    ESS: the burnin is choosen so that the ESS is maximized
  *    SEM: the burnin is choosen so that the SEM is minimized
@@ -8,34 +8,27 @@
  *    Geweke-statistic: compares a first part of the chain to a second part
  *    
  *
- * @brief Declaration of OptimalBurninFunction
+ * @brief Declaration of Func_beca
  *
  * (c) Copyright 2009- under GPL version 3
- * @date Last modified: $Date: 2012-04-22 12:06:58 -0700 (Sun, 22 Apr 2012) $
  * @author The RevBayes Development Core Team
  * @license GPL version 3
- * @version 1.0
- * @interface RbFunction
- * @package functions
- * @since Version 1.0, 2013-04-04
- *
- * $Id: Func_source.h 1431 2012-04-22 19:06:58Z hoehna $
  */
 
-#ifndef BurninEstimationConvergenceAssessmentFunction_H
-#define BurninEstimationConvergenceAssessmentFunction_H
+#ifndef Func_beca_H
+#define Func_beca_H
 
 #include "RlFunction.h"
 
 namespace RevLanguage {
     
-    class BurninEstimationConvergenceAssessmentFunction : public Function {
+    class Func_beca : public Function {
         
     public:
-        BurninEstimationConvergenceAssessmentFunction( void );
+        Func_beca( void );
         
         // Basic utility functions
-        BurninEstimationConvergenceAssessmentFunction*      clone(void) const;                                                          //!< Clone object
+        Func_beca*                                          clone(void) const;                                                          //!< Clone object
         static const std::string&                           getClassName(void);                                                         //!< Get class name
         static const TypeSpec&                              getClassTypeSpec(void);                                                     //!< Get class type spec
         const TypeSpec&                                     getTypeSpec(void) const;                                                    //!< Get language type of the object
@@ -43,9 +36,9 @@ namespace RevLanguage {
         // Func_source functions
         const ArgumentRules&                                getArgumentRules(void) const;                                               //!< Get argument rules
         const TypeSpec&                                     getReturnType(void) const;                                                  //!< Get type of return val
-        bool                                                throws(void) const { return false; }                                         //!< Function may throw exceptions
+        bool                                                throws(void) const { return false; }                                        //!< Function may throw exceptions
         
-        RevObject*                                   execute(void);                                                              //!< Execute function
+        RevObject*                                          execute(void);                                                              //!< Execute function
         
     };
     
--- src/revlanguage/functions/inference/convergence/Func_estimateBurnin.h
+++ src/revlanguage/functions/inference/convergence/Func_estimateBurnin.h
@@ -1,38 +1,31 @@
 /**
  * @file
- * This file contains the declaration of OptimalBurninFunction, which is
+ * This file contains the declaration of Func_estimateBurnin, which is
  * the function used estimate the optimal burnin on the criteria:
  *    ESS: the burnin is choosen so that the ESS is maximized
  *    SEM: the burnin is choosen so that the SEM is minimized
  *
- * @brief Declaration of OptimalBurninFunction
+ * @brief Declaration of Func_estimateBurnin
  *
  * (c) Copyright 2009- under GPL version 3
- * @date Last modified: $Date: 2012-04-22 12:06:58 -0700 (Sun, 22 Apr 2012) $
  * @author The RevBayes Development Core Team
  * @license GPL version 3
- * @version 1.0
- * @interface RbFunction
- * @package functions
- * @since Version 1.0, 2013-04-04
- *
- * $Id: Func_source.h 1431 2012-04-22 19:06:58Z hoehna $
  */
 
-#ifndef OptimalBurninFunction_H
-#define OptimalBurninFunction_H
+#ifndef Func_estimateBurnin_H
+#define Func_estimateBurnin_H
 
 #include "RlFunction.h"
 
 namespace RevLanguage {
     
-    class OptimalBurninFunction : public Function {
+    class Func_estimateBurnin : public Function {
         
     public:
-        OptimalBurninFunction( void );
+        Func_estimateBurnin( void );
         
         // Basic utility functions
-        OptimalBurninFunction*                  clone(void) const;                                                          //!< Clone object
+        Func_estimateBurnin*                    clone(void) const;                                                          //!< Clone object
         static const std::string&               getClassName(void);                                                         //!< Get class name
         static const TypeSpec&                  getClassTypeSpec(void);                                                     //!< Get class type spec
         const TypeSpec&                         getTypeSpec(void) const;                                                    //!< Get language type of the object
@@ -40,7 +33,7 @@ namespace RevLanguage {
         // Func_source functions
         const ArgumentRules&                    getArgumentRules(void) const;                                               //!< Get argument rules
         const TypeSpec&                         getReturnType(void) const;                                                  //!< Get type of return val
-        bool                                    throws(void) const { return false; }                                         //!< Function may throw exceptions
+        bool                                    throws(void) const { return false; }                                        //!< Function may throw exceptions
         
         RevObject*                              execute(void);                                                              //!< Execute function
         
