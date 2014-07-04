d9593e61c978883a9d3475017738b264526517c1
Laurent Gueguen <laurent.gueguen@univ-lyon1.fr>
2014-07-04 16:08:49 +0200

    size_t -> std::size_t

--- src/core/analysis/mcmc/convergence/EssMax.h
+++ src/core/analysis/mcmc/convergence/EssMax.h
@@ -10,14 +10,14 @@ namespace RevBayesCore {
     class EssMax : public BurninEstimatorContinuous {
     
     public:
-        EssMax(size_t b=10, double f=0.5);
+      EssMax(std::size_t b=10, double f=0.5);
     
         // implementen functions from convergence diagnostic
-        size_t      estimateBurnin(const std::vector<double>& values);
+      std::size_t      estimateBurnin(const std::vector<double>& values);
     
     private:
     
-        size_t      blockSize;                                                                                          //!< first window
+      std::size_t      blockSize;                                                                                          //!< first window
         double      frac;
     
     };
