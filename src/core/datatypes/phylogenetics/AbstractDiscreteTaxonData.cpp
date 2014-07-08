5d86bbc5d1835429621528ccac375ae415ee6f2a
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2014-07-08 15:19:12 +0200

    Removing empty files.

--- src/core/datatypes/phylogenetics/AbstractDiscreteTaxonData.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  AbstractDiscreteTaxonData.cpp
-//  RevBayes
-//
-//  Created by Sebastian Hoehna on 2/16/14.
-//  Copyright 2014 __MyCompanyName__. All rights reserved.
-//
-
-#include "AbstractDiscreteTaxonData.h"
