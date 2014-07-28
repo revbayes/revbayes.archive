1381799ffeef97958408094584f78a6a8dc9cba9
Nicolas Lartillot <nicolas.lartillot@gmail.com>
2014-07-28 17:38:21 +0200

    alternative constructors for inverse Wishart distribution

--- src/core/math/distributions/DistributionInverseWishart.h
+++ src/core/math/distributions/DistributionInverseWishart.h
@@ -27,12 +27,17 @@ namespace RevBayesCore {
             // by definition: Z = \sum_i X_i X_i', where X_i ~ Normal(0, covariance = sigma0)
             double                      pdf(const MatrixRealSymmetric& sigma0, size_t df, const MatrixRealSymmetric& z);        /*!< Dirichlet(a[]) probability density */
             double                      lnPdf(const MatrixRealSymmetric& sigma0, size_t df, const MatrixRealSymmetric& z);      /*!< Dirichlet(a[]) log_e probability density */
-            MatrixRealSymmetric             rv(const MatrixRealSymmetric& sigma0, size_t df, RandomNumberGenerator& rng);           /*!< Dirichlet(a[]) random variable */
+            MatrixRealSymmetric         rv(const MatrixRealSymmetric& sigma0, size_t df, RandomNumberGenerator& rng);           /*!< Dirichlet(a[]) random variable */
+
+            // InverseWishart of parameters sigma0 = Diagonal(kappa) and df degrees of freedom
+            double                      pdf(const std::vector<double>& kappa, size_t df, const MatrixRealSymmetric& z);        /*!< Dirichlet(a[]) probability density */
+            double                      lnPdf(const std::vector<double>& kappa, size_t df, const MatrixRealSymmetric& z);      /*!< Dirichlet(a[]) log_e probability density */
+            MatrixRealSymmetric         rv(const std::vector<double>& kappa, size_t df, RandomNumberGenerator& rng);           /*!< Dirichlet(a[]) random variable */
 
             // InverseWishart of parameters sigma0 = kappa * I and df degrees of freedom
             double                      pdf(double kappa, size_t df, const MatrixRealSymmetric& z);        /*!< Dirichlet(a[]) probability density */
             double                      lnPdf(double kappa, size_t df, const MatrixRealSymmetric& z);      /*!< Dirichlet(a[]) log_e probability density */
-            MatrixRealSymmetric             rv(double kappa, size_t dim, size_t df, RandomNumberGenerator& rng);           /*!< Dirichlet(a[]) random variable */
+            MatrixRealSymmetric         rv(double kappa, size_t dim, size_t df, RandomNumberGenerator& rng);           /*!< Dirichlet(a[]) random variable */
 
         }
     }
