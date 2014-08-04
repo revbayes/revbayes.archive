24181ae068b292f897d97ccbb7c21575e9b06739
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2014-08-04 13:47:41 +0200

    Nicer output on the screen during MCMC runs.

--- src/core/monitors/ScreenMonitor.cpp
+++ src/core/monitors/ScreenMonitor.cpp
@@ -20,36 +20,47 @@
 #include "DagNode.h"
 #include "Model.h"
 #include "Monitor.h"
+#include "StringUtilities.h"
+
+#include <sstream>
 
 using namespace RevBayesCore;
 
 /* Constructor */
-ScreenMonitor::ScreenMonitor(DagNode *n, int g, const std::string &del, bool pp, bool l, bool pr) : Monitor(g,n), separator( del ), posterior( pp ), prior( pr ), likelihood( l ) {
+ScreenMonitor::ScreenMonitor(DagNode *n, int g, bool pp, bool l, bool pr) : Monitor(g,n),
+    posterior( pp ),
+    prior( pr ),
+    likelihood( l ),
+    separator("   |   ")
+{
     
 }
 
 
 /* Constructor */
-ScreenMonitor::ScreenMonitor(const std::set<DagNode *> &n, int g, const std::string &del, bool pp, bool l, bool pr) : Monitor(g,n), separator( del ), posterior( pp ), prior( pr ), likelihood( l ) {
+ScreenMonitor::ScreenMonitor(const std::set<DagNode *> &n, int g, bool pp, bool l, bool pr) : Monitor(g,n),
+    posterior( pp ),
+    prior( pr ),
+    likelihood( l ),
+    separator("   |   ")
+{
     
 }
 
 
-ScreenMonitor::ScreenMonitor(const std::vector<DagNode *> &n, int g, const std::string &del, bool pp, bool l, bool pr) : Monitor(g,n), separator( del ), posterior( pp ), prior( pr ), likelihood( l ) {
+ScreenMonitor::ScreenMonitor(const std::vector<DagNode *> &n, int g, bool pp, bool l, bool pr) : Monitor(g,n),
+    posterior( pp ),
+    prior( pr ),
+    likelihood( l ),
+    separator("   |   ")
+{
     
-}
-
-ScreenMonitor::ScreenMonitor(const ScreenMonitor &f) : Monitor( f ) {
-    
-    separator   = f.separator;
-    prior       = f.prior;
-    posterior   = f.posterior;
-    likelihood  = f.likelihood;
 }
 
 
 /* Clone the object */
-ScreenMonitor* ScreenMonitor::clone(void) const {
+ScreenMonitor* ScreenMonitor::clone(void) const
+{
     
     return new ScreenMonitor(*this);
 }
@@ -57,56 +68,82 @@ ScreenMonitor* ScreenMonitor::clone(void) const {
 
 
 /** Monitor value at generation gen */
-void ScreenMonitor::monitor(unsigned long gen) {
+void ScreenMonitor::monitor(unsigned long gen)
+{
     
     // get the printing frequency
     unsigned long samplingFrequency = printgen;
     
     if (gen % samplingFrequency == 0) {
         // print the iteration number first
-        std::cout << gen;
+        std::stringstream ss;
+        ss << gen;
+        std::string s = ss.str();
+        StringUtilities::fillWithSpaces(s, 10, true);
+        std::cout << s;
         
-        if ( posterior ) {
+        if ( posterior )
+        {
             // add a separator before every new element
             std::cout << separator;
             
             const std::vector<DagNode*> &n = model->getDagNodes();
             double pp = 0.0;
-            for (std::vector<DagNode*>::const_iterator it = n.begin(); it != n.end(); ++it) {
+            for (std::vector<DagNode*>::const_iterator it = n.begin(); it != n.end(); ++it)
+            {
                 pp += (*it)->getLnProbability();
             }
-            std::cout << pp;
+            std::stringstream ss;
+            ss << pp;
+            std::string s = ss.str();
+            StringUtilities::fillWithSpaces(s, 12, false);
+            std::cout << s;
         }
         
-        if ( likelihood ) {
+        if ( likelihood )
+        {
             // add a separator before every new element
             std::cout << separator;
             
             const std::vector<DagNode*> &n = model->getDagNodes();
             double pp = 0.0;
-            for (std::vector<DagNode*>::const_iterator it = n.begin(); it != n.end(); ++it) {
-                if ( (*it)->isClamped() ) {
+            for (std::vector<DagNode*>::const_iterator it = n.begin(); it != n.end(); ++it)
+            {
+                if ( (*it)->isClamped() )
+                {
                     pp += (*it)->getLnProbability();
                 }
             }
-            std::cout << pp;
+            std::stringstream ss;
+            ss << pp;
+            std::string s = ss.str();
+            StringUtilities::fillWithSpaces(s, 12, false);
+            std::cout << s;
         }
         
-        if ( prior ) {
+        if ( prior )
+        {
             // add a separator before every new element
             std::cout << separator;
             
             const std::vector<DagNode*> &n = model->getDagNodes();
             double pp = 0.0;
-            for (std::vector<DagNode*>::const_iterator it = n.begin(); it != n.end(); ++it) {
-                if ( !(*it)->isClamped() ) {
+            for (std::vector<DagNode*>::const_iterator it = n.begin(); it != n.end(); ++it)
+            {
+                if ( !(*it)->isClamped() )
+                {
                     pp += (*it)->getLnProbability();
                 }
             }
-            std::cout << pp;
+            std::stringstream ss;
+            ss << pp;
+            std::string s = ss.str();
+            StringUtilities::fillWithSpaces(s, 12, false);
+            std::cout << s;
         }
         
-        for (std::vector<DagNode*>::iterator i = nodes.begin(); i != nodes.end(); ++i) {
+        for (std::vector<DagNode*>::iterator i = nodes.begin(); i != nodes.end(); ++i)
+        {
             // add a separator before every new element
             std::cout << separator;
             
@@ -114,7 +151,7 @@ void ScreenMonitor::monitor(unsigned long gen) {
             DagNode *node = *i;
             
             // print the value
-            node->printValue(std::cout, separator);
+            node->printValue(std::cout, separator, 12, false);
         }
         
         std::cout << std::endl;
@@ -132,27 +169,35 @@ void ScreenMonitor::printHeader()
     std::cout << std::endl;
     
     // print one column for the iteration number
-    std::cout << "Iteration";
+    std::string itString = "Iteration";
+    StringUtilities::fillWithSpaces(itString,10,true);
+    std::cout << itString;
     
     if ( posterior ) 
     {
         // add a separator before every new element
         std::cout << separator;
-        std::cout << "Posterior";
+        std::string poString = "Posterior";
+        StringUtilities::fillWithSpaces(poString,12,false);
+        std::cout << poString;
     }
     
     if ( likelihood ) 
     {
         // add a separator before every new element
         std::cout << separator;
-        std::cout << "Likelihood";
+        std::string liString = "Likelihood";
+        StringUtilities::fillWithSpaces(liString,12,false);
+        std::cout << liString;
     }
     
     if ( prior ) 
     {
         // add a separator before every new element
         std::cout << separator;
-        std::cout << "Prior";
+        std::string prString = "Prior";
+        StringUtilities::fillWithSpaces(prString,12,false);
+        std::cout << prString;
     }
     
     for (std::vector<DagNode *>::const_iterator it=nodes.begin(); it!=nodes.end(); it++) 
@@ -165,11 +210,13 @@ void ScreenMonitor::printHeader()
         // print the header
         if (theNode->getName() != "")
         {
-            theNode->printName(std::cout,separator);
+            theNode->printName(std::cout,separator,12,false);
         }
         else
         {
-            std::cout << "Unnamed";
+            std::string nString = "Unnamed";
+            StringUtilities::fillWithSpaces(nString,12,false);
+            std::cout << nString;
         }
     }
     
