d55c68518aeea15b2b6b90762a8137e9dd148890
Tracy Heath <tracyh@berkeley.edu>
2014-08-17 17:11:09 -0700

    updated Rev files

---
+++ src/test/TestSkylineBirthDeathHCV.h
@@ -0,0 +1,14 @@
+//
+//  TestSkylineBirthDeathHCV.h
+//  RevBayesX
+//
+//  Created by Tracy Heath on 8/17/14.
+//  Copyright (c) 2014 Tracy Heath. All rights reserved.
+//
+
+#ifndef __RevBayesX__TestSkylineBirthDeathHCV__
+#define __RevBayesX__TestSkylineBirthDeathHCV__
+
+#include <iostream>
+
+#endif /* defined(__RevBayesX__TestSkylineBirthDeathHCV__) */
