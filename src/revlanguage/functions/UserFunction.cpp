8c65496fe17d666c4b3cff7807114eefb88e7866
Fredrik Ronquist <fredrik.ronquist@nrm.se>
2014-08-26 03:42:24 -0400

    Fixed problem with type promotion when the variable was assignable. Preparing for proper inclusion of user functions in DAGs.

--- src/revlanguage/functions/UserFunction.cpp
+++ src/revlanguage/functions/UserFunction.cpp
@@ -107,10 +107,7 @@ RevPtr<Variable> UserFunction::execute( void )
 
     if ( retVal->hasDagNode() )
     {
-        UserFunction* fxn = this->clone();
-        UserFunction* code = this->clone();
-        
-        retVal->makeDeterministicValue( fxn, code );
+        retVal->makeUserFunctionValue( this->clone() );
 
         return new Variable( retVal );
     }
