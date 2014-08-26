8741128d275ff50072fb0643d3299f3d0f3b879e
Fredrik Ronquist <fredrik.ronquist@nrm.se>
2014-08-26 07:10:31 -0400

    Modest changes to the header and the contributors and contacts functions.

--- src/revlanguage/functions/basic/Func_contacts.h
+++ src/revlanguage/functions/basic/Func_contacts.h
@@ -1,14 +1,3 @@
-/**
- * @file
- * This file contains the declaration of Func_history, which is
- * used to provide the user with information on history.
- *
- * @brief Declaration of Func_history
- *
- * @author Fredrik Ronquist
- * @license GPL version 3
- */
-
 #ifndef Func_history_H
 #define Func_history_H
 
@@ -16,21 +5,27 @@
 
 namespace RevLanguage {
     
-    class Func_history : public Function {
+    /**
+     * @brief Func_contacts: print contact details of core contributors
+     *
+     * This function class prints the contact details of the core
+     * contributors to the RevBayes project.
+     */
+    class Func_contacts : public Function {
         
     public:
-        Func_history( void );
+        Func_contacts( void );
         
         // Basic utility functions
-        Func_history*                           clone(void) const;                                                          //!< Clone object
+        Func_contacts*                          clone(void) const;                                                          //!< Clone object
         static const std::string&               getClassType(void);                                                         //!< Get Rev type
         static const TypeSpec&                  getClassTypeSpec(void);                                                     //!< Get class type spec
         const TypeSpec&                         getTypeSpec(void) const;                                                    //!< Get language type of the object
         
-        // Func_history functions
+        // Func_contacts functions
         const ArgumentRules&                    getArgumentRules(void) const;                                               //!< Get argument rules
         const TypeSpec&                         getReturnType(void) const;                                                  //!< Get type of return val
-        bool                                    throws(void) const { return false; }                                         //!< Function may throw exceptions
+        bool                                    throws(void) const { return false; }                                        //!< Function may throw exceptions
         
         RevPtr<Variable>                        execute(void);                                                              //!< Execute function
         
