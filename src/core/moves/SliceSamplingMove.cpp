0b68cfeda0f1a1a64ec6ce2420965e12fff4f469
Benjamin Redelings <benjamin.redelings@duke.edu>
2014-08-27 09:45:19 -0400

    Create files for SliceSamplingMove.

---
+++ src/core/moves/SliceSamplingMove.cpp
@@ -0,0 +1,378 @@
+#include "DagNode.h"
+#include "SliceSamplingMove.h"
+#include "Proposal.h"
+#include "RandomNumberFactory.h"
+#include "RandomNumberGenerator.h"
+#include "RbMathLogic.h"
+
+#include <cmath>
+#include <iomanip>
+#include <sstream>
+#include <iostream>
+
+using namespace RevBayesCore;
+
+
+/** 
+ * Constructor
+ *
+ * Here we simply allocate and initialize the move object.
+ *
+ * \param[in]    w   The weight how often the proposal will be used (per iteration).
+ * \param[in]    t   If auto tuning should be used.
+ */
+SliceSamplingMove::SliceSamplingMove( Proposal *p, double w, bool t ) : AbstractMove(w,t),
+    affectedNodes(),
+    nodes(),
+    numAccepted( 0 ),
+    proposal( p )
+{
+    nodes = proposal->getNodes();
+    
+    for (std::set<DagNode*>::iterator it = nodes.begin(); it != nodes.end(); ++it)
+    {
+        (*it)->getAffectedNodes( affectedNodes );
+    }
+    
+}
+
+
+/**
+ * Copy constructor.
+ * We need to create a deep copy of the proposal here.
+ *
+ * \param[in]   m   The object to copy.
+ *
+ */
+SliceSamplingMove::SliceSamplingMove(const SliceSamplingMove &m) : AbstractMove(m),
+    affectedNodes( m.affectedNodes ),
+    nodes( m.nodes ),
+    numAccepted( m.numAccepted ),
+    proposal( m.proposal->clone() )
+{
+    
+}
+
+
+/**
+ * Basic destructor doing nothing.
+ */
+SliceSamplingMove::~SliceSamplingMove( void )
+{
+    delete proposal;
+}
+
+
+/** 
+ * Overloaded assignment operator.
+ * We need a deep copy of the operator.
+ */
+SliceSamplingMove& SliceSamplingMove::operator=(const RevBayesCore::SliceSamplingMove &m)
+{
+    
+    if ( this != &m )
+    {
+        // free memory
+        delete proposal;
+        
+        affectedNodes = m.affectedNodes;
+        nodes = m.nodes;
+        numAccepted = m.numAccepted;
+        proposal = m.proposal->clone();
+    }
+    
+    return *this;
+}
+
+
+/**
+ * The clone function is a convenience function to create proper copies of inherited objected.
+ * E.g. a.clone() will create a clone of the correct type even if 'a' is of derived type 'b'.
+ *
+ * \return A new copy of the SliceSamplingMove. 
+ */
+SliceSamplingMove* SliceSamplingMove::clone( void ) const 
+{
+    
+    return new SliceSamplingMove( *this );
+}
+
+
+/**
+ * Get the set of nodes on which this move is working on.
+ *
+ * \return The set of nodes.
+ */
+const std::set<DagNode*>& SliceSamplingMove::getDagNodes( void ) const
+{
+    
+    return nodes;
+}
+
+
+/**
+ * Get moves' name of object 
+ *
+ * \return The moves' name.
+ */
+const std::string& SliceSamplingMove::getMoveName( void ) const 
+{
+    
+    return proposal->getProposalName();
+}
+
+
+
+void SliceSamplingMove::performMove( double heat, bool raiseLikelihoodOnly )
+{
+    // Propose a new value
+    proposal->prepareProposal();
+    double lnHastingsRatio = proposal->doProposal();
+    
+    // first we touch all the nodes
+    // that will set the flags for recomputation
+    for (std::set<DagNode*>::iterator it = nodes.begin(); it != nodes.end(); ++it)
+    {
+        
+        (*it)->touch();
+    }
+    
+    double lnPriorRatio = 0.0;
+    double lnLikelihoodRatio = 0.0;
+    
+    // compute the probability of the current value for each node
+    for (std::set<DagNode*>::iterator it = nodes.begin(); it != nodes.end(); ++it)
+    {
+        lnPriorRatio += (*it)->getLnProbabilityRatio();
+    }
+    
+    // then we recompute the probability for all the affected nodes
+    for (std::set<DagNode*>::iterator it = affectedNodes.begin(); it != affectedNodes.end(); ++it) 
+    {
+        if ( (*it)->isClamped() )
+        {
+            
+            lnLikelihoodRatio += (*it)->getLnProbabilityRatio();
+            
+        }
+        else
+        {
+            lnPriorRatio += (*it)->getLnProbabilityRatio();
+        }
+    }
+    
+    // exponentiate with the chain heat
+    double lnPosteriorRatio;
+    if ( raiseLikelihoodOnly )
+    {
+        lnPosteriorRatio = heat * lnLikelihoodRatio + lnPriorRatio;
+    }
+    else
+    {
+        lnPosteriorRatio = heat * (lnLikelihoodRatio + lnPriorRatio);
+    }
+	
+	if ( !RbMath::isAComputableNumber(lnPosteriorRatio) ) {
+		
+            proposal->undoProposal();
+            
+            // call restore for each node
+            for (std::set<DagNode*>::iterator i = nodes.begin(); i != nodes.end(); ++i)
+            {
+                (*i)->restore();
+            }
+	}
+    else
+    {
+    
+        // finally add the Hastings ratio
+        double lnAcceptanceRatio = lnPosteriorRatio + lnHastingsRatio;
+    
+        if (lnAcceptanceRatio >= 0.0)
+        {
+            numAccepted++;
+        
+            // call accept for each node
+            for (std::set<DagNode*>::iterator i = nodes.begin(); i != nodes.end(); ++i)
+            {
+                (*i)->keep();
+            }
+        
+        }
+        else if (lnAcceptanceRatio < -300.0)
+        {
+            proposal->undoProposal();
+        
+            // call restore for each node
+            for (std::set<DagNode*>::iterator i = nodes.begin(); i != nodes.end(); ++i)
+            {
+                (*i)->restore();
+            }
+        }
+        else
+        {
+            double r = exp(lnAcceptanceRatio);
+            // Accept or reject the move
+            double u = GLOBAL_RNG->uniform01();
+            if (u < r)
+            {
+                numAccepted++;
+            
+                // call accept for each node
+                for (std::set<DagNode*>::iterator i = nodes.begin(); i != nodes.end(); ++i)
+                {
+                    (*i)->keep();
+                }
+            
+                proposal->cleanProposal();
+            }
+            else
+            {
+                proposal->undoProposal();
+            
+                // call restore for each node
+                for (std::set<DagNode*>::iterator i = nodes.begin(); i != nodes.end(); ++i)
+                {
+                    (*i)->restore();
+                }
+            }
+        }
+
+    }
+
+}
+
+
+/**
+ * Print the summary of the move.
+ *
+ * The summary just contains the current value of the tuning parameter.
+ * It is printed to the stream that it passed in.
+ *
+ * \param[in]     o     The stream to which we print the summary.
+ */
+void SliceSamplingMove::printSummary(std::ostream &o) const 
+{
+    std::streamsize previousPrecision = o.precision();
+    std::ios_base::fmtflags previousFlags = o.flags();
+    
+    o << std::fixed;
+    o << std::setprecision(4);
+    
+    // print the name
+    const std::string &n = getMoveName();
+    size_t spaces = 40 - (n.length() > 40 ? 40 : n.length());
+    o << n;
+    for (size_t i = 0; i < spaces; ++i) {
+        o << " ";
+    }
+    o << " ";
+    
+    // print the DagNode name
+    const std::string &dn_name = (*nodes.begin())->getName();
+    spaces = 20 - (dn_name.length() > 20 ? 20 : dn_name.length());
+    o << dn_name;
+    for (size_t i = 0; i < spaces; ++i) {
+        o << " ";
+    }
+    o << " ";
+    
+    // print the weight
+    int w_length = 4 - (int)log10(weight);
+    for (int i = 0; i < w_length; ++i) {
+        o << " ";
+    }
+    o << weight;
+    o << " ";
+    
+    // print the number of tries
+    int t_length = 9 - (int)log10(numTried);
+    for (int i = 0; i < t_length; ++i) {
+        o << " ";
+    }
+    o << numTried;
+    o << " ";
+    
+    // print the number of accepted
+    int a_length = 9;
+    if (numAccepted > 0) a_length -= (int)log10(numAccepted);
+    
+    for (int i = 0; i < a_length; ++i) {
+        o << " ";
+    }
+    o << numAccepted;
+    o << " ";
+    
+    // print the acceptance ratio
+    double ratio = numAccepted / (double)numTried;
+    if (numTried == 0) ratio = 0;
+    int r_length = 5;
+    
+    for (int i = 0; i < r_length; ++i) {
+        o << " ";
+    }
+    o << ratio;
+    o << " ";
+    
+    proposal->printParameterSummary( o );
+    
+    o << std::endl;
+    
+    o.setf(previousFlags);
+    o.precision(previousPrecision);
+    
+    
+}
+
+/**
+ * Reset the move counters. Here we only reset the counter for the number of accepted moves.
+ *
+ */
+void SliceSamplingMove::resetMoveCounters( void )
+{
+    numAccepted = 0;
+}
+
+
+/**
+ * Swap the current variable for a new one.
+ *
+ * \param[in]     oldN     The old variable that needs to be replaced.
+ * \param[in]     newN     The new variable.
+ */
+void SliceSamplingMove::swapNode(DagNode *oldN, DagNode *newN) 
+{
+    
+    // find the old node
+    std::set<DagNode*>::iterator pos = nodes.find( oldN );
+    // remove it from the set if it was contained
+    if ( pos != nodes.end() )
+    {
+        nodes.erase( pos );
+    }
+    // insert the new node
+    nodes.insert( newN );
+        
+    affectedNodes.clear();
+    for (std::set<DagNode*>::iterator it = nodes.begin(); it != nodes.end(); ++it)
+    {
+        (*it)->getAffectedNodes( affectedNodes );
+    }
+    
+    proposal->swapNode(oldN, newN);
+    
+}
+
+
+/**
+ * Tune the move to accept the desired acceptance ratio.
+ * We only compute the acceptance ratio here and delegate the call to the proposal.
+ */
+void SliceSamplingMove::tune( void ) {
+    
+    double rate = numAccepted / double(numTried);
+    
+    proposal->tune( rate );
+    
+}
+
---
+++ src/core/moves/SliceSamplingMove.h
@@ -0,0 +1,60 @@
+#ifndef SliceSamplingMove_H
+#define SliceSamplingMove_H
+
+#include "AbstractMove.h"
+
+#include <set>
+#include <vector>
+
+namespace RevBayesCore {
+    
+    class Proposal;
+    
+    /**
+     * Base class for all Metropolis-Hastings within an MCMC. 
+     *
+     * The base class of all moves only provides the interface for the call to propose a new move.
+     * Here the perform methods actually does the accept/reject step.
+     * All specifics are implemented in the derived classes.
+     *
+     * @copyright Copyright 2009-
+     * @author The RevBayes Development Core Team (Sebastian Hoehna)
+     * @since 2014-03-26, version 1.0
+     *
+     */
+    class SliceSamplingMove : public AbstractMove {
+        
+    public:
+        SliceSamplingMove(Proposal *p, double w, bool autoTune = false);                                                                           //!< Constructor
+        SliceSamplingMove(const SliceSamplingMove &m);                                                                                        //!< Copy constructor
+        virtual                                                 ~SliceSamplingMove(void);                                                           //!< Destructor
+
+        // overloaded operators
+        SliceSamplingMove&                                      operator=(const SliceSamplingMove &m);                                             //!< Assignment operator
+        
+        // pure virtual public methods
+        virtual SliceSamplingMove*                              clone(void) const;
+        const std::set<DagNode*>&                               getDagNodes(void) const;                                                                //!< Get the nodes vector
+        const std::string&                                      getMoveName(void) const;                                                                //!< Get the name of the move for summary printing
+        void                                                    printSummary(std::ostream &o) const;                                                    //!< Print the move summary
+        virtual void                                            swapNode(DagNode *oldN, DagNode *newN);                                                 //!< Swap the pointers to the variable on which the move works on.
+        void                                                    tune(void);                                                                             //!< Specific tuning of the move
+        
+    protected:
+        //protected methods that are overwritten from the base class
+        void                                                    performMove(double heat, bool raiseLikelihoodOnly);                                     //!< Perform the move.
+        void                                                    resetMoveCounters(void);                                                                //!< Reset the counters such as numAccepted.
+        
+    private:
+        
+        // parameters
+        std::set<DagNode*>                                      affectedNodes;                                                                          //!< The affected nodes by this move.
+        std::set<DagNode*>                                      nodes;                                                                                  //!< The set of nodes this move is working on
+        unsigned int                                            numAccepted;                                                                            //!< Number of times accepted
+        Proposal*                                               proposal;                                                                               //!< The proposal distribution
+    };
+    
+}
+
+
+#endif
