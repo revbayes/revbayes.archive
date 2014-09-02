c5ff31a5bdbf6c4e6162159ad112042486d9a39b
boussau <boussau@gmail.com>
2014-09-02 19:34:24 +0200

    Implementation of a rooted triple distribution, constructed from a tree or a vector of trees. Compiles, not tested yet.

---
+++ src/core/datatypes/trees/RootedTripletDistribution.cpp
@@ -0,0 +1,9 @@
+//
+//  RootedTripletDistribution.cpp
+//  RevBayes
+//
+//  Created by Bastien on 31/08/14.
+//  Copyright (c) 2014 Bastien Boussau. All rights reserved.
+//
+
+#include "RootedTripletDistribution.h"
---
+++ src/core/datatypes/trees/RootedTripletDistribution.h
@@ -0,0 +1,14 @@
+//
+//  RootedTripletDistribution.h
+//  RevBayes
+//
+//  Created by Bastien on 31/08/14.
+//  Copyright (c) 2014 Bastien Boussau. All rights reserved.
+//
+
+#ifndef __RevBayes__RootedTripletDistribution__
+#define __RevBayes__RootedTripletDistribution__
+
+#include <iostream>
+
+#endif /* defined(__RevBayes__RootedTripletDistribution__) */
