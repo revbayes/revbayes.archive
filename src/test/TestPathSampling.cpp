181b0a3f8c0ce8689aa365ce71cb26f8a4d291fd
Michael Landis <mlandis@gmail.com>
2014-09-10 23:38:28 -0700

    updating general thc for e.g. amino acid rate maps

--- src/test/TestPathSampling.cpp
+++ src/test/TestPathSampling.cpp
@@ -65,6 +65,14 @@
 #include "PathUniformizationSampleProposal.h"
 
 
+// amino acid uniformization test
+#include "AminoAcidRateMapFunction.h"
+#include "AminoAcidState.h"
+#include "RateMap_AminoAcid.h"
+#include "RateMatrix_Blosum62.h"
+#include "GeneralTreeHistoryCtmc.h"
+
+
 
 using namespace RevBayesCore;
 
@@ -76,7 +84,17 @@ TestPathSampling::~TestPathSampling() {
     // nothing to do
 }
 
-bool TestPathSampling::run( void )
+bool TestPathSampling::run(void)
+{
+    int i = 1;
+    if      (i == 0)
+        return run_binary();
+    else if (i == 1)
+        return run_aa();
+    else return false;
+}
+
+bool TestPathSampling::run_binary( void )
 {
     
     ////////////
@@ -142,7 +160,6 @@ bool TestPathSampling::run( void )
 	}
     glr_nonConst[0]->setValue(0.1);
     glr_nonConst[1]->setValue(0.1);
-
     DeterministicNode< std::vector< double > >* glr_vector = new DeterministicNode< std::vector< double > >( "glr_vector", new VectorFunction< double >( glr ) );
     
     // Q-map used to compute likehood under the full model
@@ -240,6 +257,154 @@ bool TestPathSampling::run( void )
     //    delete sglr;
     std::cout << "Finished CharacterHistory model test." << std::endl;
     
+    return true;
+}
+
+
+
+
+
+
+
+
+
+
+bool TestPathSampling::run_aa( void )
+{
+    
+
+    ////////////
+    // settings
+    ////////////
+    unsigned mcmcGenerations = 1000;
+    
+    ////////////
+    // io
+    ////////////
+    
+    std::vector<unsigned> old_seed = GLOBAL_RNG->getSeed();
+    std::vector<unsigned> seed;
+    //    seed.push_back(1+1); seed.push_back(1);
+    //    GLOBAL_RNG->setSeed(seed);
+    std::stringstream ss;
+    ss << ".s0_" << old_seed[0] << ".s1_" << old_seed[1];
+    
+    std::string filepath="/Users/mlandis/data/bayarea/output/";
+    std::string fn = "";
+    fn = "16tip_100areas.nex";
+    
+    std::string in_fp = "/Users/mlandis/Documents/code/revbayes-code/examples/data/";
+    std::vector<AbstractCharacterData*> data = NclReader::getInstance().readMatrices(in_fp + fn);
+    std::cout << "Read " << data.size() << " matrices." << std::endl;
+    size_t numChars = data[0]->getNumberOfCharacters();
+    
+    // tree
+    std::vector<TimeTree*> trees = NclReader::getInstance().readTimeTrees( in_fp + fn );
+    std::cout << "Read " << trees.size() << " trees." << std::endl;
+    std::cout << trees[0]->getNewickRepresentation() << std::endl;
+    size_t numNodes = trees[0]->getNumberOfNodes();
+    
+    ////////////
+    // model
+    ////////////
+    
+    // tree
+    ConstantNode<TimeTree>* tau = new ConstantNode<TimeTree>("tree", new TimeTree( *trees[0] ));
+    
+    // Q matrix for independence model/sampling
+    ConstantNode<RateMatrix>* q_sample = new ConstantNode<RateMatrix>("Q_samp", new RateMatrix_Blosum62());
+    
+    // Q-map used to compute likehood under the full model
+    AminoAcidRateMapFunction* aarmf_likelihood = new AminoAcidRateMapFunction(numChars);
+    aarmf_likelihood->setRateMatrix(q_sample);
+    DeterministicNode<RateMap> *q_likelihood = new DeterministicNode<RateMap>("Q_like", aarmf_likelihood);
+        
+    // and the character model
+    GeneralTreeHistoryCtmc<AminoAcidState, TimeTree> *ctmc = new GeneralTreeHistoryCtmc<AminoAcidState, TimeTree>(tau, 20, numChars, false);
+    ctmc->setRateMap(q_likelihood);
+    StochasticNode< AbstractCharacterData > *charactermodel = new StochasticNode< AbstractCharacterData >("ctmc", ctmc );
+    
+    // assign tips and sample histories
+    charactermodel->clamp( data[0] );
+    charactermodel->redraw();
+    
+    std::cout << "lnL = " << charactermodel->getDistribution().computeLnProbability() << "\n";
+/*
+    //    GLOBAL_RNG->setSeed(old_seed);
+    //    glr_nonConst[0]->redraw();
+    //    glr_nonConst[1]->redraw();
+    
+    std::cout << "seed " << old_seed[0] << " " << old_seed[1] << "\n";
+ 
+    ////////////
+    // moves
+    ////////////
+    
+    std::cout << "Adding moves\n";
+    RbVector<Move> moves;
+    
+    // update our transition rates
+    for( size_t i=0; i<2; i++)
+    {
+        moves.push_back( new MetropolisHastingsMove( new ScaleProposal(glr_nonConst[i], 0.1), 2.0, !true ) );
+    }
+    
+    // path
+    moves.push_back(new MetropolisHastingsMove(new PathUniformizationSampleProposal<StandardState,TimeTree>(charactermodel, tau, q_sample, 0.5), numNodes*2, false));
+    //    moves.push_back(new PathRejectionSampleMove<StandardState, TimeTree>(charactermodel, tau, q_sample, new PathRejectionSampleProposal<StandardState,TimeTree>(charactermodel, tau, q_sample, 0.4), 0.4, false, numNodes));
+    
+    //    // node
+    //    moves.push_back(new PathRejectionSampleMove<StandardState, TimeTree>(charactermodel, tau, q_sample, new NodeRejectionSampleProposal<StandardState,TimeTree>(charactermodel, tau, q_sample, 0.1), 0.1, false, numNodes*2));
+    //    moves.push_back(new PathRejectionSampleMove<StandardState, TimeTree>(charactermodel, tau, q_sample, new NodeRejectionSampleProposal<StandardState,TimeTree>(charactermodel, tau, q_sample, 0.4), 0.4, false, numNodes));
+    
+    
+    
+    ////////////
+    // monitors
+    ////////////
+    
+    std::cout << "Adding monitors\n";
+    RbVector<Monitor> monitors;
+    
+    std::set<DagNode*> monitoredNodes;
+    monitoredNodes.insert( glr_vector );
+    
+    monitors.push_back(new FileMonitor(monitoredNodes, 10, filepath + "rb" + ss.str() + ".parameters.txt", "\t"));
+    monitors.push_back(new ScreenMonitor(monitoredNodes, 10, "\t" ) );
+    monitors.push_back(new TreeCharacterHistoryNodeMonitor<StandardState,TimeTree>(charactermodel, tau, 10, filepath + "rb" + ss.str() + ".events.txt", "\t"));
+    monitors.push_back(new TreeCharacterHistoryNodeMonitor<StandardState,TimeTree>(charactermodel, tau, 10, filepath + "rb" + ss.str() + ".counts.txt", "\t", true, true, true, false, true, true, false));
+    monitors.push_back(new TreeCharacterHistoryNhxMonitor<StandardState,TimeTree>(charactermodel, tau, ta, 100, mcmcGenerations, 0.2, filepath + "rb" + ss.str() + ".nhx.txt", "\t"));
+    
+    monitors.push_back(new FileMonitor(tau, 10, filepath + "rb" + ss.str() + ".trees.txt", "\t"));
+    
+    //////////
+    // model
+    //////////
+    std::cout << "Instantiating model\n";
+    Model myModel = Model(charactermodel);
+    
+    
+    //////////
+    // mcmc
+    //////////
+    std::cout << "Instantiating mcmc\n";
+    Mcmc myMcmc = Mcmc( myModel, moves, monitors );
+    myMcmc.setScheduleType("single");
+    myMcmc.run(mcmcGenerations);
+    myMcmc.printOperatorSummary();
+    
+    
+    //////////
+    // clean-up
+    //////////
+    std::cout << "Cleaning up objects\n";
+    
+    // segfault when uncommented? strange.
+    //    delete dp;
+    //    delete glr;
+    //    delete sglr;
+    std::cout << "Finished CharacterHistory model test." << std::endl;
+*/
     return true;
 }
 
