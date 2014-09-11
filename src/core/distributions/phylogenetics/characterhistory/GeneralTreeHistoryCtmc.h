31af9485a76c98be14c3d575a9563b1e97f3ae64
Michael Landis <mlandis@gmail.com>
2014-09-11 15:58:08 -0700

    small correction

--- src/core/distributions/phylogenetics/characterhistory/GeneralTreeHistoryCtmc.h
+++ src/core/distributions/phylogenetics/characterhistory/GeneralTreeHistoryCtmc.h
@@ -386,16 +386,16 @@ bool RevBayesCore::GeneralTreeHistoryCtmc<charType, treeType>::samplePathHistory
     if (node.isRoot())
         return true;
     
-//    PathRejectionSampleProposal<charType,treeType> p(   this->getStochasticNode(),
-//                                                        const_cast<StochasticNode<treeType>* >(  static_cast<const StochasticNode<treeType>* >(this->tau)),
-//                                                        const_cast<DeterministicNode<RateMap>* >( static_cast<const DeterministicNode<RateMap>* >(homogeneousRateMap)),
-//                                                        1.0);
-    
-    ConstantNode<RateMatrix>* q_sample = new ConstantNode<RateMatrix*>("q_sample", new RateMatrix_Blosum62());
     PathRejectionSampleProposal<charType,treeType> p(   this->getStochasticNode(),
-                                                     const_cast<StochasticNode<treeType>* >(  static_cast<const StochasticNode<treeType>* >(this->tau)),
-                                                     const_cast<DeterministicNode<RateMap>* >( static_cast<const DeterministicNode<RateMap>* >(homogeneousRateMap)),
-                                                     1.0);
+                                                        const_cast<StochasticNode<treeType>* >(  static_cast<const StochasticNode<treeType>* >(this->tau)),
+                                                        const_cast<DeterministicNode<RateMap>* >( static_cast<const DeterministicNode<RateMap>* >(homogeneousRateMap)),
+                                                        1.0);
+    
+//    ConstantNode<RateMatrix>* q_sample = new ConstantNode<RateMatrix*>("q_sample", new RateMatrix_Blosum62());
+//    PathRejectionSampleProposal<charType,treeType> p(   this->getStochasticNode(),
+//                                                     const_cast<StochasticNode<treeType>* >(  static_cast<const StochasticNode<treeType>* >(this->tau)),
+//                                                     const_cast<DeterministicNode<RateMap>* >( static_cast<const DeterministicNode<RateMap>* >(homogeneousRateMap)),
+//                                                     1.0);
 
     
     p.assignNode(const_cast<TopologyNode*>(&node));
