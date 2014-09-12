32e8cd516b58e98740c3e02753f8f49f21c36b69
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2014-09-12 11:08:12 -0700

    Removing scripts and models.

--- scripts/createCMake.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh 
- # Shell script to find out all the files under a directory and 
- #its subdirectories. This also takes into consideration those files
- #or directories which have spaces or newlines in their names 
-
-./createCMakeSingle.sh "../src/core" "core" > "../projects/cmake/core/CMakeLists.txt"
-./createCMakeSingle.sh "../src/revlanguage" "revlanguage" > "../projects/cmake/revlanguage/CMakeLists.txt"
