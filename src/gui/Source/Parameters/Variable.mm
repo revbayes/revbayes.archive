eadf52ea42a86679ab186da2b9185d08ac95e89a
John Huelsenbeck <johnh@berkeley.edu>
2014-09-22 13:43:52 -0700

    Made changes to GUI

--- src/gui/Source/Parameters/Variable.mm
+++ src/gui/Source/Parameters/Variable.mm
@@ -1,11 +1,3 @@
-//
-//  Variable.m
-//  RevBayes
-//
-//  Created by John Huelsenbeck on 9/16/14.
-//  Copyright (c) 2014 John Huelsenbeck. All rights reserved.
-//
-
 #import "Variable.h"
 
 @implementation Variable
