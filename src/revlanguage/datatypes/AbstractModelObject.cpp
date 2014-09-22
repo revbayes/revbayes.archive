64e7c3b9316323da0b16aa227c39395a70b47e6f
Michael Landis <mlandis@gmail.com>
2014-09-22 14:35:16 -0700

    add rev wrapper for fnFreeBinary

--- src/revlanguage/datatypes/AbstractModelObject.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  AbstractModelObject.cpp
-//  RevBayes
-//
-//  Created by John Huelsenbeck on 9/15/14.
-//  Copyright (c) 2014 John Huelsenbeck. All rights reserved.
-//
-
-#include "AbstractModelObject.h"
