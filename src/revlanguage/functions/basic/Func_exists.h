784ffea230350222e00d99fcc9a1e4ef3985b249
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2014-10-09 14:32:51 -0700

    Adding a basic function that checks if a variable exists.

---
+++ src/revlanguage/functions/basic/Func_exists.h
@@ -0,0 +1,42 @@
+#ifndef Func_exists_H
+#define Func_exists_H
+
+#include "RlFunction.h"
+
+namespace RevLanguage {
+    
+    /**
+     * @brief Rev function to check if a variable exists in the workspace.
+     *
+     * Check in the workspace if a variable exists and returns either true or false.
+     *
+     *
+     * @copyright Copyright 2009-
+     * @author The RevBayes Development Core Team (Sebastian Hoehna)
+     * @since Version 1.0, 2014-10-09
+     *
+     */
+    class Func_exists : public Function {
+        
+    public:
+        Func_exists( void );
+        
+        // Basic utility functions
+        Func_exists*                                clone(void) const;                                                          //!< Clone object
+        static const std::string&                   getClassType(void);                                                         //!< Get Rev exists
+        static const TypeSpec&                      getClassTypeSpec(void);                                                     //!< Get class exists spec
+        const TypeSpec&                             getTypeSpec(void) const;                                                    //!< Get language exists of the object
+        
+        // Func_source functions
+        const ArgumentRules&                        getArgumentRules(void) const;                                               //!< Get argument rules
+        const TypeSpec&                             getReturnType(void) const;                                                  //!< Get exists of return val
+        bool                                        throws(void) const { return false; }                                        //!< Function may throw exceptions
+        
+        RevPtr<Variable>                            execute(void);                                                              //!< Execute function
+        
+    };
+    
+}
+
+#endif
+
