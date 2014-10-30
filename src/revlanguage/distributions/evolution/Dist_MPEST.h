88d3e6a946287f5960a03fa2873768b72a899899
boussau <boussau@gmail.com>
2014-10-30 19:23:52 +0100

    New function in the Rev language to use the MPEST distribution. Compiles, but not tested.

---
+++ src/revlanguage/distributions/evolution/Dist_MPEST.h
@@ -0,0 +1,14 @@
+//
+//  Dist_MPEST.h
+//  RevBayes
+//
+//  Created by Bastien on 30/10/14.
+//  Copyright (c) 2014 Bastien Boussau. All rights reserved.
+//
+
+#ifndef __RevBayes__Dist_MPEST__
+#define __RevBayes__Dist_MPEST__
+
+#include <stdio.h>
+
+#endif /* defined(__RevBayes__Dist_MPEST__) */
