88c0f58325f4e7f3f2532f447c9458b8b310c8e5
boussau <boussau@gmail.com>
2014-11-12 12:22:23 +0100

    Creating new monitor files for monitoring specifically the prior, the likelihood and the posterior.

---
+++ src/core/monitors/PriorMonitor.h
@@ -0,0 +1,14 @@
+//
+//  PriorMonitor.h
+//  RevBayes
+//
+//  Created by Bastien on 12/11/14.
+//  Copyright (c) 2014 Bastien Boussau. All rights reserved.
+//
+
+#ifndef __RevBayes__PriorMonitor__
+#define __RevBayes__PriorMonitor__
+
+#include <stdio.h>
+
+#endif /* defined(__RevBayes__PriorMonitor__) */
