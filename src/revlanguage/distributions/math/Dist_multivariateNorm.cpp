f5a291261732a6596c16911fa7d210f2e906088f
Michael Landis <mlandis@gmail.com>
2014-11-17 13:52:38 -0800

    update mvnorm Variable to RevVariable so it compiles

--- src/revlanguage/distributions/math/Dist_multivariateNorm.cpp
+++ src/revlanguage/distributions/math/Dist_multivariateNorm.cpp
@@ -155,7 +155,7 @@ void Dist_multivariateNorm::printValue(std::ostream& o) const {
  * \param[in]    name     Name of the member variable.
  * \param[in]    var      Pointer to the variable.
  */
-void Dist_multivariateNorm::setConstParameter(const std::string& name, const RevPtr<const Variable> &var)
+void Dist_multivariateNorm::setConstParameter(const std::string& name, const RevPtr<const RevVariable> &var)
 {
     
     if ( name == "mean" ) 
