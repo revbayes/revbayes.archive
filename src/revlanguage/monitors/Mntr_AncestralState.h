05d13f636d8ab0736776104700d98b57bb878dbb
wf <willfreyman@gmail.com>
2014-12-04 00:40:18 -0800

    fixed workspace problem with Mntr_AncestralState

--- src/revlanguage/monitors/Mntr_AncestralState.h
+++ src/revlanguage/monitors/Mntr_AncestralState.h
@@ -160,23 +160,23 @@ const TypeSpec& Mntr_AncestralState<treeType>::getClassTypeSpec(void)
 template <class treeType>
 const MemberRules& Mntr_AncestralState<treeType>::getParameterRules(void) const {
     
-    static MemberRules modelMonitorMemberRules;
+    static MemberRules asMonitorMemberRules;
     static bool rulesSet = false;
     
     if ( !rulesSet ) 
     {
-		modelMonitorMemberRules.push_back( new ArgumentRule("tree"           , treeType::getClassTypeSpec(), ArgumentRule::BY_REFERENCE, ArgumentRule::STOCHASTIC ) );
-		modelMonitorMemberRules.push_back( new ArgumentRule("ctmc"           , RevObject::getClassTypeSpec(), ArgumentRule::BY_VALUE, ArgumentRule::ANY ) );
-        modelMonitorMemberRules.push_back( new ArgumentRule("filename"      , RlString::getClassTypeSpec() , ArgumentRule::BY_VALUE ) );
-		modelMonitorMemberRules.push_back( new ArgumentRule("type"      , RlString::getClassTypeSpec() , ArgumentRule::BY_VALUE ) );
-        modelMonitorMemberRules.push_back( new ArgumentRule("printgen"      , Natural::getClassTypeSpec()  , ArgumentRule::BY_VALUE, ArgumentRule::ANY, new Natural(1) ) );
-        modelMonitorMemberRules.push_back( new ArgumentRule("separator"     , RlString::getClassTypeSpec() , ArgumentRule::BY_VALUE, ArgumentRule::ANY, new RlString("\t") ) );
-        modelMonitorMemberRules.push_back( new ArgumentRule("append"        , RlBoolean::getClassTypeSpec(), ArgumentRule::BY_VALUE, ArgumentRule::ANY, new RlBoolean(false) ) );
+		asMonitorMemberRules.push_back( new ArgumentRule("tree"           , treeType::getClassTypeSpec(), ArgumentRule::BY_REFERENCE, ArgumentRule::STOCHASTIC ) );
+		asMonitorMemberRules.push_back( new ArgumentRule("ctmc"           , RevObject::getClassTypeSpec(), ArgumentRule::BY_VALUE, ArgumentRule::ANY ) );
+        asMonitorMemberRules.push_back( new ArgumentRule("filename"      , RlString::getClassTypeSpec() , ArgumentRule::BY_VALUE ) );
+		asMonitorMemberRules.push_back( new ArgumentRule("type"      , RlString::getClassTypeSpec() , ArgumentRule::BY_VALUE ) );
+        asMonitorMemberRules.push_back( new ArgumentRule("printgen"      , Natural::getClassTypeSpec()  , ArgumentRule::BY_VALUE, ArgumentRule::ANY, new Natural(1) ) );
+        asMonitorMemberRules.push_back( new ArgumentRule("separator"     , RlString::getClassTypeSpec() , ArgumentRule::BY_VALUE, ArgumentRule::ANY, new RlString("\t") ) );
+        asMonitorMemberRules.push_back( new ArgumentRule("append"        , RlBoolean::getClassTypeSpec(), ArgumentRule::BY_VALUE, ArgumentRule::ANY, new RlBoolean(false) ) );
         
         rulesSet = true;
     }
     
-    return modelMonitorMemberRules;
+    return asMonitorMemberRules;
 }
 
 /** Get type spec */
