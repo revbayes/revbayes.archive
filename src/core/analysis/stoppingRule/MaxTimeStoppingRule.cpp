f2e49996e82fd985b33b0de26ef72b7829328483
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2015-01-11 19:40:28 -0800

    Outline for stopping rules. If these are fulfilled the automatic analysis will stop.

---
+++ src/core/analysis/stoppingRule/MaxTimeStoppingRule.cpp
@@ -0,0 +1,9 @@
+//
+//  MaxTimeStoppingRule.cpp
+//  revbayes
+//
+//  Created by Sebastian Hoehna on 1/11/15.
+//  Copyright (c) 2015 Sebastian Hoehna. All rights reserved.
+//
+
+#include "MaxTimeStoppingRule.h"
