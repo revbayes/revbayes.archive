11f90ba9961d3435ff3331fc6f2c84848a9178f2
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2015-01-31 09:53:11 +0100

    Adding the hyperbolic tangent function to Rev.

--- src/revlanguage/functions/math/Func_hyperbolicTangent.h
+++ src/revlanguage/functions/math/Func_hyperbolicTangent.h
@@ -1,7 +1,7 @@
-#ifndef Func_mean_H
-#define Func_mean_H
+#ifndef Func_hyperbolicTangent_H
+#define Func_hyperbolicTangent_H
 
-#include "Real.h"
+#include "Probability.h"
 #include "RlTypedFunction.h"
 
 #include <string>
@@ -9,25 +9,25 @@
 namespace RevLanguage {
     
     /**
-     * The RevLanguage wrapper of the arithmetic mean function.
+     * The RevLanguage wrapper of the hyperbolic tangent function.
      *
-     * The RevLanguage wrapper of the mean function connects
-     * the variables/parameters of the function and creates the internal MeanFunction object.
-     * Please read the MeanFunction.h for more info.
+     * The RevLanguage wrapper of the hyperbolic function function connects
+     * the variables/parameters of the function and creates the internal HyperbolicTangentFunction object.
+     * Please read the HyperbolicTangentFunction.h for more info.
      *
      *
      * @copyright Copyright 2009-
      * @author The RevBayes Development Core Team (Sebastian Hoehna)
-     * @since 2014-07-27, version 1.0
+     * @since 2015-01-31, version 1.0
      *
      */
-    class Func_mean :  public TypedFunction<Real> {
+    class Func_hyperbolicTangent :  public TypedFunction<Probability> {
         
     public:
-        Func_mean( void );
+        Func_hyperbolicTangent( void );
         
         // Basic utility functions
-        Func_mean*                                      clone(void) const;                                                              //!< Clone the object
+        Func_hyperbolicTangent*                         clone(void) const;                                                              //!< Clone the object
         static const std::string&                       getClassType(void);                                                             //!< Get Rev type
         static const TypeSpec&                          getClassTypeSpec(void);                                                         //!< Get class type spec
         const TypeSpec&                                 getTypeSpec(void) const;                                                        //!< Get the type spec of the instance
