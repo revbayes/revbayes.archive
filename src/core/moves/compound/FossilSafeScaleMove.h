6046e6df6449343a50b4eee2c4ebaf5ddde1b47b
Michael Landis <mlandis@gmail.com>
2015-02-24 13:25:20 -0800

    Add scale moves for unit-rescaled fossil tree.

---
+++ src/core/moves/compound/FossilSafeScaleMove.h
@@ -0,0 +1,14 @@
+//
+//  FossilSafeScaleMove.h
+//  revbayes-proj
+//
+//  Created by Michael Landis on 2/21/15.
+//  Copyright (c) 2015 Michael Landis. All rights reserved.
+//
+
+#ifndef __revbayes_proj__FossilSafeScaleMove__
+#define __revbayes_proj__FossilSafeScaleMove__
+
+#include <iostream>
+
+#endif /* defined(__revbayes_proj__FossilSafeScaleMove__) */
