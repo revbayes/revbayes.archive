e0b9d911ecf1f097a56610530836cdbc9e011b61
John Huelsenbeck <johnh@berkeley.edu>
2015-04-30 14:40:10 -0700

    Added a read character data function

---
+++ src/revlanguage/functions/io/Func_readCharacterDataUniversal.cpp
@@ -0,0 +1,9 @@
+//
+//  Func_readCharacterData.cpp
+//  RevBayes
+//
+//  Created by John Huelsenbeck on 4/3/15.
+//  Copyright (c) 2015 John Huelsenbeck. All rights reserved.
+//
+
+#include "Func_readCharacterData.h"
---
+++ src/revlanguage/functions/io/Func_readCharacterDataUniversal.h
@@ -0,0 +1,14 @@
+//
+//  Func_readCharacterData.h
+//  RevBayes
+//
+//  Created by John Huelsenbeck on 4/3/15.
+//  Copyright (c) 2015 John Huelsenbeck. All rights reserved.
+//
+
+#ifndef __RevBayes__Func_readCharacterData__
+#define __RevBayes__Func_readCharacterData__
+
+#include <stdio.h>
+
+#endif /* defined(__RevBayes__Func_readCharacterData__) */
