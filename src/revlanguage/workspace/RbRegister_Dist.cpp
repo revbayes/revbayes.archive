f48e2ad948c49573fd08b2742d8d7f8df4677a8d
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2015-05-11 19:13:44 -0700

    Implementing and testing the skyline coalescent.

--- src/revlanguage/workspace/RbRegister_Dist.cpp
+++ src/revlanguage/workspace/RbRegister_Dist.cpp
@@ -113,6 +113,7 @@
 #include "Dist_bdp.h"
 #include "Dist_BirthDeathMultiRate.h"
 #include "Dist_Coalescent.h"
+#include "Dist_CoalescentSkyline.h"
 #include "Dist_constPopMultispCoal.h"
 #include "Dist_divDepYuleProcess.h"
 #include "Dist_empiricalTree.h"
@@ -240,6 +241,9 @@ void RevLanguage::Workspace::initializeDistGlobalWorkspace(void)
         
         // coalescent (constant population sizes)
         addDistribution( "dnCoalescent",                new Dist_Coalescent() );
+        
+        // coalescent (skyline population sizes)
+        addDistribution( "dnCoalescentSkyline",         new Dist_CoalescentSkyline() );
 
         // multispecies coalescent (per branch constant population sizes)
         addDistribution( "dnCoalMultiSpeciesConst",     new Dist_constPopMultispCoal() );
