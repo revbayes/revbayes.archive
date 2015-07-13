6bd4dbe3dc6cc64237a04a0d8d67a86db949101a
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2015-07-13 11:22:48 -0700

    Updating makefiles for windows

--- projects/cmake/build-win.sh
+++ projects/cmake/build-win.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ./regenerate.sh -win true
-cmake . -G "MSYS Makefiles" -DCMAKE_C_COMPILER=GCC -DCMAKE_COMPILER=G++ -DCMAKE_BUILD_TYPE=release
+cmake . -G "Unix Makefiles" -DCMAKE_C_COMPILER=GCC -DCMAKE_CXX_COMPILER=G++ -DCMAKE_BUILD_TYPE=release
 make -j 4
