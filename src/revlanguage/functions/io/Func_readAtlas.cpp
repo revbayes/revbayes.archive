9f5739c96edca50893752d4e1aa4b55997ede6ba
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2015-07-14 13:47:15 -0700

    output formatting.

--- src/revlanguage/functions/io/Func_readAtlas.cpp
+++ src/revlanguage/functions/io/Func_readAtlas.cpp
@@ -23,48 +23,30 @@
 using namespace RevLanguage;
 
 /** Clone object */
-Func_readAtlas* Func_readAtlas::clone( void ) const {
+Func_readAtlas* Func_readAtlas::clone( void ) const
+{
     
     return new Func_readAtlas( *this );
 }
 
 
 /** Execute function */
-RevPtr<RevVariable> Func_readAtlas::execute( void ) {
+RevPtr<RevVariable> Func_readAtlas::execute( void )
+{
     
     // get the information from the arguments for reading the file
     const RlString& fn = static_cast<const RlString&>( args[0].getVariable()->getRevObject() );
     
-    RevBayesCore::TimeAtlasDataReader* tadr = new RevBayesCore::TimeAtlasDataReader(fn.getValue());
+    RevBayesCore::TimeAtlasDataReader* tadr = new RevBayesCore::TimeAtlasDataReader( fn.getValue() );
     RevBayesCore::TimeAtlas* atlas = new RevBayesCore::TimeAtlas(tadr);
 
     return new RevVariable( new RlAtlas(atlas) );
 }
 
 
-/** Format the error exception string for problems specifying the file/path name */
-void Func_readAtlas::formatError(RevBayesCore::RbFileManager& fm, std::string& errorStr) {
-    
-    bool fileNameProvided    = fm.isFileNamePresent();
-    bool isFileNameGood      = fm.testFile();
-    bool isDirectoryNameGood = fm.testDirectory();
-    
-    if ( fileNameProvided == false && isDirectoryNameGood == false )
-        errorStr += "Could not read contents of directory \"" + fm.getFilePath() + "\" because the directory does not exist";
-    else if (fileNameProvided == true && (isFileNameGood == false || isDirectoryNameGood == false)) {
-        errorStr += "Could not read file named \"" + fm.getFileName() + "\" in directory named \"" + fm.getFilePath() + "\" ";
-        if (isFileNameGood == false && isDirectoryNameGood == true)
-            errorStr += "because the file does not exist";
-        else if (isFileNameGood == true && isDirectoryNameGood == false)
-            errorStr += "because the directory does not exist";
-        else
-            errorStr += "because neither the directory nor the file exist";
-    }
-}
-
-
 /** Get argument rules */
-const ArgumentRules& Func_readAtlas::getArgumentRules( void ) const {
+const ArgumentRules& Func_readAtlas::getArgumentRules( void ) const
+{
     
     static ArgumentRules argumentRules = ArgumentRules();
     static bool rulesSet = false;
@@ -82,7 +64,8 @@ const ArgumentRules& Func_readAtlas::getArgumentRules( void ) const {
 
 
 /** Get Rev type of object */
-const std::string& Func_readAtlas::getClassType(void) {
+const std::string& Func_readAtlas::getClassType(void)
+{
     
     static std::string revType = "Func_readAtlas";
     
@@ -90,7 +73,8 @@ const std::string& Func_readAtlas::getClassType(void) {
 }
 
 /** Get class type spec describing type of object */
-const TypeSpec& Func_readAtlas::getClassTypeSpec(void) {
+const TypeSpec& Func_readAtlas::getClassTypeSpec(void)
+{
     
     static TypeSpec revTypeSpec = TypeSpec( getClassType(), new TypeSpec( Function::getClassTypeSpec() ) );
     
@@ -98,7 +82,8 @@ const TypeSpec& Func_readAtlas::getClassTypeSpec(void) {
 }
 
 /** Get type spec */
-const TypeSpec& Func_readAtlas::getTypeSpec( void ) const {
+const TypeSpec& Func_readAtlas::getTypeSpec( void ) const
+{
     
     static TypeSpec typeSpec = getClassTypeSpec();
     
@@ -107,7 +92,8 @@ const TypeSpec& Func_readAtlas::getTypeSpec( void ) const {
 
 
 /** Get return type */
-const TypeSpec& Func_readAtlas::getReturnType( void ) const {
+const TypeSpec& Func_readAtlas::getReturnType( void ) const
+{
     
     static TypeSpec returnTypeSpec = RlAtlas::getClassTypeSpec();
     return returnTypeSpec;
