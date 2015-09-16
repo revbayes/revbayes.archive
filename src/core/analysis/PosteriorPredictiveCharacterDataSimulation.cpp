b62cbf5b783831444268178e29b1a9de84e7835b
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2015-09-16 12:47:46 -0500

    creating classes for the posterior predictive simulation

---
+++ src/core/analysis/PosteriorPredictiveCharacterDataSimulation.cpp
@@ -0,0 +1,9 @@
+//
+//  PosteriorPredictiveCharacterDataSimulation.cpp
+//  RevBayesPrediction
+//
+//  Created by Sebastian Hoehna on 9/16/15.
+//  Copyright (c) 2015 Sebastian Hoehna. All rights reserved.
+//
+
+#include "PosteriorPredictiveCharacterDataSimulation.h"
---
+++ src/core/analysis/PosteriorPredictiveCharacterDataSimulation.h
@@ -0,0 +1,14 @@
+//
+//  PosteriorPredictiveCharacterDataSimulation.h
+//  RevBayesPrediction
+//
+//  Created by Sebastian Hoehna on 9/16/15.
+//  Copyright (c) 2015 Sebastian Hoehna. All rights reserved.
+//
+
+#ifndef __RevBayesPrediction__PosteriorPredictiveCharacterDataSimulation__
+#define __RevBayesPrediction__PosteriorPredictiveCharacterDataSimulation__
+
+#include <stdio.h>
+
+#endif /* defined(__RevBayesPrediction__PosteriorPredictiveCharacterDataSimulation__) */
