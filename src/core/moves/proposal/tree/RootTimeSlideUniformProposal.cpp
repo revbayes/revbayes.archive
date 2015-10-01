4e6c30c7bf0190decc6bdb2f391dcb84e94412a0
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2015-10-01 09:23:53 -0700

    Adding a root-time slide proposal which seems to be necessary for the FBDP and SS-BDP

---
+++ src/core/moves/proposal/tree/RootTimeSlideUniformProposal.cpp
@@ -0,0 +1,9 @@
+//
+//  RootTimeSlideUniformProposal.cpp
+//  RevBayesTree
+//
+//  Created by Sebastian Hoehna on 9/30/15.
+//  Copyright © 2015 Sebastian Hoehna. All rights reserved.
+//
+
+#include "RootTimeSlideUniformProposal.hpp"
---
+++ src/revlanguage/moves/tree/Move_RootTimeSlideUniform.h
@@ -0,0 +1,14 @@
+//
+//  Move_RootTimeSlideUniform.hpp
+//  RevBayesTree
+//
+//  Created by Sebastian Hoehna on 9/30/15.
+//  Copyright © 2015 Sebastian Hoehna. All rights reserved.
+//
+
+#ifndef Move_RootTimeSlideUniform_hpp
+#define Move_RootTimeSlideUniform_hpp
+
+#include <stdio.h>
+
+#endif /* Move_RootTimeSlideUniform_hpp */
