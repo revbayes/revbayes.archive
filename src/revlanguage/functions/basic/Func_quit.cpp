358506b69dc9e5dd125932de82f31cc1bb2a0c13
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2015-11-02 17:24:44 +0100

    more aliases

--- src/revlanguage/functions/basic/Func_quit.cpp
+++ src/revlanguage/functions/basic/Func_quit.cpp
@@ -64,6 +64,21 @@ const TypeSpec& Func_quit::getClassTypeSpec(void)
 
 
 /**
+ * Get the alternative Rev names (aliases) for the constructor function.
+ *
+ * \return Rev aliases of constructor function.
+ */
+std::vector<std::string> Func_quit::getFunctionNameAliases( void ) const
+{
+    // create alternative constructor function names variable that is the same for all instance of this class
+    std::vector<std::string> a_names;
+    a_names.push_back( "q" );
+    
+    return a_names;
+}
+
+
+/**
  * Get the primary Rev name for this function.
  */
 std::string Func_quit::getFunctionName( void ) const
