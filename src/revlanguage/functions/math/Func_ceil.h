8a68ca8f359a5c76ef950e015f3d68f7d338404d
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2015-11-03 16:53:26 +0100

    Another help entry.

--- src/revlanguage/functions/math/Func_ceil.h
+++ src/revlanguage/functions/math/Func_ceil.h
@@ -36,7 +36,17 @@ namespace RevLanguage {
         // Function functions you have to override
         RevBayesCore::TypedFunction< typename retType::valueType>*          createFunction(void) const;                                 //!< Create a function object
         const ArgumentRules&                                                getArgumentRules(void) const;                               //!< Get argument rules
+
+    protected:
         
+        std::vector<std::string>                                            getHelpAuthor(void) const;                                  //!< Get the author(s) of this function
+        std::vector<std::string>                                            getHelpDescription(void) const;                             //!< Get the description for this function
+        std::vector<std::string>                                            getHelpDetails(void) const;                                 //!< Get the more detailed description of the function
+        std::string                                                         getHelpExample(void) const;                                 //!< Get an executable and instructive example
+        std::vector<RevBayesCore::RbHelpReference>                          getHelpReferences(void) const;                              //!< Get some references/citations for this function
+        std::vector<std::string>                                            getHelpSeeAlso(void) const;                                 //!< Get suggested other functions
+        std::string                                                         getHelpTitle(void) const;                                   //!< Get the title of this help entry
+
     };
     
 }
@@ -126,6 +136,116 @@ std::string RevLanguage::Func_ceil<valType, retType>::getFunctionName( void ) co
     std::string f_name = "ceil";
     
     return f_name;
+}
+
+
+
+/**
+ * Get the author(s) of this function so they can receive credit (and blame) for it.
+ */
+template <typename valType, typename retType>
+std::vector<std::string> RevLanguage::Func_ceil<valType, retType>::getHelpAuthor(void) const
+{
+    // create a vector of authors for this function
+    std::vector<std::string> authors;
+    authors.push_back( "Sebastian Hoehna" );
+    
+    return authors;
+}
+
+
+/**
+ * Get the (brief) description for this function
+ */
+template <typename valType, typename retType>
+std::vector<std::string> RevLanguage::Func_ceil<valType, retType>::getHelpDescription(void) const
+{
+    // create a variable for the description of the function
+    std::vector<std::string> descriptions;
+    descriptions.push_back( "The 'ceil' function maps the value of a number to the smallest following integer." );
+    
+    return descriptions;
+}
+
+
+/**
+ * Get the more detailed description of the function
+ */
+template <typename valType, typename retType>
+std::vector<std::string> RevLanguage::Func_ceil<valType, retType>::getHelpDetails(void) const
+{
+    // create a variable for the description of the function
+    std::vector<std::string> details;
+    
+    return details;
+}
+
+
+/**
+ * Get an executable and instructive example.
+ * These example should help the users to show how this function works but
+ * are also used to test if this function still works.
+ */
+template <typename valType, typename retType>
+std::string RevLanguage::Func_ceil<valType, retType>::getHelpExample(void) const
+{
+    // create an example as a single string variable.
+    std::string example = "";
+    example += "# compute the ceiling of a real number\n";
+    example += "number <- 3.4\n";
+    example += "ceiled_number <- ceil(number)\n";
+    example += "if (ceiled_number != 4.0) {\n";
+    example += "    print(\"Problem when computing a ceiled value.\")\n";
+    example += "} else {\n";
+    example += "    print(\"Correct computation of a ceiled value.\")\n";
+    example += "}\n";
+    
+    return example;
+}
+
+
+/**
+ * Get some references/citations for this function
+ *
+ */
+template <typename valType, typename retType>
+std::vector<RevBayesCore::RbHelpReference> RevLanguage::Func_ceil<valType, retType>::getHelpReferences(void) const
+{
+    // create an entry for each reference
+    std::vector<RevBayesCore::RbHelpReference> references;
+    
+    
+    return references;
+}
+
+
+/**
+ * Get the names of similar and suggested other functions
+ */
+template <typename valType, typename retType>
+std::vector<std::string> RevLanguage::Func_ceil<valType, retType>::getHelpSeeAlso(void) const
+{
+    // create an entry for each suggested function
+    std::vector<std::string> see_also;
+    see_also.push_back( "abs" );
+    see_also.push_back( "floor" );
+    see_also.push_back( "round" );
+    
+    
+    return see_also;
+}
+
+
+/**
+ * Get the title of this help entry
+ */
+template <typename valType, typename retType>
+std::string RevLanguage::Func_ceil<valType, retType>::getHelpTitle(void) const
+{
+    // create a title variable
+    std::string title = "Ceiling value of a number";
+    
+    return title;
 }
 
 
