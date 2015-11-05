fdf06c231050c57bf5f4682869f5d5c61a7a1e19
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2015-11-05 13:34:57 +0100

    working on the html help

--- src/revlanguage/functions/math/Func_discretizeDistribution.h
+++ src/revlanguage/functions/math/Func_discretizeDistribution.h
@@ -23,9 +23,20 @@ namespace RevLanguage {
         const TypeSpec&                                                     getTypeSpec(void) const;                                    //!< Get language type of the object
         
         // Regular functions
-        RevBayesCore::TypedFunction< RevBayesCore::RbVector<double> >*      createFunction(void) const;                                                     //!< Create internal function object
+        RevBayesCore::TypedFunction< RevBayesCore::RbVector<double> >*      createFunction(void) const;                                 //!< Create internal function object
         const ArgumentRules&                                                getArgumentRules(void) const;                               //!< Get argument rules
         
+        
+    protected:
+        
+        std::vector<std::string>                                            getHelpAuthor(void) const;                                  //!< Get the author(s) of this function
+        std::vector<std::string>                                            getHelpDescription(void) const;                             //!< Get the description for this function
+        std::vector<std::string>                                            getHelpDetails(void) const;                                 //!< Get the more detailed description of the function
+        std::string                                                         getHelpExample(void) const;                                 //!< Get an executable and instructive example
+        std::vector<RevBayesCore::RbHelpReference>                          getHelpReferences(void) const;                              //!< Get some references/citations for this function
+        std::vector<std::string>                                            getHelpSeeAlso(void) const;                                 //!< Get suggested other functions
+        std::string                                                         getHelpTitle(void) const;                                   //!< Get the title of this help entry
+
     };
     
 }
