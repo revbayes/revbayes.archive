2605e95d60d428d0d2e87fcca7679d1b9314a6fc
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2015-11-23 10:24:07 -0800

    help for mcmc object

--- src/revlanguage/functions/inference/Func_Mcmc.cpp
+++ src/revlanguage/functions/inference/Func_Mcmc.cpp
@@ -117,6 +117,138 @@ std::string Func_Mcmc::getFunctionName( void ) const
 }
 
 
+/**
+ * Get the author(s) of this function so they can receive credit (and blame) for it.
+ */
+std::vector<std::string> Func_Mcmc::getHelpAuthor(void) const
+{
+    // create a vector of authors for this function
+    std::vector<std::string> authors;
+    authors.push_back( "Sebastian Hoehna" );
+    
+    return authors;
+}
+
+
+/**
+ * Get the (brief) description for this function
+ */
+std::vector<std::string> Func_Mcmc::getHelpDescription(void) const
+{
+    // create a variable for the description of the function
+    std::vector<std::string> descriptions;
+    
+    std::string description1 = "";
+    description1 += "The MCMC analysis object keeps a model and the associated moves and monitors.";
+    description1 += "The object is used to run Markov chain Monte Carlo (MCMC) simulation on";
+    description1 += "the model, using the provided moves, to obtain a sample of the posterior probability";
+    description1 += "distribution. During the analysis, the monitors are responsible for sampling model parameters of interest.";
+    descriptions.push_back( description1 );
+    
+    return descriptions;
+}
+
+
+/**
+ * Get the more detailed description of the function
+ */
+std::vector<std::string> Func_Mcmc::getHelpDetails(void) const
+{
+    // create a variable for the description of the function
+    std::vector<std::string> details;
+    
+    std::string details1 = "";
+    details1 += "The MCMC analysis object produced by a call to this function keeps copies of the model";
+    details1 += "and the associated moves and monitors. The MCMC analysis object is used to run Markov";
+    details1 += "chain Monte Carlo (MCMC) simulation on the model, using the provided moves,";
+    details1 += "to obtain a sample of the posterior probability distribution. During the analysis,";
+    details1 += "the monitors are responsible for sampling model parameters of interest.";
+    details.push_back( details1 );
+    
+    
+    return details;
+}
+
+
+/**
+ * Get an executable and instructive example.
+ * These example should help the users to show how this function works but
+ * are also used to test if this function still works.
+ */
+std::string Func_Mcmc::getHelpExample(void) const
+{
+    // create an example as a single string variable.
+    std::string example = "";
+    
+    example += "# Create a simple model (unclamped)\n";
+    example += "a ~ exponential(1)\n";
+    example += "mymodel = model(a)\n";
+    example += "\n";
+    example += "# Create a move vector and a monitor vector\n";
+    example += "moves[1] = mvScale(a, lambda=1.0, weight=1.0)\n";
+    example += "monitors[1] = mnFile(a,\"output/out.log\")\n";
+    example += "\n";
+    example += "# Create an mcmc object\n";
+    example += "mymcmcObject = mcmc( mymodel, monitors, moves)\n";
+    example += "\n";
+    example += "# Run a short analysis\n";
+    example += "mymcmcObject.burnin( generations = 400, tuningInterval = 100)\n";
+    example += "mymcmcObject.run( generations = 400)\n";
+    example += "\n";
+    example += "# print the summary of the operators (now tuned)\n";
+    example += "mymcmcObject.operatorSummary()\n";
+    
+    return example;
+}
+
+
+/**
+ * Get some references/citations for this function
+ *
+ */
+std::vector<RevBayesCore::RbHelpReference> Func_Mcmc::getHelpReferences(void) const
+{
+    // create an entry for each reference
+    std::vector<RevBayesCore::RbHelpReference> references;
+    
+    RevBayesCore::RbHelpReference ref1;
+    ref1.setCitation("Metropolis N, AW Rosenbluth, MN Rosenbluth, AH Teller, E Teller (1953). Equation of state calculations by fast computing machines. Journal of Chemical Physics, 21:1087-1092.");
+    references.push_back( ref1 );
+    
+    RevBayesCore::RbHelpReference ref2;
+    ref2.setCitation("Hastings WK (1970) Monte Carlo sampling methods using Markov chains and their applications. Biometrika, 57:97-109.");
+    references.push_back( ref2 );
+
+    return references;
+}
+
+
+/**
+ * Get the names of similar and suggested other functions
+ */
+std::vector<std::string> Func_Mcmc::getHelpSeeAlso(void) const
+{
+    // create an entry for each suggested function
+    std::vector<std::string> see_also;
+    see_also.push_back( "mcmcmc" );
+    
+    
+    return see_also;
+}
+
+
+/**
+ * Get the title of this help entry
+ */
+std::string Func_Mcmc::getHelpTitle(void) const
+{
+    // create a title variable
+    std::string title = "Create an MCMC analysis object";
+    
+    return title;
+}
+
+
 const TypeSpec& Func_Mcmc::getTypeSpec( void ) const
 {
     
