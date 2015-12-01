159c2697d6a04602abbdc962474a182c66836cfd
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2015-12-01 09:39:09 -0800

    oops

--- src/revlanguage/distributions/math/Dist_gamma.cpp
+++ src/revlanguage/distributions/math/Dist_gamma.cpp
@@ -110,7 +110,7 @@ std::vector<std::string> Dist_gamma::getHelpDetails(void) const
     
     std::string details_1 = "";
     details_1 += "The gamma distribution is the probability of the sum of exponentially distributed variables.";
-    details_1 += "Thus, it provides a natural prior distribution for parameters that could be considered as sums of exponential variables."
+    details_1 += "Thus, it provides a natural prior distribution for parameters that could be considered as sums of exponential variables.";
     
     details.push_back( details_1 );
     
