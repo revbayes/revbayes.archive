fb75ef21e71e723a7161395e61ef8e7be2d94a61
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2015-11-30 17:30:15 -0800

    updating help info

--- src/revlanguage/distributions/math/Dist_gamma.cpp
+++ src/revlanguage/distributions/math/Dist_gamma.cpp
@@ -7,24 +7,28 @@
 
 using namespace RevLanguage;
 
-Dist_gamma::Dist_gamma() : PositiveContinuousDistribution() {
+Dist_gamma::Dist_gamma() : PositiveContinuousDistribution()
+{
     
 }
 
 
-Dist_gamma::~Dist_gamma() {
+Dist_gamma::~Dist_gamma()
+{
     
 }
 
 
 
-Dist_gamma* Dist_gamma::clone( void ) const {
+Dist_gamma* Dist_gamma::clone( void ) const
+{
     
     return new Dist_gamma(*this);
 }
 
 
-RevBayesCore::GammaDistribution* Dist_gamma::createDistribution( void ) const {
+RevBayesCore::GammaDistribution* Dist_gamma::createDistribution( void ) const
+{
     // get the parameters
     RevBayesCore::TypedDagNode<double>* sh  = static_cast<const RealPos &>( shape->getRevObject() ).getDagNode();
     RevBayesCore::TypedDagNode<double>* r   = static_cast<const RealPos &>( rate->getRevObject() ).getDagNode();
@@ -36,7 +40,8 @@ RevBayesCore::GammaDistribution* Dist_gamma::createDistribution( void ) const {
 
 
 /* Get Rev type of object */
-const std::string& Dist_gamma::getClassType(void) { 
+const std::string& Dist_gamma::getClassType(void)
+{
     
     static std::string revType = "Dist_gamma";
     
@@ -44,7 +49,8 @@ const std::string& Dist_gamma::getClassType(void) {
 }
 
 /* Get class type spec describing type of object */
-const TypeSpec& Dist_gamma::getClassTypeSpec(void) { 
+const TypeSpec& Dist_gamma::getClassTypeSpec(void)
+{
     
     static TypeSpec revTypeSpec = TypeSpec( getClassType(), new TypeSpec( PositiveContinuousDistribution::getClassTypeSpec() ) );
     
@@ -65,6 +71,108 @@ std::string Dist_gamma::getDistributionFunctionName( void ) const
     std::string d_name = "gamma";
     
     return d_name;
+}
+
+
+/**
+ * Get the author(s) of this function so they can receive credit (and blame) for it.
+ */
+std::vector<std::string> Dist_gamma::getHelpAuthor(void) const
+{
+    // create a vector of authors for this function
+    std::vector<std::string> authors;
+    authors.push_back( "Sebastian Hoehna" );
+    
+    return authors;
+}
+
+
+/**
+ * Get the (brief) description for this function
+ */
+std::vector<std::string> Dist_gamma::getHelpDescription(void) const
+{
+    // create a variable for the description of the function
+    std::vector<std::string> descriptions;
+    descriptions.push_back( "A Bernoulli-distributed random variable takes the value 1 with probability p and the value 0 with probability 1-p." );
+    
+    return descriptions;
+}
+
+
+/**
+ * Get the more detailed description of the function
+ */
+std::vector<std::string> Dist_gamma::getHelpDetails(void) const
+{
+    // create a variable for the description of the function
+    std::vector<std::string> details;
+    
+    return details;
+}
+
+
+/**
+ * Get an executable and instructive example.
+ * These example should help the users to show how this function works but
+ * are also used to test if this function still works.
+ */
+std::string Dist_gamma::getHelpExample(void) const
+{
+    // create an example as a single string variable.
+    std::string example = "";
+    
+    example += "p ~ dnBeta(1.0,1.0)\n";
+    example += "x ~ dnBernoulli(p)\n";
+    example += "x.clamp(1)\n";
+    example += "moves[1] = mvSlide(p, delta=0.1, weight=1.0)\n";
+    example += "monitors[1] = screenmonitor(printgen=1000, separator = "	", speciation)\n";
+    example += "mymodel = model(p)\n";
+    example += "mymcmc = mcmc(mymodel, monitors, moves)\n";
+    example += "mymcmc.burnin(generations=20000,tuningInterval=100)\n";
+    example += "mymcmc.run(generations=200000)\n";
+    
+    return example;
+}
+
+
+/**
+ * Get some references/citations for this function
+ *
+ */
+std::vector<RevBayesCore::RbHelpReference> Dist_gamma::getHelpReferences(void) const
+{
+    // create an entry for each reference
+    std::vector<RevBayesCore::RbHelpReference> references;
+    
+    
+    return references;
+}
+
+
+/**
+ * Get the names of similar and suggested other functions
+ */
+std::vector<std::string> Dist_gamma::getHelpSeeAlso(void) const
+{
+    // create an entry for each suggested function
+    std::vector<std::string> see_also;
+    see_also.push_back( "dnBinomial" );
+    
+    
+    return see_also;
+}
+
+
+/**
+ * Get the title of this help entry
+ */
+std::string Dist_gamma::getHelpTitle(void) const
+{
+    // create a title variable
+    std::string title = "Bernoulli Distribution";
+    
+    return title;
 }
 
 
--- src/revlanguage/distributions/math/Dist_multivariateNorm.h
+++ src/revlanguage/distributions/math/Dist_multivariateNorm.h
@@ -23,7 +23,7 @@ namespace RevLanguage {
      */
     class Dist_multivariateNorm :  public TypedDistribution<ModelVector<Real> > {
     
-        public:
+    public:
                                                         Dist_multivariateNorm( void );
     
         // Basic utility functions
@@ -43,12 +43,20 @@ namespace RevLanguage {
         // Distribution functions you have to override
         RevBayesCore::MultivariateNormalDistribution*   createDistribution(void) const;
     
-        protected:
+    protected:
     
+        std::vector<std::string>                        getHelpAuthor(void) const;                                                              //!< Get the author(s) of this function
+        std::vector<std::string>                        getHelpDescription(void) const;                                                         //!< Get the description for this function
+        std::vector<std::string>                        getHelpDetails(void) const;                                                             //!< Get the more detailed description of the function
+        std::string                                     getHelpExample(void) const;                                                             //!< Get an executable and instructive example
+        std::vector<RevBayesCore::RbHelpReference>      getHelpReferences(void) const;                                                          //!< Get some references/citations for this function
+        std::vector<std::string>                        getHelpSeeAlso(void) const;                                                             //!< Get suggested other functions
+        std::string                                     getHelpTitle(void) const;                                                               //!< Get the title of this help entry
+
         void                                            setConstParameter(const std::string& name, const RevPtr<const RevVariable> &var);       //!< Set member variable
 
     
-        private:
+    private:
         
         RevPtr<const RevVariable>                       mean;
         RevPtr<const RevVariable>                       covariance;
