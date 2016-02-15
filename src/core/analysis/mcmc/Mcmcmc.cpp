ced31816ea32f5d02a200d0a9b83730f5a8d10da
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2016-02-15 11:44:45 -0800

    More debug output

--- src/core/analysis/mcmc/Mcmcmc.cpp
+++ src/core/analysis/mcmc/Mcmcmc.cpp
@@ -270,11 +270,11 @@ void Mcmcmc::monitor(unsigned long g)
     
     for (size_t i = 0; i < num_chains; ++i)
     {
+        
         if ( chains[i] != NULL && chains[i]->isChainActive() )
         {
             chains[i]->monitor(g);
         }
-        std::cerr << std::endl;
         
     }
     
