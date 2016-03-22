3019b28eb747d0846af45d0801f654c81d0afe26
Michael Landis <mlandis@gmail.com>
2016-03-22 14:55:13 -0400

    clean up and validate FBDP

--- src/core/distributions/phylogenetics/tree/birthdeath/ConstantRateFossilizedBirthDeathProcess.h
+++ src/core/distributions/phylogenetics/tree/birthdeath/ConstantRateFossilizedBirthDeathProcess.h
@@ -32,8 +32,8 @@ namespace RevBayesCore {
         double                                              lnProbNumTaxa(size_t n, double start, double end, bool MRCA) const { throw RbException("Cannot compute P(nTaxa)."); }
         double                                              simulateDivergenceTime(double origin, double present) const;                    //!< Simulate a speciation event.
         double                                              pSurvival(double start, double end) const;                                      //!< Compute the probability of survival of the process (without incomplete taxon sampling).
-		double												pZero(double t) const;
-		double												lnQ(double t) const;
+		double												pZero(double t, double c1, double c2) const;
+		double												lnQ(double t, double c1, double c2) const;
 		double												pHatZero(double t) const;
 		
         // members
