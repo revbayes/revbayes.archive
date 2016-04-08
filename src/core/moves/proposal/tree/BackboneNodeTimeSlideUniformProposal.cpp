17a3ae6601fa3bebdf4d1554a45e20ffc01a4f5d
Michael Landis <mlandis@gmail.com>
2016-04-08 14:11:26 -0400

    clean up function argument mismatch output

---
+++ src/core/moves/proposal/tree/BackboneNodeTimeSlideUniformProposal.cpp
@@ -0,0 +1,9 @@
+//
+//  BackboneNodeTimeSlideUniformProposal.cpp
+//  revbayes-proj
+//
+//  Created by Michael Landis on 4/1/16.
+//  Copyright © 2016 Michael Landis. All rights reserved.
+//
+
+#include "BackboneNodeTimeSlideUniformProposal.h"
