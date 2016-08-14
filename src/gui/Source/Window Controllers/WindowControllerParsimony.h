ab6d1ea18331f3c19b0c52d73cb34b9acd5fc28b
John Huelsenbeck <johnh@berkeley.edu>
2016-08-14 20:14:08 +0200

    The parsimony tool is now working. It uses PAUP* to do the heavy lifting. Next up: help system and cover flow for tree viewing.

--- src/gui/Source/Window Controllers/WindowControllerParsimony.h
+++ src/gui/Source/Window Controllers/WindowControllerParsimony.h
@@ -1,39 +1,99 @@
 #import <Cocoa/Cocoa.h>
 @class ToolParsimony;
 
+#define HEURISTIC   0
+#define BANDB       1
+#define EXHAUSTIVE  2
 
 
 @interface WindowControllerParsimony : NSWindowController {
 
-    IBOutlet NSPopUpButton*         tieBreakerSelector;
-    IBOutlet NSSegmentedControl*    bioNjSelector;
     ToolParsimony*                  myTool;
+    int                             searchMethod;
+    
+    IBOutlet NSTabView*             searchMethodSelectorTab;
+    IBOutlet NSPopUpButton*         hsSwapSelector;
+    IBOutlet NSComboBox*            hsKeepSelector;
+    IBOutlet NSPopUpButton*         hsMulTreesSelector;
+    IBOutlet NSComboBox*            hsRearrLimitSelector;
+    IBOutlet NSComboBox*            hsReconLimitSelector;
+    IBOutlet NSComboBox*            hsNBestSelector;
+    IBOutlet NSTextField*           hsRetainSelector;
+    IBOutlet NSPopUpButton*         hsAllSwapSelector;
+    IBOutlet NSPopUpButton*         hsUseNonMinSelector;
+    IBOutlet NSPopUpButton*         hsSteepestSelector;
+    IBOutlet NSTextField*           hsNChuckSelector;
+    IBOutlet NSComboBox*            hsChuckScoreSelector;
+    IBOutlet NSPopUpButton*         hsAbortRepSelector;
+    IBOutlet NSPopUpButton*         hsRandomizeSelector;
+    IBOutlet NSPopUpButton*         hsAddSeqSelector;
+    IBOutlet NSTextField*           hsNRepsSelector;
+    IBOutlet NSComboBox*            hsHoldSelector;
+    IBOutlet NSComboBox*            bbKeepSelector;
+    IBOutlet NSPopUpButton*         bbMulTreesSelector;
+    IBOutlet NSTextField*           bbUpBoundSelector;
+    IBOutlet NSPopUpButton*         bbAddSeqSelector;
+    IBOutlet NSComboBox*            exKeepSelector;
 
-    /*NSString*                       hsSwap;
-    hsKeep
+    NSString*                       hsSwap;
+    NSString*                       hsKeep;
     NSString*                       hsMulTrees;
-    hsRearrLimit;
-    hsReconLimit;
-    hsNBest;
+    NSString*                       hsRearrLimit;
+    NSString*                       hsReconLimit;
+    NSString*                       hsNBest;
     NSString*                       hsRetain;
     NSString*                       hsAllSwap;
     NSString*                       hsUseNonMin;
     NSString*                       hsSteepest;
     int                             hsNChuck;
-    hsChuckScore;
+    NSString*                       hsChuckScore;
     NSString*                       hsAbortRep;
     NSString*                       hsRandomize;
     NSString*                       hsAddSeq;
     int                             hsNReps;
-    hsHold;
-    bbKeep;
+    NSString*                       hsHold;
+    NSString*                       bbKeep;
     NSString*                       bbMulTrees;
     double                          bbUpBound;
     NSString*                       bbAddSeq;
-    exKeep;*/
+    NSString*                       exKeep;
+
+    NSString*                       old_hsKeep;
+    NSString*                       old_hsRearrLimit;
+    NSString*                       old_hsReconLimit;
+    NSString*                       old_hsNBest;
+    NSString*                       old_hsChuckScore;
+    NSString*                       old_hsHold;
+    NSString*                       old_bbKeep;
+    NSString*                       old_exKeep;
 }
 
+@property (nonatomic)        int       searchMethod;
+@property (nonatomic,strong) NSString* hsSwap;
+@property (nonatomic,strong) NSString* hsKeep;
+@property (nonatomic,strong) NSString* hsMulTrees;
+@property (nonatomic,strong) NSString* hsRearrLimit;
+@property (nonatomic,strong) NSString* hsReconLimit;
+@property (nonatomic,strong) NSString* hsNBest;
+@property (nonatomic,strong) NSString* hsRetain;
+@property (nonatomic,strong) NSString* hsAllSwap;
+@property (nonatomic,strong) NSString* hsUseNonMin;
+@property (nonatomic,strong) NSString* hsSteepest;
+@property (nonatomic)        int       hsNChuck;
+@property (nonatomic,strong) NSString* hsChuckScore;
+@property (nonatomic,strong) NSString* hsAbortRep;
+@property (nonatomic,strong) NSString* hsRandomize;
+@property (nonatomic,strong) NSString* hsAddSeq;
+@property (nonatomic)        int       hsNReps;
+@property (nonatomic,strong) NSString* hsHold;
+@property (nonatomic,strong) NSString* bbKeep;
+@property (nonatomic,strong) NSString* bbMulTrees;
+@property (nonatomic)        double    bbUpBound;
+@property (nonatomic,strong) NSString* bbAddSeq;
+@property (nonatomic,strong) NSString* exKeep;
+
 - (IBAction)cancelButtonAction:(id)sender;
+- (BOOL)checkEntry:(NSString*)val forValidPossibilities:(NSArray*)possibilities;
 - (id)initWithTool:(ToolParsimony*)t;
 - (IBAction)okButtonAction:(id)sender;
 
