dfb90ebf24c88f169e553ee3ef6ef9df9be43997
Lyndon Coghill <lcoghill@lsu.edu>
2016-09-22 10:46:15 -0500

    Added a general empirical quantile function.

--- src/revlanguage/workspace/RbRegister_Func.cpp
+++ src/revlanguage/workspace/RbRegister_Func.cpp
@@ -209,6 +209,7 @@
 #include "Func_ceil.h"
 #include "Func_coala.h"
 #include "Func_diagonalMatrix.h"
+#include "Func_empiricalQuantile.h"
 #include "Func_exp.h"
 #include "Func_floor.h"
 #include "Func_lnProbability.h"
@@ -353,6 +354,9 @@ void RevLanguage::Workspace::initializeFuncGlobalWorkspace(void)
 
         // diagonal matrix
         addFunction( new Func_diagonalMatrix() );
+        
+        // empirical quantile function
+        addFunction( new Func_empiricalQuantile()  );
 
         // exponential function
         addFunction( new Func_exp() );
