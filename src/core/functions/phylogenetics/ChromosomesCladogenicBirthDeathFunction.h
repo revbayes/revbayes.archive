2762185ab352fab3a497e49d6ca668cca68cd27e
wf <willfreyman@gmail.com>
2016-10-03 23:45:33 -0700

    correctly updating event map rates in the SSE class

--- src/core/functions/phylogenetics/ChromosomesCladogenicBirthDeathFunction.h
+++ src/core/functions/phylogenetics/ChromosomesCladogenicBirthDeathFunction.h
@@ -25,33 +25,33 @@ namespace RevBayesCore {
         ChromosomesCladogenicBirthDeathFunction( const TypedDagNode< RbVector< double > >* sr, unsigned mc );
         virtual                                            ~ChromosomesCladogenicBirthDeathFunction(void);
         
-        const static unsigned NO_CHANGE                     = 0;         // N -> N
-        const static unsigned FISSION                       = 1;         // N -> N + 1
-        const static unsigned FUSION                        = 2;         // N -> N - 1
-        const static unsigned POLYPLOIDIZATION              = 3;         // N -> N * 2
-        const static unsigned DEMIPOLYPLOIDIZATION          = 4;         // N -> N * 1.5
+        const static unsigned NO_CHANGE                             = 0;         // N -> N
+        const static unsigned FISSION                               = 1;         // N -> N + 1
+        const static unsigned FUSION                                = 2;         // N -> N - 1
+        const static unsigned POLYPLOIDIZATION                      = 3;         // N -> N * 2
+        const static unsigned DEMIPOLYPLOIDIZATION                  = 4;         // N -> N * 1.5
         
         // public member functions
-        ChromosomesCladogenicBirthDeathFunction*            clone(void) const;
-        const std::map< std::vector<unsigned>, double >&    getEventMap(void) const;
-        void                                                update(void);
+        ChromosomesCladogenicBirthDeathFunction*                    clone(void) const;
+        const std::map< std::vector<unsigned>, double >&            getEventMap(void) const;
+        void                                                        update(void);
         
     protected:
         
-        void                                                swapParameterInternal(const DagNode *oldP, const DagNode *newP);
+        void                                                        swapParameterInternal(const DagNode *oldP, const DagNode *newP);
         
     private:
         
-        void                                                buildEventMap(void);
-        void                                                updateSpeciationRates(void);
+        void                                                        buildEventMap(void);
+        void                                                        updateSpeciationRates(void);
         
         // members
-        const TypedDagNode< RbVector<double> >*             speciationRates;
-        unsigned                                            maxChromo;
-        unsigned                                            numEventTypes;
-        std::map< std::vector<unsigned>, unsigned >         eventMapTypes;
-        std::map< std::vector<unsigned>, double >           eventMap;
-        std::vector< std::vector<unsigned> >                eventMapCounts;
+        const TypedDagNode< RbVector<double> >*                     speciationRates;
+        unsigned                                                    maxChromo;
+        unsigned                                                    numEventTypes;
+        std::map< std::vector<unsigned>, std::vector<unsigned> >    eventMapTypes;
+        std::map< std::vector<unsigned>, double >                   eventMap;
+        std::vector< std::vector<unsigned> >                        eventMapCounts;
         
     };
     
