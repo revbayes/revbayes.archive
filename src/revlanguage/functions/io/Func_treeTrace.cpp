761fd16b10fc8a2f3d9a91656ce868c5b249c1ba
Walker Pett <will.pett@gmail.com>
2016-12-19 15:11:00 -0600

    Allow generic Tree type with Func_treeTrace

--- src/revlanguage/functions/io/Func_treeTrace.cpp
+++ src/revlanguage/functions/io/Func_treeTrace.cpp
@@ -50,9 +50,18 @@ RevPtr<RevVariable> Func_treeTrace::execute( void )
             t.addObject( new RevBayesCore::Tree( trees[i] ) );
         }
     }
-    else
+    else if ( args[0].getVariable()->getRevObject().isType( ModelVector<BranchLengthTree>::getClassTypeSpec() ) )
     {
         const ModelVector<RevLanguage::BranchLengthTree>& trees = static_cast<const ModelVector<RevLanguage::BranchLengthTree>&>( args[0].getVariable()->getRevObject() );
+
+        for (size_t i = 0; i < trees.size(); ++i)
+        {
+            t.addObject( new RevBayesCore::Tree( trees[i] ) );
+        }
+    }
+    else if ( args[0].getVariable()->getRevObject().isType( ModelVector<Tree>::getClassTypeSpec() ) )
+    {
+        const ModelVector<RevLanguage::Tree>& trees = static_cast<const ModelVector<RevLanguage::Tree>&>( args[0].getVariable()->getRevObject() );
 
         for (size_t i = 0; i < trees.size(); ++i)
         {
@@ -78,6 +87,7 @@ const ArgumentRules& Func_treeTrace::getArgumentRules( void ) const
     if (!rules_set)
     {
         std::vector<TypeSpec> treeTypes;
+        treeTypes.push_back( ModelVector<Tree>::getClassTypeSpec() );
         treeTypes.push_back( ModelVector<TimeTree>::getClassTypeSpec() );
         treeTypes.push_back( ModelVector<BranchLengthTree>::getClassTypeSpec() );
         argumentRules.push_back( new ArgumentRule( "trees", treeTypes, "Vector of trees.", ArgumentRule::BY_VALUE, ArgumentRule::ANY ) );
