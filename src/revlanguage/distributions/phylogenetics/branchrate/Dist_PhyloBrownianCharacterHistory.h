34d598d209b1bd9e9d40b269ba4c8b0c7a1c18e7
Michael R May <mikeryanmay@gmail.com>
2017-02-06 14:10:08 -0800

    working on data-augmentation for continuous-traits

---
+++ src/revlanguage/distributions/phylogenetics/branchrate/Dist_PhyloBrownianCharacterHistory.h
@@ -0,0 +1,47 @@
+#ifndef Dist_PhyloBrownianREML_H
+#define Dist_PhyloBrownianREML_H
+
+#include "ContinuousCharacterData.h"
+#include "RlContinuousCharacterData.h"
+#include "RlTypedDistribution.h"
+#include "Tree.h"
+
+namespace RevLanguage {
+    
+    class Dist_PhyloBrownianREML :  public TypedDistribution< ContinuousCharacterData > {
+        
+    public:
+        Dist_PhyloBrownianREML( void );
+        virtual ~Dist_PhyloBrownianREML();
+        
+        // Basic utility functions
+        Dist_PhyloBrownianREML*                         clone(void) const;                                                                      //!< Clone the object
+        static const std::string&                       getClassType(void);                                                                     //!< Get Rev type
+        static const TypeSpec&                          getClassTypeSpec(void);                                                                 //!< Get class type spec
+        std::string                                     getDistributionFunctionName(void) const;                                                //!< Get the Rev-name for this distribution.
+        const TypeSpec&                                 getTypeSpec(void) const;                                                                //!< Get the type spec of the instance
+        const MemberRules&                              getParameterRules(void) const;                                                          //!< Get member rules (const)
+        void                                            printValue(std::ostream& o) const;                                                      //!< Print the general information on the function ('usage')
+        
+        
+        // Distribution functions you have to override
+        RevBayesCore::TypedDistribution< RevBayesCore::ContinuousCharacterData >*      createDistribution(void) const;
+        
+    protected:
+        
+        void                                            setConstParameter(const std::string& name, const RevPtr<const RevVariable> &var);       //!< Set member variable
+        
+        
+    private:
+        
+        RevPtr<const RevVariable>                       tree;
+        RevPtr<const RevVariable>                       branchRates;
+        RevPtr<const RevVariable>                       site_rates;
+        RevPtr<const RevVariable>                       nSites;
+        
+        
+    };
+    
+}
+
+#endif
