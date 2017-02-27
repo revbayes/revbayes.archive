d9528179764adfb47d84919f23e5ed916c4cabb2
Michael R May <mikeryanmay@gmail.com>
2017-02-27 09:56:00 -0800

    merging character_history and mvbm

--- src/revlanguage/utils/GitVersion.cpp
+++ src/revlanguage/utils/GitVersion.cpp
@@ -1,4 +1,4 @@
 #include "GitVersion.h"
-const char *build_git_sha = "038c63";
-const char *build_date = "Thu Feb 23 20:34:30 PST 2017";
+const char *build_git_sha = "7b7a66";
+const char *build_date = "Sun Feb 26 17:05:22 PST 2017";
 const char *build_git_branch = "character_history";
