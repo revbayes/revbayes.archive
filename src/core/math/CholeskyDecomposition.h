b93d6248346b8248122bd2a46faf0525336a1cca
Michael R May <mikeryanmay@gmail.com>
2017-04-11 15:10:00 -0700

    including mvbm

---
+++ src/core/math/CholeskyDecomposition.h
@@ -0,0 +1,35 @@
+#ifndef CholeskyDecomposition_H
+#define CholeskyDecomposition_H
+
+#include "MatrixComplex.h"
+#include "MatrixReal.h"
+
+#include <complex>
+
+
+namespace RevBayesCore {
+
+    class CholeskyDecomposition {
+
+    public:
+                                                CholeskyDecomposition(const MatrixReal* m);
+
+        void                                    update(void);
+        const MatrixReal                        getInverse(void) const { return inverseMatrix; }
+
+    private:
+
+        void                                    computeInverse(void);
+        void                                    decomposeMatrix(void);
+        
+        size_t                                  n;                                              //!< Row and column dimension (square matrix)
+        const MatrixReal*                       qPtr;                                           //!< A pointer to the matrix for this cholesky decomposition
+        MatrixReal                              L;
+        MatrixReal                              inverseMatrix;
+        bool                                    isSemiPositiveDefinite;
+
+    };
+    
+}
+
+#endif
