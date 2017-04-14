d04b1812ca7ca743f47199f80d2b3ff75825ccce
Andy Magee <afmagee@uw.edu>
2017-04-14 11:02:27 -0700

    Adding halfCauchy and halfNormal distributions.

--- src/revlanguage/utils/GitVersion.cpp
+++ src/revlanguage/utils/GitVersion.cpp
@@ -1,4 +1,4 @@
 #include "GitVersion.h"
-const char *build_git_sha = "bb89f9";
-const char *build_date = "Wed Mar 29 13:10:17 PDT 2017";
-const char *build_git_branch = "shrinkage_priors";
+const char *build_git_sha = "3addf79";
+const char *build_date = "Fri Apr 14 09:52:18 PDT 2017";
+const char *build_git_branch = "development";
