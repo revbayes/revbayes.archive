f1aab00979184a5f2b3f1a63d500009d8f3fff8d
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2017-07-23 08:11:36 +0200

    Adding the ability to compute a distance matrix from an alignment

--- src/revlanguage/workspace/RbRegister_Dist.cpp
+++ src/revlanguage/workspace/RbRegister_Dist.cpp
@@ -301,7 +301,7 @@ void RevLanguage::Workspace::initializeDistGlobalWorkspace(void)
 		AddDistribution< Tree                       >( new Dist_empiricalTree() );
 		
 		// Distance Matrix Gamma distribution
-		AddDistribution< RlDistanceMatrix           >( new Dist_phyloDistanceGamma() );
+		AddDistribution< DistanceMatrix             >( new Dist_phyloDistanceGamma() );
 
 		
         /* Statistical distributions on simple variables (in folder "distributions/math") */
