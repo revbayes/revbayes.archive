a3bcc7296ecc68b0dbd45fda509cc590ab94fe91
boussau <boussau@gmail.com>
2017-08-01 23:16:20 +0200

    Adding help information and example code for Dist_multispeciesCoalescentInverseGammaPrior.

--- src/revlanguage/distributions/phylogenetics/tree/Dist_multispeciesCoalescentInverseGammaPrior.cpp
+++ src/revlanguage/distributions/phylogenetics/tree/Dist_multispeciesCoalescentInverseGammaPrior.cpp
@@ -194,3 +194,161 @@ const TypeSpec& Dist_multispeciesCoalescentInverseGammaPrior::getTypeSpec( void 
 
     return type_spec;
 }
+
+
+/**
+ * Get the author(s) of this function so they can receive credit (and blame) for it.
+ */
+std::vector<std::string> Dist_multispeciesCoalescentInverseGammaPrior::getHelpAuthor(void) const
+{
+    // create a vector of authors for this function
+    std::vector<std::string> authors;
+    authors.push_back( "Sebastian Hoehna, Bastien Boussau" );
+
+    return authors;
+}
+
+
+/**
+ * Get the (brief) description for this function
+ */
+std::vector<std::string> Dist_multispeciesCoalescentInverseGammaPrior::getHelpDescription(void) const
+{
+    // create a variable for the description of the function
+    std::vector<std::string> descriptions;
+    descriptions.push_back( "Multispecies coalescent distribution describing how gene trees can be generated from within a species tree given effective population sizes. Requires an ultrametric species tree, parameters of an inverse gamma prior on effective population sizes, and taxa with species and individual names." );
+
+    return descriptions;
+}
+
+
+/**
+ * Get the more detailed description of the function
+ */
+std::vector<std::string> Dist_multispeciesCoalescentInverseGammaPrior::getHelpDetails(void) const
+{
+    // create a variable for the description of the function
+    std::vector<std::string> details;
+
+    std::string details_1 = "";
+    details_1 += "The species tree must be ultrametric.";
+
+    details.push_back( details_1 );
+
+    std::string details_2 = "";
+    details_2 += "Parameters of an inverse gamma prior on effective population sizes must be provided.";
+
+    details.push_back( details_2 );
+
+    std::string details_3 = "";
+    details_3 += "This distribution uses a conjugate prior on effective population sizes.";
+
+    details.push_back( details_3 );
+
+    std::string details_4 = "";
+    details_4 += "As a consequence, effective population sizes are integrated out and treated as nuisance parameters.";
+
+    details.push_back( details_4 );
+
+    std::string details_5 = "";
+    details_5 += "If you are interested in reconstructing ancestral effective population sizes, use dnMultiSpeciesCoalescent.";
+
+    details.push_back( details_5 );
+
+
+
+    return details;
+}
+
+
+/**
+ * Get an executable and instructive example.
+ * These examples should help the users to show how this function works but
+ * are also used to test if this function still works.
+ */
+std::string Dist_multispeciesCoalescentInverseGammaPrior::getHelpExample(void) const
+{
+    // create an example as a single string variable.
+    std::string example = "";
+    example += "# We are going to save the trees we simulate in the folder simulatedTrees:\n";
+    example += "dataFolder = \"simulatedTrees\" \n";
+    example += "# Let’s simulate a species tree with 10 taxa, 2 gene trees, 3 alleles per species:\n";
+    example += "n_species <- 10\n";
+    example += "n_genes <- 2\n";
+    example += "n_alleles <- 3\n";
+    example += "# we simulate an ultrametric species tree:\n";
+    example += "# Species names:\n";
+    example += "for (i in 1:n_species) {\n";
+    example += "        species[i] <- taxon(taxonName=\"Species_\"+i, speciesName=\"Species_\"+i)\n";
+    example += "}\n";
+    example += "spTree ~ dnBirthDeath(lambda=0.3, mu=0.2, rootAge=10, rho=1, samplingStrategy=\"uniform\", condition=\"nTaxa\", taxa=species)\n";
+    example += "print(spTree)\n";
+    example += "# let's pick constant parameters for the inverse gamma distribution:\n";
+    example += "alpha <- 3\n";
+    example += "beta <- 0.003\n";
+    example += "# let's simulate gene trees now:\n";
+    example += "# taxa names:\n";
+    example += "for (g in 1:n_genes) {\n";
+    example += "  for (i in 1:n_species) {\n";
+    example += "    for (j in 1:n_alleles) {\n";
+    example += "        taxons[g][(i-1)*n_alleles+j] <- taxon(taxonName=\"Species_\"+i+\"_\"+j, speciesName=\"Species_\"+i)\n";
+    example += "    }\n";
+    example += "  }\n";
+    example += "  geneTrees[g] ~ dnMultiSpeciesCoalescentInverseGamma(speciesTree=spTree, shape=alpha, rate=beta, taxa=taxons[g])\n";
+    example += "  print(geneTrees[g])\n";
+    example += "}\n";
+    example += "# We can save the species tree and the gene trees: \n";
+    example += "write(speciesTree, filename=dataFolder+\"speciesTree\")\n";
+    example += "# Saving the gene trees\n";
+    example += "for (i in 1:(n_genes)) {\n";
+    example += "  write(geneTrees[i], filename=dataFolder+\"geneTree_\"+i+\".tree\")\n";
+    example += "}\n";
+    example += "\n";
+
+    return example;
+}
+
+
+/**
+ * Get some references/citations for this function
+ *
+ */
+std::vector<RevBayesCore::RbHelpReference> Dist_multispeciesCoalescentInverseGammaPrior::getHelpReferences(void) const
+{
+    // create an entry for each reference
+    std::vector<RevBayesCore::RbHelpReference> references;
+    RevBayesCore::RbHelpReference ref = RevBayesCore::RbHelpReference();
+    ref.setCitation(" Algorithmic improvements to species delimitation and phylogeny estimation under the multispecies coalescent. Jones G.  Journal of Mathematical Biology. 2016.");
+    ref.setDoi("DOI: 10.1007/s00285-016-1034-0");
+    ref.setUrl("http://www.indriid.com/2016/2016-06-01-STACEY.pdf");
+
+    references.push_back(ref);
+
+    return references;
+}
+
+
+/**
+ * Get the names of similar and suggested other functions
+ */
+std::vector<std::string> Dist_multispeciesCoalescentInverseGammaPrior::getHelpSeeAlso(void) const
+{
+    // create an entry for each suggested function
+    std::vector<std::string> see_also;
+    see_also.push_back( "dnMultiSpeciesCoalescent" );
+
+
+    return see_also;
+}
+
+
+/**
+ * Get the title of this help entry
+ */
+std::string Dist_multispeciesCoalescentInverseGammaPrior::getHelpTitle(void) const
+{
+    // create a title variable
+    std::string title = "Multispecies coalescent Distribution with inverse gamma prior on effective population sizes";
+
+    return title;
+}
