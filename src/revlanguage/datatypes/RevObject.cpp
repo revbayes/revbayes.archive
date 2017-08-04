b7d1f4433d164d52b069d2284e5d6c8786f860b2
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2017-08-04 15:06:25 +0200

    Enabling to see example code from the help

--- src/revlanguage/datatypes/RevObject.cpp
+++ src/revlanguage/datatypes/RevObject.cpp
@@ -357,6 +357,12 @@ RevBayesCore::RbHelpEntry* RevObject::getHelpEntry( void ) const
     // description
     helpEntry.setDescription( getHelpDescription() );
     
+    // details
+    helpEntry.setDetails( getHelpDetails() );
+    
+    // example
+    helpEntry.setExample( getHelpExample() );
+    
     helpEntry.setReferences( getHelpReferences() );
     
     // author
