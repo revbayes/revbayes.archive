86e193c0ccba2ea638ff12e22de28b7c88e87c8d
boussau <boussau@gmail.com>
2017-08-12 02:54:52 +0200

    Added the possibility to read one or several Newick strings into one or several trees.

--- src/revlanguage/functions/io/Func_readTrees.cpp
+++ src/revlanguage/functions/io/Func_readTrees.cpp
@@ -4,6 +4,7 @@
 #include "Func_readTrees.h"
 #include "ModelVector.h"
 #include "NclReader.h"
+#include "NewickConverter.h"
 #include "RbException.h"
 #include "RbFileManager.h"
 #include "RevNullObject.h"
@@ -30,7 +31,7 @@ using namespace RevLanguage;
  */
 Func_readTrees* Func_readTrees::clone( void ) const
 {
-    
+
     return new Func_readTrees( *this );
 }
 
@@ -38,38 +39,58 @@ Func_readTrees* Func_readTrees::clone( void ) const
 /** Execute function */
 RevPtr<RevVariable> Func_readTrees::execute( void )
 {
-    
+
     // get the information from the arguments for reading the file
-    const RlString& fn = static_cast<const RlString&>( args[0].getVariable()->getRevObject() );
-    
-    // get the global instance of the NCL reader and clear warnings from its warnings buffer
-    RevBayesCore::NclReader reader = RevBayesCore::NclReader();
-    
+    const std::string&  fn = static_cast<const RlString&>( args[0].getVariable()->getRevObject() ).getValue();
+    const std::string&  text = static_cast<const RlString&>( args[1].getVariable()->getRevObject() ).getValue();
+
     ModelVector<TimeTree> *trees = new ModelVector<TimeTree>();
-    std::vector<RevBayesCore::Tree*> tmp = reader.readTimeTrees( fn.getValue() );
-    for (std::vector<RevBayesCore::Tree*>::iterator t = tmp.begin(); t != tmp.end(); ++t) 
-    {
-        trees->push_back( TimeTree(*t) );
+
+    if (fn != "") {
+      // get the global instance of the NCL reader and clear warnings from its warnings buffer
+      RevBayesCore::NclReader reader = RevBayesCore::NclReader();
+
+      std::vector<RevBayesCore::Tree*> tmp = reader.readTimeTrees( fn );
+      for (std::vector<RevBayesCore::Tree*>::iterator t = tmp.begin(); t != tmp.end(); ++t)
+      {
+          trees->push_back( TimeTree(*t) );
+      }
     }
-    
+
+    if (text != "") {
+
+      string aux;
+      std::istringstream iss(text);
+      RevBayesCore::NewickConverter c;
+
+      while (std::getline(iss, aux)) {
+        RevBayesCore::Tree *blTree = c.convertFromNewick( aux );
+        trees->push_back( TimeTree(*blTree) );
+
+      }
+    }
+
+
     return new RevVariable( trees );
+
 }
 
 
 /** Get argument rules */
 const ArgumentRules& Func_readTrees::getArgumentRules( void ) const
 {
-    
+
     static ArgumentRules argumentRules = ArgumentRules();
     static bool rules_set = false;
-    
+
     if (!rules_set)
     {
-        
-        argumentRules.push_back( new ArgumentRule( "file", RlString::getClassTypeSpec(), "The name of the file containing the trees.", ArgumentRule::BY_VALUE, ArgumentRule::ANY ) );
+
+        argumentRules.push_back( new ArgumentRule( "file", RlString::getClassTypeSpec(), "The name of the file containing the trees.", ArgumentRule::BY_VALUE, ArgumentRule::ANY, new RlString("") ) );
+        argumentRules.push_back( new ArgumentRule( "text", RlString::getClassTypeSpec(), "A string containing one or several newick trees, separated by end of lines.", ArgumentRule::BY_VALUE, ArgumentRule::ANY, new RlString("") ) );
         rules_set = true;
     }
-    
+
     return argumentRules;
 }
 
@@ -77,20 +98,20 @@ const ArgumentRules& Func_readTrees::getArgumentRules( void ) const
 /** Get Rev type of object */
 const std::string& Func_readTrees::getClassType(void)
 {
-    
+
     static std::string rev_type = "Func_readTrees";
-    
-	return rev_type; 
+
+	return rev_type;
 }
 
 
 /** Get class type spec describing type of object */
 const TypeSpec& Func_readTrees::getClassTypeSpec(void)
 {
-    
+
     static TypeSpec rev_type_spec = TypeSpec( getClassType(), new TypeSpec( Function::getClassTypeSpec() ) );
-    
-	return rev_type_spec; 
+
+	return rev_type_spec;
 }
 
 
@@ -101,7 +122,7 @@ std::string Func_readTrees::getFunctionName( void ) const
 {
     // create a name variable that is the same for all instance of this class
     std::string f_name = "readTrees";
-    
+
     return f_name;
 }
 
@@ -109,9 +130,9 @@ std::string Func_readTrees::getFunctionName( void ) const
 /** Get type spec */
 const TypeSpec& Func_readTrees::getTypeSpec( void ) const
 {
-    
+
     static TypeSpec type_spec = getClassTypeSpec();
-    
+
     return type_spec;
 }
 
@@ -119,11 +140,7 @@ const TypeSpec& Func_readTrees::getTypeSpec( void ) const
 /** Get return type */
 const TypeSpec& Func_readTrees::getReturnType( void ) const
 {
-    
+
     static TypeSpec returnTypeSpec = ModelVector<TimeTree>::getClassTypeSpec();
     return returnTypeSpec;
 }
-
-
-
-
