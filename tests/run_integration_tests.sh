b239a79e968fb9e5e167388e9f1fcf6ae39eb8b2
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2017-08-16 16:32:29 +0200

    Adding the first integration test output

--- tests/run_integration_tests.sh
+++ tests/run_integration_tests.sh
@@ -1,2 +1,16 @@
-echo "Number of wrong results in basic test:" > "Test_basic_results.txt"
-diff "Expected_results_basic.txt" "Test_basic.txt" | wc -l >> "Test_basic_results.txt"
+#!/bin/bash
+
+for f in scripts/*; do
+    rb $f > output_tmp.txt
+done
+
+rm output_tmp.txt
+
+for f in $(ls output); do
+    echo "$f"
+    res=$(diff <(tail -n +3 output/$f) <(tail -n +3 output_expected/$f) | wc -l)
+    if [ $res -gt 0 ]; then
+        exit 113
+    fi
+done
+#diff "Expected_results_basic.txt" "Test_basic.txt" | wc -l >> "Test_basic_results.txt"
