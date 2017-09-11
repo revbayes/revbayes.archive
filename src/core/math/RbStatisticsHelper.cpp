0383b007f013e112ad54e28fb4d12318b1493542
Michael R May <mikeryanmay@gmail.com>
2017-09-11 14:04:55 -0700

    stabilizing gamma random number generator

--- src/core/math/RbStatisticsHelper.cpp
+++ src/core/math/RbStatisticsHelper.cpp
@@ -306,7 +306,13 @@ int RbStatistics::Helper::poissonRatioUniforms(double lambda, RandomNumberGenera
  * \return Returns a gamma-distributed random variable. 
  * \throws Does not throw an error.
  */
-double RbStatistics::Helper::rndGamma(double a, RandomNumberGenerator& rng) {
+double RbStatistics::Helper::rndGamma(double s, RandomNumberGenerator& rng) {
+
+//    double r = rndGamma1(s, rng); // this algorithm is broken
+//    double r = rndGamma2(s, rng); // this algorithm is broken
+//    double r = rndGamma3(s, rng); // this algorithm is unstable for a < 1
+    double r = rndGamma4(s, rng);
+    return (r);
     
 //	double r = 0.0;
 //	if (s <= 0.0)      
@@ -318,6 +324,250 @@ double RbStatistics::Helper::rndGamma(double a, RandomNumberGenerator& rng) {
 //	else           
 //		r = - log(rng.uniform01());
 //	return (r);
+    
+//    // Adapted code from R (rgamma)
+//    /* Constants : */
+//    const static double sqrt32 = 5.656854;
+//    const static double exp_m1 = 0.36787944117144232159;/* exp(-1) = 1/e */
+//    
+//    /* Coefficients q[k] - for q0 = sum(q[k]*a^(-k))
+//     * Coefficients a[k] - for q = q0+(t*t/2)*sum(a[k]*v^k)
+//     * Coefficients e[k] - for exp(q)-1 = sum(e[k]*q^k)
+//     */
+//    const static double q1 = 0.04166669;
+//    const static double q2 = 0.02083148;
+//    const static double q3 = 0.00801191;
+//    const static double q4 = 0.00144121;
+//    const static double q5 = -7.388e-5;
+//    const static double q6 = 2.4511e-4;
+//    const static double q7 = 2.424e-4;
+//    
+//    const static double a1 = 0.3333333;
+//    const static double a2 = -0.250003;
+//    const static double a3 = 0.2000062;
+//    const static double a4 = -0.1662921;
+//    const static double a5 = 0.1423657;
+//    const static double a6 = -0.1367177;
+//    const static double a7 = 0.1233795;
+//    
+//    /* State variables [FIXME for threading!] :*/
+//    static double aa = 0.;
+//    static double aaa = 0.;
+//    static double s, s2, d;    /* no. 1 (step 1) */
+//    static double q0, b, si, c;/* no. 2 (step 4) */
+//    
+//    double e, p, q, r, t, u, v, w, x, ret_val;
+//    
+//    if (!RbMath::isFinite(a) || a < 0.0 ) {
+//        throw RbException("Infinite parameters for rgamma.");
+//    }
+//    
+//    if (a < 1.) { /* GS algorithm for parameters a < 1 */
+//        if(a == 0)
+//            return 0.;
+//        e = 1.0 + exp_m1 * a;
+//        while ( true ) {
+//            p = e * rng.uniform01();
+//            if (p >= 1.0) {
+//                x = -log((e - p) / a);
+//                if (exp( rng.uniform01() ) >= (1.0 - a) * log(x))
+//                    break;
+//            } else {
+//                x = exp(log(p) / a);
+//                if (exp( rng.uniform01() ) >= x)
+//                    break;
+//            }
+//        }
+//        return x;
+//    }
+//    
+//    /* --- a >= 1 : GD algorithm --- */
+//    
+//    /* Step 1: Recalculations of s2, s, d if a has changed */
+//    if (a != aa) {
+//        aa = a;
+//        s2 = a - 0.5;
+//        s = sqrt(s2);
+//        d = sqrt32 - s * 12.0;
+//    }
+//    /* Step 2: t = standard normal deviate,
+//     x = (s,1/2) -normal deviate. */
+//    
+//    /* immediate acceptance (i) */
+//    t = RbStatistics::Normal::rv(rng);
+//    x = s + 0.5 * t;
+//    ret_val = x * x;
+//    if (t >= 0.0)
+//        return ret_val;
+//    
+//    /* Step 3: u = 0,1 - uniform sample. squeeze acceptance (s) */
+//    u = rng.uniform01();
+//    if (d * u <= t * t * t)
+//        return ret_val;
+//    
+//    /* Step 4: recalculations of q0, b, si, c if necessary */
+//    
+//    if (a != aaa) {
+//        aaa = a;
+//        r = 1.0 / a;
+//        q0 = ((((((q7 * r + q6) * r + q5) * r + q4) * r + q3) * r
+//               + q2) * r + q1) * r;
+//        
+//        /* Approximation depending on size of parameter a */
+//        /* The constants in the expressions for b, si and c */
+//        /* were established by numerical experiments */
+//        
+//        if (a <= 3.686) {
+//            b = 0.463 + s + 0.178 * s2;
+//            si = 1.235;
+//            c = 0.195 / s - 0.079 + 0.16 * s;
+//        } else if (a <= 13.022) {
+//            b = 1.654 + 0.0076 * s2;
+//            si = 1.68 / s + 0.275;
+//            c = 0.062 / s + 0.024;
+//        } else {
+//            b = 1.77;
+//            si = 0.75;
+//            c = 0.1515 / s;
+//        }
+//    }
+//    /* Step 5: no quotient test if x not positive */
+//    
+//    if (x > 0.0) {
+//        /* Step 6: calculation of v and quotient q */
+//        v = t / (s + s);
+//        if (fabs(v) <= 0.25)
+//            q = q0 + 0.5 * t * t * ((((((a7 * v + a6) * v + a5) * v + a4) * v
+//                                      + a3) * v + a2) * v + a1) * v;
+//        else
+//            q = q0 - s * t + 0.25 * t * t + (s2 + s2) * log(1.0 + v);
+//        
+//        
+//        /* Step 7: quotient acceptance (q) */
+//        if (log(1.0 - u) <= q)
+//            return ret_val;
+//    }
+//    
+//    while (true) {
+//        /* Step 8: e = standard exponential deviate
+//         *	u =  0,1 -uniform deviate
+//         *	t = (b,si)-double exponential (laplace) sample */
+//        e = exp( rng.uniform01() );
+//        u = rng.uniform01();
+//        u = u + u - 1.0;
+//        if (u < 0.0)
+//            t = b - si * e;
+//        else
+//            t = b + si * e;
+//        /* Step	 9:  rejection if t < tau(1) = -0.71874483771719 */
+//        if (t >= -0.71874483771719) {
+//            /* Step 10:	 calculation of v and quotient q */
+//            v = t / (s + s);
+//            if (fabs(v) <= 0.25)
+//                q = q0 + 0.5 * t * t *
+//                ((((((a7 * v + a6) * v + a5) * v + a4) * v + a3) * v
+//                  + a2) * v + a1) * v;
+//            else
+//                q = q0 - s * t + 0.25 * t * t + (s2 + s2) * log(1.0 + v);
+//            /* Step 11:	 hat acceptance (h) */
+//            /* (if q not positive go to step 8) */
+//            if (q > 0.0) {
+//                w = expm1(q);
+//                /*  ^^^^^ original code had approximation with rel.err < 2e-7 */
+//                /* if t is rejected sample again at step 8 */
+//                if (c * fabs(u) <= w * exp(e - 0.5 * t * t))
+//                    break;
+//            }
+//        }
+//    } /* repeat .. until  `t' is accepted */
+//    x = s + 0.5 * t;
+//    return x * x;
+
+    
+}
+
+/*!
+ * This function is used when generating gamma-distributed random variables.
+ *
+ * \brief Subfunction for gamma random variables.
+ * \param s is the shape parameter of the gamma. 
+ * \return Returns a gamma-distributed random variable. 
+ * \throws Does not throw an error.
+ */
+double RbStatistics::Helper::rndGamma1(double s, RandomNumberGenerator& rng)
+{
+    
+	double			r, x = 0.0, small = 1e-37, w;
+	static double   a, p, uf, ss = 10.0, d;
+	
+	if (s != ss) {
+		a  = 1.0 - s;
+		p  = a / (a + s * exp(-a));
+		uf = p * pow(small / a, s);
+		d  = a * log(a);
+		ss = s;
+    }
+	for (;;) {
+		r = rng.uniform01();
+		if (r > p)        
+			x = a - log((1.0 - r) / (1.0 - p)), w = a * log(x) - d;
+		else if (r>uf)  
+			x = a * pow(r / p, 1.0 / s), w = x;
+		else            
+			return (0.0);
+		r = rng.uniform01();
+		if (1.0 - r <= w && r > 0.0)
+            if (r*(w + 1.0) >= 1.0 || -log(r) <= w)  
+                continue;
+		break;
+    }
+    
+	return (x);
+}
+
+/*!
+ * This function is used when generating gamma-distributed random variables.
+ *
+ * \brief Subfunction for gamma random variables.
+ * \param s is the shape parameter of the gamma. 
+ * \return Returns a gamma-distributed random variable. 
+ * \throws Does not throw an error.
+ */
+double RbStatistics::Helper::rndGamma2(double s, RandomNumberGenerator& rng) {
+    
+	double			r, d, f, g, x;
+	static double	b, h, ss = 0.0;
+	
+	if (s != ss) {
+		b  = s - 1.0;
+		h  = sqrt(3.0 * s - 0.75);
+		ss = s;
+    }
+	for (;;) {
+		r = rng.uniform01();
+		g = r - r * r;
+		f = (r - 0.5) * h / sqrt(g);
+		x = b + f;
+		if (x <= 0.0) 
+			continue;
+		r = rng.uniform01();
+		d = 64.0 * r * r * g * g * g;
+		if (d * x < x - 2.0 * f * f || log(d) < 2.0 * (b * log(x / b) - f))  
+			break;
+    }
+    
+	return x;
+}
+
+/*!
+ * This function is used when generating gamma-distributed random variables.
+ *
+ * \brief Subfunction for gamma random variables.
+ * \param a is the shape parameter of the gamma.
+ * \return Returns a gamma-distributed random variable.
+ * \throws Does not throw an error.
+ */
+double RbStatistics::Helper::rndGamma3(double a, RandomNumberGenerator& rng) {
     
     // Adapted code from R (rgamma)
     /* Constants : */
@@ -476,80 +726,74 @@ double RbStatistics::Helper::rndGamma(double a, RandomNumberGenerator& rng) {
     } /* repeat .. until  `t' is accepted */
     x = s + 0.5 * t;
     return x * x;
-    
-    
+
 }
 
 /*!
  * This function is used when generating gamma-distributed random variables.
  *
  * \brief Subfunction for gamma random variables.
- * \param s is the shape parameter of the gamma. 
- * \return Returns a gamma-distributed random variable. 
+ * \param s is the shape parameter of the gamma.
+ * \return Returns a gamma-distributed random variable.
  * \throws Does not throw an error.
  */
-double RbStatistics::Helper::rndGamma1(double s, RandomNumberGenerator& rng)
-{
+double RbStatistics::Helper::rndGamma4(double s, RandomNumberGenerator& rng) {
     
-	double			r, x = 0.0, small = 1e-37, w;
-	static double   a, p, uf, ss = 10.0, d;
-	
-	if (s != ss) {
-		a  = 1.0 - s;
-		p  = a / (a + s * exp(-a));
-		uf = p * pow(small / a, s);
-		d  = a * log(a);
-		ss = s;
-    }
-	for (;;) {
-		r = rng.uniform01();
-		if (r > p)        
-			x = a - log((1.0 - r) / (1.0 - p)), w = a * log(x) - d;
-		else if (r>uf)  
-			x = a * pow(r / p, 1.0 / s), w = x;
-		else            
-			return (0.0);
-		r = rng.uniform01();
-		if (1.0 - r <= w && r > 0.0)
-            if (r*(w + 1.0) >= 1.0 || -log(r) <= w)  
-                continue;
-		break;
+
+    // This algorithm is based on:
+    // G. Marsaglia and W. Tsang. A simple method for generating gamma variables. ACM Transactions on Mathematical Software, 26(3):363-372, 2000
+    
+    double alpha        = s;
+    bool   alpha_scaled = false;
+    
+    // check that alpha is finite
+    if ( !RbMath::isFinite(s) || s < 0.0 )
+    {
+        throw RbException("Infinite parameters for rgamma.");
     }
     
-	return (x);
+    // check if alpha is less than 1
+    if ( s < 1.0 )
+    {
+        alpha = s + 1;
+        alpha_scaled = true;
+    }
+    
+    
+    double d = alpha - 1.0 / 3.0;
+    double c = 1 / sqrt(9 * d);
+    
+    double x;
+    while(true) {
+        
+        double z = RbStatistics::Normal::rv(rng);
+        double u = rng.uniform01();
+        double v = pow(1 + c * z, 3);
+        
+        double log_u = log(u);
+        double rhs   = 0.5 * pow(z, 2) + d - d * v + d * log(v);
+        
+        if( z > (-1 / c) && log_u < rhs )
+        {
+            x = d * v;
+            break;
+        }
+        
+    }
+    
+    // check if we need to transform the RV
+    if ( alpha_scaled == true ) {
+        double u = rng.uniform01();
+        x *= pow(u, 1 / s);
+    }
+    
+    return x;
+    
 }
 
-/*!
- * This function is used when generating gamma-distributed random variables.
- *
- * \brief Subfunction for gamma random variables.
- * \param s is the shape parameter of the gamma. 
- * \return Returns a gamma-distributed random variable. 
- * \throws Does not throw an error.
- */
-double RbStatistics::Helper::rndGamma2(double s, RandomNumberGenerator& rng) {
-    
-	double			r, d, f, g, x;
-	static double	b, h, ss = 0.0;
-	
-	if (s != ss) {
-		b  = s - 1.0;
-		h  = sqrt(3.0 * s - 0.75);
-		ss = s;
-    }
-	for (;;) {
-		r = rng.uniform01();
-		g = r - r * r;
-		f = (r - 0.5) * h / sqrt(g);
-		x = b + f;
-		if (x <= 0.0) 
-			continue;
-		r = rng.uniform01();
-		d = 64.0 * r * r * g * g * g;
-		if (d * x < x - 2.0 * f * f || log(d) < 2.0 * (b * log(x / b) - f))  
-			break;
-    }
-    
-	return (x);
-}
+
+
+
+
+
 
