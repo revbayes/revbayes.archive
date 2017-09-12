df018b6495b0a382e4f9b12926c54c4243314079
Will Freyman <willfreyman@gmail.com>
2017-09-11 18:16:31 -0700

    fix to keep travis from timing out

--- tests/run_integration_tests.sh
+++ tests/run_integration_tests.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
 for f in scripts/*; do
-    ../projects/cmake/rb $f > output_tmp.txt
+    echo "#### Running test script: $f"
+    ../projects/cmake/rb $f # print output to keep Travis from timing out
 done
-
-rm output_tmp.txt
 
 failed=0
 for f in $(ls output); do
     res=$(diff <(tail -n +3 output/$f) <(tail -n +3 output_expected/$f) | wc -l)
     if [ $res -gt 0 ]; then
-        echo "Warning! Test failed: $f"
+        echo "#### Warning! Test failed: $f"
         (( failed++ ))
     else
-        echo "Test passed: $f"
+        echo "#### Test passed: $f"
     fi
 done
 #diff "Expected_results_basic.txt" "Test_basic.txt" | wc -l >> "Test_basic_results.txt"
 if [ $failed -gt 0 ]; then
-    echo "Warning! $failed tests failed."
+    echo "#### Warning! $failed tests failed."
     exit 113
 else
-    echo "All tests passed."
+    echo "#### All tests passed."
 fi
