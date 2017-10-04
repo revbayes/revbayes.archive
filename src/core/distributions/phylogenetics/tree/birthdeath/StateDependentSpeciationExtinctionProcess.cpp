revbayes/revbayes.archive

commit 10326d9194a93d94d94488f18d05d0b788da1ba8

adding support for origin age in CDBDP

src/core/distributions/phylogenetics/tree/birthdeath/StateDependentSpeciationExtinctionProcess.cpp
@@ -30,14 +30,15 @@ using namespace RevBayesCore;
  * The constructor connects the parameters of the birth-death process (DAG structure)
  * and initializes the probability density by computing the combinatorial constant of the tree structure.
  */
-StateDependentSpeciationExtinctionProcess::StateDependentSpeciationExtinctionProcess(const TypedDagNode<double> *ra,
+StateDependentSpeciationExtinctionProcess::StateDependentSpeciationExtinctionProcess(const TypedDagNode<double> *age,
                                                                                    const TypedDagNode<RbVector<double> > *ext,
                                                                                    const TypedDagNode<RateGenerator>* q,
                                                                                    const TypedDagNode<double>* r,
                                                                                    const TypedDagNode< Simplex >* p,
                                                                                    const TypedDagNode<double> *rh,
                                                                                    const std::string &cdt,
-                                                                                   const std::vector<Taxon> &tn) : TypedDistribution<Tree>( new TreeDiscreteCharacterData() ),
+                                                                                   const std::vector<Taxon> &tn,
+                                                                                   bool uo) : TypedDistribution<Tree>( new TreeDiscreteCharacterData() ),
     condition( cdt ),
     taxa( tn ),
     active_likelihood( std::vector<bool>(2*tn.size()-1, 0) ),
@@ -48,9 +49,10 @@ StateDependentSpeciationExtinctionProcess::StateDependentSpeciationExtinctionPro
     num_states( ext->getValue().size() ),
     scaling_factors( std::vector<std::vector<double> >(2*tn.size()-1, std::vector<double>(2,0.0) ) ),
     use_cladogenetic_events( false ),
+    use_origin( uo ),
     sample_character_history( false ),
     cladogenesis_matrix( NULL ),
-    root_age( ra ),
+    process_age( age ),
     mu( ext ),
     lambda(NULL),
     psi( NULL),
@@ -67,10 +69,10 @@ StateDependentSpeciationExtinctionProcess::StateDependentSpeciationExtinctionPro
     addParameter( Q );
     addParameter( rho );
     addParameter( rate );
-    addParameter( root_age );
+    addParameter( process_age );
     
     // set the length of the time slices used by the ODE for numerical integration
-    dt = root_age->getValue() / NUM_TIME_SLICES * 50.0;
+    dt = process_age->getValue() / NUM_TIME_SLICES * 50.0;
 
     value->getTreeChangeEventHandler().addListener( this );
 
@@ -156,15 +158,32 @@ double StateDependentSpeciationExtinctionProcess::computeLnProbability( void )
         
     }
     
-    double ra = value->getRoot().getAge();
+    size_t num_initial_lineages = 2;
+    const TopologyNode& root = value->getRoot();
+
+    if (use_origin) {
+        // If we are conditioning on survival from the origin,
+        // then we must divide by 2 the log survival probability computed by AbstractBirthDeathProcess
+        num_initial_lineages = 1;
+    }
+
+    // if conditioning on root, root node must be a "true" bifurcation event
+    else if (root.getChild(0).isSampledAncestor() || root.getChild(1).isSampledAncestor())
+    {
+        return RbConstants::Double::neginf;
+    }
+
+    // present time
+    double ra = root.getAge();
+    double process_time = getOriginAge();
     
-    if ( ra != root_age->getValue() )
+    if ( ra > process_time || ra != getRootAge() )
     {
         return RbConstants::Double::neginf;
     }
     
-    const std::vector<TopologyNode*> &c = value->getRoot().getChildren();
-    
+    const std::vector<TopologyNode*> &c = root.getChildren();
+
     for (std::vector<TopologyNode*>::const_iterator it = c.begin(); it != c.end(); ++it)
     {
         if ( ra < (*it)->getAge() )
@@ -172,7 +191,7 @@ double StateDependentSpeciationExtinctionProcess::computeLnProbability( void )
             return RbConstants::Double::neginf;
         }
     }
-    
+
     if( value->getNumberOfNodes() != dirty_nodes.size() )
     {
         dirty_nodes = std::vector<bool>(value->getNumberOfNodes(), true);
@@ -188,7 +207,7 @@ double StateDependentSpeciationExtinctionProcess::computeLnProbability( void )
     // conditioning on survival
     if ( condition == "survival" )
     {
-        lnProbTimes = - 2*log( pSurvival(0, ra) );
+        lnProbTimes = - num_initial_lineages*log( pSurvival(0, process_time) );
     }
     
     // multiply the probability of a descendant of the initial species
@@ -399,7 +418,22 @@ void StateDependentSpeciationExtinctionProcess::computeNodeProbability(const Rev
 
 double StateDependentSpeciationExtinctionProcess::computeRootLikelihood( void ) const
 {
-    
+    // get the likelihoods of descendant nodes
+    const TopologyNode     &root            = value->getRoot();
+    size_t                  node_index      = root.getIndex();
+    const TopologyNode     &left            = root.getChild(0);
+    size_t                  left_index      = left.getIndex();
+    computeNodeProbability( left, left_index );
+    const TopologyNode     &right           = root.getChild(1);
+    size_t                  right_index     = right.getIndex();
+    computeNodeProbability( right, right_index );
+
+    // get the likelihoods of descendant nodes
+    const std::vector<double> &left_likelihoods  = node_partial_likelihoods[left_index][active_likelihood[left_index]];
+    const std::vector<double> &right_likelihoods = node_partial_likelihoods[right_index][active_likelihood[right_index]];
+
+    std::vector<double> &node_likelihood  = node_partial_likelihoods[node_index][active_likelihood[node_index]];
+
     std::map<std::vector<unsigned>, double> eventMap;
     std::vector<double> speciation_rates;
     if ( use_cladogenetic_events == true )
@@ -415,16 +449,6 @@ double StateDependentSpeciationExtinctionProcess::computeRootLikelihood( void ) 
     {
         speciation_rates = lambda->getValue();
     }
-    
-    // get the likelihoods of descendant nodes
-    const TopologyNode     &root            = value->getRoot();
-    size_t                  node_index      = root.getIndex();
-    const TopologyNode     &left            = root.getChild(0);
-    size_t                  left_index      = left.getIndex();
-    computeNodeProbability( left, left_index );
-    const TopologyNode     &right           = root.getChild(1);
-    size_t                  right_index     = right.getIndex();
-    computeNodeProbability( right, right_index );
 
     bool speciation_node = true;
     if( left.isSampledAncestor() || right.isSampledAncestor() )
@@ -433,17 +457,13 @@ double StateDependentSpeciationExtinctionProcess::computeRootLikelihood( void ) 
     }
 
     // merge descendant likelihoods
-    const std::vector<double> &left_likelihoods  = node_partial_likelihoods[left_index][active_likelihood[left_index]];
-    const std::vector<double> &right_likelihoods = node_partial_likelihoods[right_index][active_likelihood[right_index]];
-    const RbVector<double> &freqs = getRootFrequencies();
-    double prob = 0.0;
-    state_type node_likelihood = std::vector<double>(2 * num_states, 0);
-    for (size_t i = 0; i < num_states; ++i)
+    for (size_t i=0; i<num_states; ++i)
     {
-        
+        node_likelihood[i] = left_likelihoods[i];
+
         if ( use_cladogenetic_events == true && speciation_node == true )
         {
-            
+
             double like_sum = 0.0;
             std::map<std::vector<unsigned>, double>::iterator it;
             for (it = eventMap.begin(); it != eventMap.end(); it++)
@@ -453,25 +473,75 @@ double StateDependentSpeciationExtinctionProcess::computeRootLikelihood( void ) 
                 if (i == states[0])
                 {
                     double likelihoods = left_likelihoods[num_states + states[1]] * right_likelihoods[num_states + states[2]];
-                    like_sum += likelihoods * speciation_rate;
+                    like_sum += speciation_rate * likelihoods;
                 }
             }
-            // weight by root frequencies
-            node_likelihood[i] = left_likelihoods[i];
-            node_likelihood[num_states + i] = freqs[i] * like_sum;
-            prob += freqs[i] * like_sum;
+            node_likelihood[num_states + i] = like_sum;
 
         }
         else
         {
-            prob += freqs[i] * left_likelihoods[num_states + i] * right_likelihoods[num_states + i];
+            node_likelihood[num_states + i] = left_likelihoods[num_states + i] * right_likelihoods[num_states + i];
+            node_likelihood[num_states + i] *= speciation_node ? speciation_rates[i] : 1.0;
         }
-        
     }
     
-    // store the likelihoods
-    node_partial_likelihoods[node_index][active_likelihood[node_index]] = node_likelihood;
-    
+    // calculate likelihoods for the root branch
+    if( use_origin )
+    {
+        double begin_age = getRootAge();
+        double end_age = getOriginAge();
+
+        if ( sample_character_history == false )
+        {
+            // numerically integrate over the entire branch length
+            numericallyIntegrateProcess(node_likelihood, begin_age, end_age, true, false);
+        }
+        else
+        {
+            // calculate the conditional likelihoods for each time slice moving
+            // along this branch backwards in time from the tip towards the root
+
+            std::vector<std::vector<double> > branch_likelihoods;
+            size_t current_dt = 0;
+
+            // calculate partial likelihoods for each time slice and store them in branch_likelihoods
+            while ( (current_dt * dt) + begin_age < end_age )
+            {
+
+                std::vector<double> dt_likelihood;
+
+                double current_dt_start = (current_dt * dt) + begin_age;
+                double current_dt_end = ((current_dt + 1) * dt) + begin_age;
+                if (current_dt_end > end_age)
+                {
+                    current_dt_end = end_age;
+                }
+                numericallyIntegrateProcess(node_likelihood, current_dt_start, current_dt_end, true, false);
+
+                std::vector<double>::const_iterator first = node_likelihood.begin() + num_states;
+                std::vector<double>::const_iterator last = node_likelihood.begin() + (num_states * 2);
+                dt_likelihood = std::vector<double>(first, last);
+
+                branch_likelihoods.push_back(dt_likelihood);
+                current_dt++;
+
+            }
+
+            // save the branch conditional likelihoods
+            branch_partial_likelihoods[node_index] = branch_likelihoods;
+        }
+    }
+
+    // sum the root likelihoods
+    const RbVector<double> &freqs = getRootFrequencies();
+    double prob = 0.0;
+
+    for (size_t i = 0; i < num_states; ++i)
+    {
+        prob += freqs[i] * node_likelihood[num_states + i];
+    }
+
     scaling_factors[node_index][active_likelihood[node_index]] = scaling_factors[left_index][active_likelihood[left_index]] + scaling_factors[right_index][active_likelihood[right_index]];
 
     return log(prob) + scaling_factors[node_index][active_likelihood[node_index]];
@@ -1292,7 +1362,7 @@ void StateDependentSpeciationExtinctionProcess::executeProcedure(const std::stri
 void StateDependentSpeciationExtinctionProcess::getAffected(RbOrderedSet<DagNode *> &affected, RevBayesCore::DagNode *affecter)
 {
     
-    if ( affecter == root_age)
+    if ( affecter == process_age)
     {
         dag_node->getAffectedNodes( affected );
     }
@@ -1336,6 +1406,36 @@ const RateGenerator& StateDependentSpeciationExtinctionProcess::getEventRateMatr
 }
 
 
+double StateDependentSpeciationExtinctionProcess::getOriginAge( void ) const
+{
+
+    return process_age->getValue();
+}
+
+
+/**
+ * By default, the root age is assumed to be equal to the origin time.
+ * This should be overridden if a distinct root age is needed
+ */
+double StateDependentSpeciationExtinctionProcess::getRootAge( void ) const
+{
+
+    if(use_origin)
+    {
+        if(value->getNumberOfNodes() > 0)
+        {
+            return value->getRoot().getAge();
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    else
+        return getOriginAge();
+}
+
+
 /**
  * Get the stationary root frequencies
  */
@@ -1360,7 +1460,7 @@ std::vector<double> StateDependentSpeciationExtinctionProcess::getRootFrequencie
 void StateDependentSpeciationExtinctionProcess::keepSpecialization(DagNode *affecter)
 {
     
-    if ( affecter == root_age )
+    if ( affecter == process_age )
     {
         dag_node->keepAffected();
     }
@@ -1449,10 +1549,17 @@ void StateDependentSpeciationExtinctionProcess::redrawValue( void )
 void StateDependentSpeciationExtinctionProcess::restoreSpecialization(DagNode *affecter)
 {
     
-    if ( affecter == root_age )
+    if ( affecter == process_age )
     {
-        value->getRoot().setAge( root_age->getValue() );
-        dag_node->restoreAffected();
+        if( use_origin == false )
+        {
+            value->getRoot().setAge( process_age->getValue() );
+        }
+
+        if ( dag_node != NULL )
+        {
+            dag_node->restoreAffected();
+        }
     }
     
     // reset the flags
@@ -1555,7 +1662,7 @@ void StateDependentSpeciationExtinctionProcess::setNumberOfTimeSlices( double n 
 {
     
     NUM_TIME_SLICES = n;
-    dt = root_age->getValue() / NUM_TIME_SLICES;
+    dt = process_age->getValue() / NUM_TIME_SLICES;
     
 }
 
@@ -1575,16 +1682,16 @@ void StateDependentSpeciationExtinctionProcess::setValue(Tree *v, bool f )
     
     value->getTreeChangeEventHandler().addListener( this );
     
-    if ( root_age != NULL )
+    if ( process_age != NULL && use_origin == false )
     {
-        const StochasticNode<double> *stoch_root_age = dynamic_cast<const StochasticNode<double>* >(root_age);
-        if ( stoch_root_age != NULL )
+        const StochasticNode<double> *stoch_process_age = dynamic_cast<const StochasticNode<double>* >(process_age);
+        if ( stoch_process_age != NULL )
         {
-            const_cast<StochasticNode<double> *>(stoch_root_age)->setValue( new double( value->getRoot().getAge() ), f);
+            const_cast<StochasticNode<double> *>(stoch_process_age)->setValue( new double( value->getRoot().getAge() ), f);
         }
         else
         {
-            value->getRoot().setAge( root_age->getValue() );
+            value->getRoot().setAge( process_age->getValue() );
         }
         
     }
@@ -1612,9 +1719,9 @@ void StateDependentSpeciationExtinctionProcess::simulateTree( void )
 void StateDependentSpeciationExtinctionProcess::swapParameterInternal(const DagNode *oldP, const DagNode *newP)
 {
     
-    if ( oldP == root_age )
+    if ( oldP == process_age )
     {
-        root_age = static_cast<const TypedDagNode<double>* >( newP );
+        process_age = static_cast<const TypedDagNode<double>* >( newP );
     }
     if ( oldP == mu )
     {
@@ -1660,10 +1767,17 @@ void StateDependentSpeciationExtinctionProcess::swapParameterInternal(const DagN
 void StateDependentSpeciationExtinctionProcess::touchSpecialization(DagNode *affecter, bool touchAll)
 {
     
-    if ( affecter == root_age )
+    if ( affecter == process_age )
     {
-        value->getRoot().setAge( root_age->getValue() );
-        dag_node->touchAffected();
+        if( use_origin == false)
+        {
+            value->getRoot().setAge( process_age->getValue() );
+        }
+
+        if ( dag_node != NULL )
+        {
+            dag_node->touchAffected();
+        }
     }
     
     if ( affecter != this->dag_node )

src/core/distributions/phylogenetics/tree/birthdeath/StateDependentSpeciationExtinctionProcess.h
@@ -37,7 +37,8 @@ namespace RevBayesCore {
                                                   const TypedDagNode<Simplex>* p,
                                                   const TypedDagNode<double> *rh,
                                                   const std::string &cdt,
-                                                  const std::vector<Taxon> &tn);
+                                                  const std::vector<Taxon> &tn,
+                                                  bool uo);
         
         // pure virtual member functions
         virtual StateDependentSpeciationExtinctionProcess*              clone(void) const;
@@ -46,6 +47,8 @@ namespace RevBayesCore {
         double                                                          computeLnProbability(void);
         void                                                            fireTreeChangeEvent(const TopologyNode &n, const unsigned& m=0);                                                 //!< The tree has changed and we want to know which part.
         const AbstractHomologousDiscreteCharacterData&                  getCharacterData() const;
+        double                                                          getOriginAge(void) const;
+        double                                                          getRootAge(void) const;
         virtual void                                                    redrawValue(void);
         void                                                            setCladogenesisMatrix(const TypedDagNode< CladogeneticSpeciationRateMatrix > *r);
         void                                                            setSerialSamplingRates(const TypedDagNode< RbVector<double> > *r);
@@ -100,11 +103,12 @@ namespace RevBayesCore {
         size_t                                                          num_states;
         mutable std::vector<std::vector<double> >                       scaling_factors;
         bool                                                            use_cladogenetic_events;                                                                            //!< do we use the speciation rates from the cladogenetic event map?
+        bool                                                            use_origin;
         bool                                                            sample_character_history;                                                                           //!< are we sampling the character history along branches?
         
         // parameters
         const TypedDagNode< CladogeneticSpeciationRateMatrix >*         cladogenesis_matrix;
-        const TypedDagNode<double>*                                     root_age;                                                                                           //!< Time since the origin.
+        const TypedDagNode<double>*                                     process_age;                                                                                           //!< Time since the origin.
         const TypedDagNode<RbVector<double> >*                          mu;
         const TypedDagNode<RbVector<double> >*                          lambda;
         const TypedDagNode<RbVector<double> >*                          psi;

src/revlanguage/distributions/phylogenetics/tree/Dist_CharacterDependentBirthDeathProcess.cpp
@@ -46,7 +46,7 @@ RevBayesCore::TypedDistribution<RevBayesCore::Tree>* Dist_CharacterDependentBirt
 {
     
     // Get the parameters
-    RevBayesCore::TypedDagNode<double>* ra   = static_cast<const RealPos &>( root_age->getRevObject() ).getDagNode();
+    RevBayesCore::TypedDagNode<double>* ra   = static_cast<const RealPos &>( start_age->getRevObject() ).getDagNode();
 //    RevBayesCore::TypedDagNode<long>*    rs   = static_cast<const Natural &>( root_state->getRevObject() ).getDagNode();
     RevBayesCore::TypedDagNode<RevBayesCore::RbVector<double> >* sp  = static_cast<const ModelVector<RealPos> &>( speciation_rates->getRevObject() ).getDagNode();
     RevBayesCore::TypedDagNode<RevBayesCore::RbVector<double> >* ex  = static_cast<const ModelVector<RealPos> &>( extinction_rates->getRevObject() ).getDagNode();
@@ -75,8 +75,10 @@ RevBayesCore::TypedDistribution<RevBayesCore::Tree>* Dist_CharacterDependentBirt
     // condition
     const std::string& cond                     = static_cast<const RlString &>( condition->getRevObject() ).getValue();
     
+    // the start condition
+    bool uo = ( start_condition == "originAge" ? true : false );
     
-    RevBayesCore::StateDependentSpeciationExtinctionProcess*   d = new RevBayesCore::StateDependentSpeciationExtinctionProcess( ra, ex, q, r, bf, rh, cond, t );
+    RevBayesCore::StateDependentSpeciationExtinctionProcess*   d = new RevBayesCore::StateDependentSpeciationExtinctionProcess( ra, ex, q, r, bf, rh, cond, t, uo );
     d->setSpeciationRates( sp );
     
     // set the number of time slices for the numeric ODE
@@ -176,7 +178,10 @@ const MemberRules& Dist_CharacterDependentBirthDeathProcess::getParameterRules(v
     if ( !rules_set )
     {
         
-        memberRules.push_back( new ArgumentRule( "rootAge"   , RealPos::getClassTypeSpec()              , "The age of the root."                        , ArgumentRule::BY_CONSTANT_REFERENCE   , ArgumentRule::ANY ) );
+        std::vector<std::string> aliases;
+        aliases.push_back("rootAge");
+        aliases.push_back("originAge");
+        memberRules.push_back( new ArgumentRule( aliases, RealPos::getClassTypeSpec()    , "The start time of the process.", ArgumentRule::BY_CONSTANT_REFERENCE, ArgumentRule::ANY ) );
         std::vector<std::string> slabels;
         slabels.push_back("speciationRates");
         slabels.push_back("lambda");
@@ -219,9 +224,10 @@ const TypeSpec& Dist_CharacterDependentBirthDeathProcess::getTypeSpec( void ) co
 void Dist_CharacterDependentBirthDeathProcess::setConstParameter(const std::string& name, const RevPtr<const RevVariable> &var)
 {
     
-    if ( name == "rootAge" )
+    if( name == "rootAge" || name == "originAge" )
     {
-        root_age = var;
+        start_age = var;
+        start_condition = name;
     }
     else if ( name == "pi" )
     {

src/revlanguage/distributions/phylogenetics/tree/Dist_CharacterDependentBirthDeathProcess.h
@@ -40,7 +40,7 @@ namespace RevLanguage {
         
     private:
         
-        RevPtr<const RevVariable>                                   root_age;
+        RevPtr<const RevVariable>                                   start_age;
         RevPtr<const RevVariable>                                   root_frequencies;
         RevPtr<const RevVariable>                                   speciation_rates;
         RevPtr<const RevVariable>                                   extinction_rates;
@@ -51,6 +51,7 @@ namespace RevLanguage {
         RevPtr<const RevVariable>                                   condition;                                                                              //!< The condition of the process (none/survival/#Taxa)
         RevPtr<const RevVariable>                                   taxa;
         RevPtr<const RevVariable>                                   num_time_slices;
+        std::string                                                 start_condition;                                                                     //!< The start condition of the process (rootAge/originAge)
 
     };
     

src/revlanguage/distributions/phylogenetics/tree/Dist_CharacterDependentCladoBirthDeathProcess.cpp
@@ -48,7 +48,7 @@ RevBayesCore::TypedDistribution<RevBayesCore::Tree>* Dist_CharacterDependentClad
 {
     
     // Get the parameters
-    RevBayesCore::TypedDagNode<double>* ra                              = static_cast<const RealPos &>( root_age->getRevObject() ).getDagNode();
+    RevBayesCore::TypedDagNode<double>* ra                              = static_cast<const RealPos &>( start_age->getRevObject() ).getDagNode();
     RevBayesCore::TypedDagNode<RevBayesCore::RbVector<double> >* ex     = static_cast<const ModelVector<Real> &>( extinction_rates->getRevObject() ).getDagNode();
     RevBayesCore::TypedDagNode<RevBayesCore::RateGenerator>* q          = static_cast<const RateGenerator &>( event_rate_matrix->getRevObject() ).getDagNode();
     RevBayesCore::TypedDagNode<double>*  r                              = static_cast<const RealPos &>( event_rate->getRevObject() ).getDagNode();
@@ -57,8 +57,11 @@ RevBayesCore::TypedDistribution<RevBayesCore::Tree>* Dist_CharacterDependentClad
     std::vector<RevBayesCore::Taxon> t                                  = static_cast<const ModelVector<Taxon> &>( taxa->getRevObject() ).getValue();
     const std::string& cond                                             = static_cast<const RlString &>( condition->getRevObject() ).getValue();
     
+    // the start condition
+    bool uo = ( start_condition == "originAge" ? true : false );
+
     //RevBayesCore::CharacterDependentCladoBirthDeathProcess* d = new RevBayesCore::CharacterDependentCladoBirthDeathProcess( ra, ex, q, r, rf, rh, cond, t );
-    RevBayesCore::StateDependentSpeciationExtinctionProcess*   d = new RevBayesCore::StateDependentSpeciationExtinctionProcess( ra, ex, q, r, rf, rh, cond, t );
+    RevBayesCore::StateDependentSpeciationExtinctionProcess*   d = new RevBayesCore::StateDependentSpeciationExtinctionProcess( ra, ex, q, r, rf, rh, cond, t, uo );
     
     // set the cladogenetic speciation rate event map
     RevBayesCore::TypedDagNode<RevBayesCore::CladogeneticSpeciationRateMatrix>* cp = static_cast<const CladogeneticSpeciationRateMatrix &>( cladoEvents->getRevObject() ).getDagNode();
@@ -133,7 +136,10 @@ const MemberRules& Dist_CharacterDependentCladoBirthDeathProcess::getParameterRu
     if ( !rules_set )
     {
         
-        memberRules.push_back( new ArgumentRule( "rootAge",           RealPos::getClassTypeSpec(),              "The age of the root.",                                     ArgumentRule::BY_CONSTANT_REFERENCE, ArgumentRule::ANY ) );
+        std::vector<std::string> aliases;
+        aliases.push_back("rootAge");
+        aliases.push_back("originAge");
+        memberRules.push_back( new ArgumentRule( aliases, RealPos::getClassTypeSpec()    , "The start time of the process.", ArgumentRule::BY_CONSTANT_REFERENCE, ArgumentRule::ANY ) );
         memberRules.push_back( new ArgumentRule( "cladoEventMap",     CladogeneticSpeciationRateMatrix::getClassTypeSpec(),           "The map of speciation rates to cladogenetic event types.", ArgumentRule::BY_CONSTANT_REFERENCE, ArgumentRule::ANY ) );
         memberRules.push_back( new ArgumentRule( "extinctionRates",   ModelVector<Real>::getClassTypeSpec(),    "The vector of extinction rates for the observed states.",  ArgumentRule::BY_CONSTANT_REFERENCE, ArgumentRule::ANY ) );
         memberRules.push_back( new ArgumentRule( "Q",                 RateGenerator::getClassTypeSpec(),        "The rate matrix of jumping between rate categories.",      ArgumentRule::BY_CONSTANT_REFERENCE, ArgumentRule::ANY ) );
@@ -168,9 +174,10 @@ const TypeSpec& Dist_CharacterDependentCladoBirthDeathProcess::getTypeSpec( void
 void Dist_CharacterDependentCladoBirthDeathProcess::setConstParameter(const std::string& name, const RevPtr<const RevVariable> &var)
 {
     
-    if ( name == "rootAge" )
+    if( name == "rootAge" || name == "originAge" )
     {
-        root_age = var;
+        start_age = var;
+        start_condition = name;
     }
     else if ( name == "cladoEventMap" )
     {

src/revlanguage/distributions/phylogenetics/tree/Dist_CharacterDependentCladoBirthDeathProcess.h
@@ -39,7 +39,7 @@ namespace RevLanguage {
     private:
         
         RevPtr<const RevVariable>                                   cladoEvents;
-        RevPtr<const RevVariable>                                   root_age;
+        RevPtr<const RevVariable>                                   start_age;
         RevPtr<const RevVariable>                                   root_frequencies;
         RevPtr<const RevVariable>                                   extinction_rates;
         RevPtr<const RevVariable>                                   event_rate_matrix;
@@ -48,7 +48,8 @@ namespace RevLanguage {
         RevPtr<const RevVariable>                                   condition;                                                                              //!< The condition of the process (none/survival/#Taxa)
         RevPtr<const RevVariable>                                   taxa;
         RevPtr<const RevVariable>                                   num_time_slices;
-        
+        std::string                                                 start_condition;                                                                     //!< The start condition of the process (rootAge/originAge)
+
     };
     
 }

commit e42e24b49e71160bd3d35f4811e67456ceb111e6

tricky and important SSE fix for bug introduced in 10326d9

src/core/distributions/phylogenetics/tree/birthdeath/StateDependentSpeciationExtinctionProcess.cpp
@@ -157,7 +157,7 @@ double StateDependentSpeciationExtinctionProcess::computeLnProbability( void )
         
     }
     
-    size_t num_initial_lineages = 2;
+    double num_initial_lineages = 2; // this needs to be a double!
     const TopologyNode& root = value->getRoot();
 
     if (use_origin) {