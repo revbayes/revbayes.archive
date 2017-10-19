15f68eccdaa20384a7ba476614cf71dd1876645e
Walker Pett <will.pett@gmail.com>
2017-10-19 09:54:51 -0500

    add independence MH tree proposal

---
+++ src/revlanguage/moves/tree/Move_IndependentTopology.cpp
@@ -0,0 +1,162 @@
+#include "Move_IndependentTopology.h"
+#include "ArgumentRule.h"
+#include "ArgumentRules.h"
+#include "IndependentTopologyProposal.h"
+#include "ModelVector.h"
+#include "MetropolisHastingsMove.h"
+#include "RbException.h"
+#include "RealPos.h"
+#include "RlClade.h"
+#include "RlTimeTree.h"
+#include "RlTypedDistribution.h"
+#include "TypedDagNode.h"
+#include "TypeSpec.h"
+
+
+using namespace RevLanguage;
+
+
+Move_IndependentTopology::Move_IndependentTopology() : Move()
+{
+    
+}
+
+
+/**
+ * The clone function is a convenience function to create proper copies of inherited objected.
+ * E.g. a.clone() will create a clone of the correct type even if 'a' is of derived type 'b'.
+ *
+ * \return A new copy of the process.
+ */
+Move_IndependentTopology* Move_IndependentTopology::clone(void) const
+{
+    
+    return new Move_IndependentTopology(*this);
+}
+
+
+void Move_IndependentTopology::constructInternalObject( void )
+{
+    // we free the memory first
+    delete value;
+    
+    // now allocate a new empirical tree move
+    double w = static_cast<const RealPos &>( weight->getRevObject() ).getValue();
+    
+    RevBayesCore::TypedDagNode<RevBayesCore::Tree>* tmp = static_cast<const TimeTree &>( tree->getRevObject() ).getDagNode();
+    RevBayesCore::StochasticNode<RevBayesCore::Tree> *t = static_cast<RevBayesCore::StochasticNode<RevBayesCore::Tree> *>( tmp );
+
+    const Distribution& rlDistribution                        = static_cast<const Distribution &>( proposal_distribution->getRevObject() );
+    RevBayesCore::TypedDistribution<RevBayesCore::Tree>* prop = static_cast<RevBayesCore::TypedDistribution<RevBayesCore::Tree>* >( rlDistribution.createDistribution() );
+
+    const RevBayesCore::Clade& c      = static_cast<const Clade&>( outgroup->getRevObject() ).getValue();
+
+    RevBayesCore::Proposal *p = new RevBayesCore::IndependentTopologyProposal(t, prop, c);
+    value = new RevBayesCore::MetropolisHastingsMove(p, w, false);
+    
+}
+
+
+/** Get Rev type of object */
+const std::string& Move_IndependentTopology::getClassType(void)
+{
+    static std::string rev_type = "Move_IndependentTopology";
+    
+    return rev_type;
+}
+
+/** Get class type spec describing type of object */
+const TypeSpec& Move_IndependentTopology::getClassTypeSpec(void)
+{
+    
+    static TypeSpec rev_type_spec = TypeSpec( getClassType(), new TypeSpec( Move::getClassTypeSpec() ) );
+    
+    return rev_type_spec;
+}
+
+
+/**
+ * Get the Rev name for the constructor function.
+ *
+ * \return Rev name of constructor function.
+ */
+std::string Move_IndependentTopology::getMoveName( void ) const
+{
+    // create a constructor function name variable that is the same for all instance of this class
+    std::string c_name = "IndependentTopology";
+    
+    return c_name;
+}
+
+
+/** Return member rules (no members) */
+const MemberRules& Move_IndependentTopology::getParameterRules(void) const
+{
+    
+    static MemberRules move_member_rules;
+    static bool rules_set = false;
+    
+    if ( !rules_set )
+    {
+        move_member_rules.push_back( new ArgumentRule( "tree", TimeTree::getClassTypeSpec(), "The stochastic time tree variable on which this moves operates.", ArgumentRule::BY_REFERENCE, ArgumentRule::STOCHASTIC ) );
+        move_member_rules.push_back( new ArgumentRule( "proposal", TypedDistribution<Tree>::getClassTypeSpec(), "The proposal topology distribution.",  ArgumentRule::BY_VALUE, ArgumentRule::ANY ) );
+        move_member_rules.push_back( new ArgumentRule( "outgroup", Clade::getClassTypeSpec(), "Outgroup used to root the proposal topologies.",  ArgumentRule::BY_VALUE, ArgumentRule::ANY, new Clade() ) );
+
+        /* Inherit weight from Move, put it after variable */
+        const MemberRules& inheritedRules = Move::getParameterRules();
+        move_member_rules.insert( move_member_rules.end(), inheritedRules.begin(), inheritedRules.end() );
+        
+        rules_set = true;
+    }
+    
+    return move_member_rules;
+}
+
+/** Get type spec */
+const TypeSpec& Move_IndependentTopology::getTypeSpec( void ) const
+{
+    
+    static TypeSpec type_spec = getClassTypeSpec();
+    
+    return type_spec;
+}
+
+
+/** Get type spec */
+void Move_IndependentTopology::printValue(std::ostream &o) const
+{
+    
+    o << "Move_IndependentTopology(";
+    if (tree != NULL)
+    {
+        o << tree->getName();
+    }
+    else
+    {
+        o << "?";
+    }
+    o << ")";
+}
+
+
+/** Set a member variable */
+void Move_IndependentTopology::setConstParameter(const std::string& name, const RevPtr<const RevVariable> &var) {
+    
+    if ( name == "tree" )
+    {
+        tree = var;
+    }
+    else if( name == "proposal" )
+    {
+        proposal_distribution = var;
+    }
+    else if( name == "outgroup" )
+    {
+        outgroup = var;
+    }
+    else
+    {
+        Move::setConstParameter(name, var);
+    }
+}
+
