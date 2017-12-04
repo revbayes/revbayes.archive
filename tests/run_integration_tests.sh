dbaea39284b1bb2c2723972389281f8c62f6c66c
Walker Pett <will.pett@gmail.com>
2017-12-04 16:16:01 -0600

    modularizing integration tests; detecting non-zero exit status

--- tests/run_integration_tests.sh
+++ tests/run_integration_tests.sh
@@ -1,23 +1,82 @@
 #!/bin/bash
 
-rm -rf output
+tests=()
+status=()
 
-for f in scripts/*; do
-    printf "\n\n#### Running test script: $f\n\n"
-    ../projects/cmake/rb $f # print output so we can see any error messages
-done
-
-failed=0
-printf "\n\n#### Checking output from test scripts... \n"
-for f in $(ls output_expected); do
-    if [ ! -e output/$f ] || ! diff <(tail -n +3 output/$f) <(tail -n +3 output_expected/$f) > /dev/null; then
-        printf ">>>> Warning! Test failed: $f\n"
-        (( failed++ ))
+for t in test_*; do
+    if [ -d $t ]; then
+        tests+=($t)
     else
-        printf "#### Test passed: $f\n"
+        continue
     fi
+    
+    printf "\n\n#### Running test: $t\n\n"
+    cd $t
+    
+    rm -rf output data
+    ln -s ../data data
+
+    res=0
+    # run the test scripts
+    for f in scripts/*; do
+        ../../projects/cmake/rb $f # print output so we can see any error messages
+        res="$?"
+        if [ $res = 1 ]; then
+            res="error: $f"
+        elif [ $res = 139 ]; then
+            res="segfault: $f"
+        elif [ $res != 0 ]; then
+            res="error $res: $f"
+            break
+        fi
+    done
+
+    # store the exit status
+    status+=("$res")
+
+    rm data
+
+    cd ..
 done
-#diff "Expected_results_basic.txt" "Test_basic.txt" | wc -l >> "Test_basic_results.txt"
+
+printf "\n\n#### Checking output from tests... \n"
+failed=0
+i=0
+while [  $i -lt ${#tests[@]} ]; do
+    t=${tests[$i]}
+    cd $t
+
+    # check if output matches expected output
+    errs=()
+    for f in $(ls output_expected); do
+        if [ ! -e output/$f ]; then
+            errs+=("missing:  $f")
+        elif ! diff <(tail -n +3 output/$f) <(tail -n +3 output_expected/$f) > /dev/null; then
+            errs+=("mismatch: $f")
+        fi
+    done
+
+    if [ "${status[$i]}" != 0 ]; then
+        errs=("${status[$i]}")
+    fi
+
+    # failure if the output doesn't match, or the script aborted
+    if [ ${#errs[@]} -gt 0 ]; then
+        ((failed++))
+        printf ">>>> Test failed: $t\n"
+        for errmsg in "${errs[@]}"; do
+            printf "\t$errmsg\n"
+        done
+    else
+        printf "#### Test passed: $t\n"
+    fi
+
+    cd ..
+
+    ((i++))
+done
+
+
 if [ $failed -gt 0 ]; then
     printf "\n\n#### Warning! $failed tests failed.\n\n"
     exit 113
