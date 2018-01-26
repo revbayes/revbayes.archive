f1489b773520b2c054e60b59cbcf6b74475f6015
Michael Landis <mlandis@gmail.com>
2018-01-26 12:23:40 -0500

    remove unused local file

--- src/core/distributions/WeightedSampleDistribution.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  WeightedSampleDistribution.cpp
-//  revbayes-proj
-//
-//  Created by Michael Landis on 1/25/18.
-//  Copyright © 2018 Michael Landis. All rights reserved.
-//
-
-#include "WeightedSampleDistribution.hpp"
