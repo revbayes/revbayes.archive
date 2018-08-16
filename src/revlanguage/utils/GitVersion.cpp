61b4f0398c81e45cecf5805600e01d3603636b50
Michael R May <mikeryanmay@gmail.com>
2018-08-16 07:41:35 +0200

    cleaning up help for poisson distribution

--- src/revlanguage/utils/GitVersion.cpp
+++ src/revlanguage/utils/GitVersion.cpp
@@ -1,4 +1,4 @@
 #include "GitVersion.h"
-const char *build_git_sha = "29e738";
-const char *build_date = "Thu Apr 12 14:45:20 -03 2018";
+const char *build_git_sha = "b27d5f";
+const char *build_date = "Wed Aug 15 17:57:07 CEST 2018";
 const char *build_git_branch = "development";
