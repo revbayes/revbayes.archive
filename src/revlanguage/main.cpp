fe8b40e2e9873e8d45613e3f40fffbc49c6d638f
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2018-08-16 16:29:21 +0200

    reverting last change because we found a way to get xcode to work again

--- src/revlanguage/main.cpp
+++ src/revlanguage/main.cpp
@@ -2,9 +2,9 @@
 #include <string>
 #include <vector>
 
-//#include <boost/program_options.hpp>
-//namespace po = boost::program_options;
-//using po::variables_map;
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+using po::variables_map;
 
 #include "RbVersion.h"
 #include "RbException.h"
@@ -38,54 +38,54 @@ string short_description()
     return "Bayesian phylogenetic inference using probabilistic graphical models and an interpreted language";
 }
 
-////
-//variables_map parse_cmd_line(int argc, char* argv[])
-//{
-//    using namespace po;
 //
-//    // Put all options in one group for now.
-//    options_description general("Options");
-//    general.add_options()
-//    ("help,h","Show information on flags.")
-//    ("version,v","Show version and exit.")
-//
-//    // implicit_value(1) means that -V => -V1
-////      RevBayes doesn't use a global verbose_logging flag.
-////    ("verbose,V",value<int>()->implicit_value(1),"Log extra information for debugging.")
-//
-//    ("batch,b","Run in batch mode.")
-//    // composing means that --file can occur multiple times
-//    ("file",value<vector<string> >()->composing(),"File(s) to source.")
-//    ("setOption",value<vector<string> >()->composing(),"Set an option key=value.")
-//    ;
-//
-//    // Treat all positional options as "file" options.
-//    positional_options_description p;
-//    p.add("file", -1);
-//
-//    // Parse the command line into variables_map 'args'
-//    variables_map args;
-//    store(command_line_parser(argc, argv).options(general).positional(p).run(), args);
-//    notify(args);
-//
-//    // Print flags and usage info in this function since we know the flags here.
-//    if (args.count("help"))
-//    {
-//    // Do we want to avoid displaying --file here, since its a positional option also?
-//
-//    std::cout<<usage()<<"\n";
-//    std::cout<<short_description()<<"\n\n";
-//    std::cout<<general<<"\n";
-//    std::cout<<"See http://revbayes.github.io for more information\n";
-//    std::exit(0);
-//    }
-//
-//    return args;
-//}
+variables_map parse_cmd_line(int argc, char* argv[])
+{
+    using namespace po;
+
+    // Put all options in one group for now.
+    options_description general("Options");
+    general.add_options()
+	("help,h","Show information on flags.")
+	("version,v","Show version and exit.")
+
+	// implicit_value(1) means that -V => -V1
+//      RevBayes doesn't use a global verbose_logging flag.
+//	("verbose,V",value<int>()->implicit_value(1),"Log extra information for debugging.")
+
+	("batch,b","Run in batch mode.")
+	// composing means that --file can occur multiple times
+	("file",value<vector<string> >()->composing(),"File(s) to source.")
+	("setOption",value<vector<string> >()->composing(),"Set an option key=value.")
+	;
+
+    // Treat all positional options as "file" options.
+    positional_options_description p;
+    p.add("file", -1);
+
+    // Parse the command line into variables_map 'args'
+    variables_map args;
+    store(command_line_parser(argc, argv).options(general).positional(p).run(), args);
+    notify(args);
+
+    // Print flags and usage info in this function since we know the flags here.
+    if (args.count("help"))
+    {
+	// Do we want to avoid displaying --file here, since its a positional option also?
+
+	std::cout<<usage()<<"\n";
+	std::cout<<short_description()<<"\n\n";
+	std::cout<<general<<"\n";
+	std::cout<<"See http://revbayes.github.io for more information\n";
+	std::exit(0);
+    }
+
+    return args;
+}
 
 int main(int argc, char* argv[]) {
 
-//      using namespace po;
+      using namespace po;
 #   ifdef RB_MPI
     int process_id = 0;
     int num_processes = 0;
@@ -118,43 +118,42 @@ int main(int argc, char* argv[]) {
 #endif
 
 
-//    /* Parse argv to get the command line arguments */
-//    variables_map args = parse_cmd_line(argc, argv);
-//
-//    if (args.count("version"))
-//    {
-//    std::cout<<RbVersion().getVersion()<<std::endl;
-//    exit(0);
-//    }
-//
-//    if (args.count("verbose"))
-//    {
-//    int verbosity = args["verbose"].as<int>();
-//    }
-//
-//    if (args.count("setOption"))
-//    {
-//    vector<string> options = args["setOption"].as<vector<string> >();
-//    for(int i=0;i<options.size();i++)
-//    {
-//        vector<string> tokens;
-//        StringUtilities::stringSplit(options[i], "=", tokens);
-//        if (tokens.size() != 2)
-//        throw RbException("Option '"+options[i]+"' must have the form key=value");
-//        else
-//        RbSettings::userSettings().setOption( tokens[0], tokens[1], false );
-//    }
-//    }
-//
-//    /*default to interactive mode*/
-    bool batch_mode = false;
-//    batch_mode  = args.count("batch");
-//    // FIXME -- the batch_mode variable appears to have no effect if true.
-//
-//    /* seek out files from command line */
+    /* Parse argv to get the command line arguments */
+    variables_map args = parse_cmd_line(argc, argv);
+
+    if (args.count("version"))
+    {
+	std::cout<<RbVersion().getVersion()<<std::endl;
+	exit(0);
+    }
+
+    if (args.count("verbose"))
+    {
+	int verbosity = args["verbose"].as<int>();
+    }
+
+    if (args.count("setOption"))
+    {
+	vector<string> options = args["setOption"].as<vector<string> >();
+	for(int i=0;i<options.size();i++)
+	{
+	    vector<string> tokens;
+	    StringUtilities::stringSplit(options[i], "=", tokens);
+	    if (tokens.size() != 2)
+		throw RbException("Option '"+options[i]+"' must have the form key=value");
+	    else
+		RbSettings::userSettings().setOption( tokens[0], tokens[1], false );
+	}
+    }
+
+    /*default to interactive mode*/
+    bool batch_mode = args.count("batch");
+    // FIXME -- the batch_mode variable appears to have no effect if true.
+
+    /* seek out files from command line */
     vector<string> sourceFiles;
-//    if (args.count("file"))
-//    sourceFiles = args["file"].as<vector<string> >();
+    if (args.count("file"))
+	sourceFiles = args["file"].as<vector<string> >();
 
     /* initialize environment */
     RevLanguageMain rl = RevLanguageMain(batch_mode);
