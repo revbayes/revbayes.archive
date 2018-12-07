e713fd4863ae03e4308070d75804e8b0770abbeb
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2018-12-07 16:22:33 +0100

    some work on the coalescent

--- src/core/distributions/phylogenetics/tree/coalescent/PiecewiseConstantCoalescent.cpp
+++ src/core/distributions/phylogenetics/tree/coalescent/PiecewiseConstantCoalescent.cpp
@@ -5,6 +5,7 @@
 #include "RandomNumberGenerator.h"
 #include "RbConstants.h"
 #include "RbMathCombinatorialFunctions.h"
+#include "StochasticNode.h"
 #include "TopologyNode.h"
 
 #include <algorithm>
@@ -12,18 +13,21 @@
 
 using namespace RevBayesCore;
 
-PiecewiseConstantCoalescent::PiecewiseConstantCoalescent(const TypedDagNode<RbVector<double> > *N, const TypedDagNode<RbVector<double> > *i, const std::vector<Taxon> &tn, const std::vector<Clade> &c) :
+PiecewiseConstantCoalescent::PiecewiseConstantCoalescent(const TypedDagNode<RbVector<double> > *N, const TypedDagNode<RbVector<double> > *i, METHOD_TYPES meth, const std::vector<Taxon> &tn, const std::vector<Clade> &c) :
     AbstractCoalescent( tn, c ),
     Nes( N ),
-    intervalStarts( i )
+    interval_starts_var( i ),
+    interval_method( meth )
 {
     // add the parameters to our set (in the base class)
     // in that way other class can easily access the set of our parameters
     // this will also ensure that the parameters are not getting deleted before we do
     addParameter( Nes );
-    addParameter( intervalStarts );
+    addParameter( interval_starts_var );
     
     simulateTree();
+    
+    updateIntervals();
 }
 
 
@@ -71,7 +75,6 @@ double PiecewiseConstantCoalescent::computeLnProbabilityTimes( void ) const
     std::sort(ages.begin(), ages.end());
     
     const RbVector<double> &popSizes  = Nes->getValue();
-    const RbVector<double> &intervals = intervalStarts->getValue();
     size_t currentInterval = 0;
     
     for (size_t i = 0; i < ages.size(); ++i)
@@ -93,15 +96,15 @@ double PiecewiseConstantCoalescent::computeLnProbabilityTimes( void ) const
             theta = popSizes[currentInterval];
             double age = ages[i];
             double max = age;
-            if ( currentInterval < intervals.size() )
+            if ( currentInterval < interval_starts.size() )
             {
-                max = (age > intervals[currentInterval]) ? intervals[currentInterval] : age;
+                max = (age > interval_starts[currentInterval]) ? interval_starts[currentInterval] : age;
             }
 //            double min = (prevCoalescentTime > intervals[currentInterval]) ? intervals[currentInterval] : prevCoalescentTime;
             
             deltaAge = max - prevCoalescentTime;
 //            deltaAge = age - min;
-            valid = currentInterval >= intervals.size() || age < intervals[currentInterval];
+            valid = currentInterval >= interval_starts.size() || age < interval_starts[currentInterval];
             if ( !valid )
             {
                 ++currentInterval;
@@ -119,6 +122,52 @@ double PiecewiseConstantCoalescent::computeLnProbabilityTimes( void ) const
     
 }
 
+
+void PiecewiseConstantCoalescent::executeMethod(const std::string &n, const std::vector<const DagNode *> &args, RbVector<double> &rv) const
+{
+    
+    if ( n == "getIntervalAges" )
+    {
+        
+        rv = interval_starts;
+        
+    }
+//    else if ( n == "averageExtinctionRate" )
+//    {
+//
+//
+//    }
+    else
+    {
+        throw RbException("The piecewise-constant coalescent process does not have a member method called '" + n + "'.");
+    }
+    
+}
+
+
+/**
+ * Keep the current value and reset some internal flags. Nothing to do here.
+ */
+void PiecewiseConstantCoalescent::keepSpecialization(DagNode *affecter)
+{
+    
+    // nothing to do here
+    
+}
+
+/**
+ * Restore the current value and reset some internal flags.
+ * If the root age variable has been restored, then we need to change the root age of the tree too.
+ */
+void PiecewiseConstantCoalescent::restoreSpecialization(DagNode *affecter)
+{
+    
+    // just re-update the start times of the intervals
+    updateIntervals();
+    
+}
+
+
 /**
  * Simulate new coalescent times.
  *
@@ -128,50 +177,77 @@ double PiecewiseConstantCoalescent::computeLnProbabilityTimes( void ) const
  */
 std::vector<double> PiecewiseConstantCoalescent::simulateCoalescentAges( size_t n ) const
 {
+    
+    // first check if we want to set the interval ages here!
+    if ( interval_method == SPECIFIED )
+    {
+        if ( interval_starts_var != NULL )
+        {
+            interval_starts = interval_starts_var->getValue();
+        }
+        else
+        {
+            throw RbException("You have to provide the start times of the coalescent skyline intervals if you chose 'SPECIFIED' as the method of choece.");
+        }
+    }
+    else
+    {
+        interval_starts = RbVector<double>(Nes->getValue().size()-1, RbConstants::Double::inf);
+    }
+    size_t num_events_per_interval = size_t( ceil( double(n)/Nes->getValue().size()) );
+    size_t current_num_events_in_interval = 0;
+    
     // Get the rng
     RandomNumberGenerator* rng = GLOBAL_RNG;
     
     // now simulate the ages
     
     // allocate the vector for the times
-    std::vector<double> coalescentTimes = std::vector<double>(n,0.0);
+    std::vector<double> coalescent_times = std::vector<double>(n,0.0);
     
     
     const RbVector<double> &popSizes  = Nes->getValue();
-    const RbVector<double> &intervals = intervalStarts->getValue();
-    size_t currentInterval = 0;
+    size_t current_interval = 0;
     // draw a time for each speciation event condition on the time of the process
     for (size_t i = 0; i < n; ++i)
     {
         double prevCoalescentTime = 0.0;
         if ( i > 0 )
         {
-            prevCoalescentTime = coalescentTimes[i-1];
+            prevCoalescentTime = coalescent_times[i-1];
         }
         
         size_t j = num_taxa - i;
         double nPairs = j * (j-1) / 2.0;
         
-        double simAge = 0.0;
+        double sim_age = 0.0;
         bool valid = false;
         do
         {
-            double theta = 1.0 / (2.0*popSizes[currentInterval]);
+            double theta = 1.0 / (2.0*popSizes[current_interval]);
             double lambda = nPairs * theta / 2.0;
             double u = RbStatistics::Exponential::rv( lambda, *rng);
-            simAge = prevCoalescentTime + u;
-            valid = currentInterval >= intervals.size() || simAge < intervals[currentInterval];
-            if ( !valid )
+            sim_age = prevCoalescentTime + u;
+            valid = current_interval >= interval_starts.size() || sim_age < interval_starts[current_interval];
+            if ( valid == false )
             {
-                ++currentInterval;
+                ++current_interval;
             }
             
-        } while ( !valid );
+        } while ( valid == false );
         
-        coalescentTimes[i] = simAge;
+        coalescent_times[i] = sim_age;
+        
+        ++current_num_events_in_interval;
+        if ( interval_method == EVENTS && current_num_events_in_interval == num_events_per_interval && current_interval < interval_starts.size() )
+        {
+            interval_starts[current_interval] = sim_age;
+            current_num_events_in_interval = 0;
+        }
+        
     }
     
-    return coalescentTimes;
+    return coalescent_times;
 }
 
 
@@ -188,8 +264,123 @@ void PiecewiseConstantCoalescent::swapParameterInternal(const DagNode *oldP, con
     {
         Nes = static_cast<const TypedDagNode<RbVector<double> >* >( newP );
     }
-    else if (oldP == intervalStarts)
+    else if (oldP == interval_starts_var)
     {
-        intervalStarts = static_cast<const TypedDagNode<RbVector<double> >* >( newP );
+        interval_starts_var = static_cast<const TypedDagNode<RbVector<double> >* >( newP );
     }
+}
+
+
+
+/**
+ * Touch the current value and reset some internal flags.
+ * If the root age variable has been restored, then we need to change the root age of the tree too.
+ */
+void PiecewiseConstantCoalescent::touchSpecialization(DagNode *affecter, bool touchAll)
+{
+    
+    // just update the start times of the intervals
+    updateIntervals();
+    
+}
+
+
+
+/**
+ * Touch the current value and reset some internal flags.
+ * If the root age variable has been restored, then we need to change the root age of the tree too.
+ */
+void PiecewiseConstantCoalescent::updateIntervals( void )
+{
+    
+    // first check if we want to set the interval ages here!
+    if ( interval_method == SPECIFIED )
+    {
+        if ( interval_starts_var != NULL )
+        {
+            interval_starts = interval_starts_var->getValue();
+        }
+        else
+        {
+            throw RbException("You have to provide the start times of the coalescent skyline intervals if you chose 'SPECIFIED' as the method of choece.");
+        }
+        
+        // clean all the sets
+        interval_starts.clear();
+        pop_sizes.clear();
+        
+//        double present_time = process_age->getValue();
+        
+        std::set<double> event_times;
+        
+        const std::vector<double>& times = interval_starts_var->getValue();
+        for (std::vector<double>::const_iterator it = times.begin(); it != times.end(); ++it)
+        {
+            event_times.insert( *it );
+        }
+        
+        const std::vector<double> &p = Nes->getValue();
+        
+        size_t index = p.size()-1;
+        
+        pop_sizes.push_back( p[index] );
+        
+        size_t pos = 0;
+        for (std::set<double>::iterator it = event_times.begin(); it != event_times.end(); ++it)
+        {
+            double t = *it;
+            
+            // add the time to our vector
+            interval_starts.push_back( t );
+            
+            // add the population size at the event t
+            pos = size_t( find(times.begin(), times.end(), t) - times.begin() );
+            if ( pos != times.size() )
+            {
+                index = pos;
+                pop_sizes.push_back( p[index] );
+            }
+            else
+            {
+                pop_sizes.push_back( p[index] );
+            }
+            
+        }
+        
+        
+    }
+    else if ( interval_method == EVENTS )
+    {
+        interval_starts = RbVector<double>(Nes->getValue().size()-1, RbConstants::Double::inf);
+        
+        if ( this->value != NULL )
+        {            
+            // retrieved the speciation times
+            std::vector<double> ages;
+            for (size_t i = 0; i < value->getNumberOfInteriorNodes()+1; ++i)
+            {
+                const TopologyNode& n = value->getInteriorNode( i );
+                double a = n.getAge();
+                ages.push_back(a);
+            }
+            // sort the vector of times in ascending order
+            std::sort(ages.begin(), ages.end());
+            
+            size_t num_events_per_interval = size_t( ceil( double(num_taxa-1.0)/Nes->getValue().size()) );
+            size_t current_interval = 0;
+            size_t current_num_events_in_interval = 0;
+            for (size_t i = 0; i < num_taxa-1; ++i)
+            {
+                ++current_num_events_in_interval;
+                if ( current_num_events_in_interval == num_events_per_interval )
+                {
+                    interval_starts[current_interval] = ages[i];
+                    current_num_events_in_interval = 0;
+                    ++current_interval;
+                }
+            }
+        }
+
+    }
+    
 }
