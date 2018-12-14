c97ba765ed4c50920d77d6484abc72994993035d
Michael Landis <mlandis@gmail.com>
2018-12-14 16:15:58 -0500

    Revert "Adding reinitialization counts"
    This reverts commit 44a0e51cb743275678a27b945724c317fc0b27b1.

--- src/core/dag/DagNode.h
+++ src/core/dag/DagNode.h
@@ -12,18 +12,18 @@
 #include <vector>
 
 namespace RevBayesCore {
-
+    
     class Distribution;
     class Monitor;
     class Move;
     class AbstractTrace;
 
     class DagNode : public Parallelizable, public MemberObject<double> {
-
+    
     public:
-
+        
         enum DagNodeTypes { CONSTANT, DETERMINISTIC, STOCHASTIC };
-
+    
         virtual                                                    ~DagNode(void);                                                                                      //!< Virtual destructor
 
         // pure virtual methods
@@ -43,7 +43,7 @@ namespace RevBayesCore {
         virtual void                                                setValueFromFile(const std::string &dir) = 0;                                               //!< Set value from string.
         virtual void                                                setValueFromString(const std::string &v) = 0;                                               //!< Set value from string.
         virtual void                                                writeToFile(const std::string &dir) const = 0;                                              //!< Write the value of this node to a file within the given directory.
-
+        
         // public member functions
         void                                                        addChild(DagNode *child) const;                                                             //!< Add a new child node
         void                                                        addMonitor(Monitor *m);                                                                     //!< Add a new monitor on this node
@@ -97,20 +97,20 @@ namespace RevBayesCore {
     protected:
                                                                     DagNode(const std::string &n);                                                              //!< Constructor
                                                                     DagNode(const DagNode &n);                                                                  //!< Copy Constructor
-
+    
         DagNode&                                                    operator=(const DagNode &d);                                                                //!< Overloaded assignment operator
-
+        
         virtual void                                                getAffected(RbOrderedSet<DagNode *>& affected, DagNode* affecter) = 0;                      //!< get affected nodes
         virtual void                                                keepMe(DagNode* affecter) = 0;                                                              //!< Keep value of myself
         virtual void                                                restoreMe(DagNode *restorer) = 0;                                                           //!< Restore value of this nodes
         virtual void                                                touchMe(DagNode *toucher, bool touchAll) = 0;                                                              //!< Touch myself (flag for recalculation)
-
+    
         // helper functions
         void                                                        getPrintableChildren(std::vector<DagNode*> &c) const;
         void                                                        getPrintableParents(std::vector<const DagNode*> &p) const;
         void                                                        printChildren(std::ostream& o, size_t indent, size_t lineLen, bool verbose=false) const;    //!< Print children DAG nodes
         void                                                        printParents(std::ostream& o, size_t indent, size_t lineLen, bool verbose=false) const;     //!< Print children DAG nodes
-
+        
         // members
         mutable std::vector<DagNode*>                               children;                                                                                   //!< The children in the model graph of this node
         bool                                                        elementVar;
@@ -125,14 +125,14 @@ namespace RevBayesCore {
         int                                                         visit_affected_count;
         int                                                         visit_keep_count;
         int                                                         visit_restore_count;
-        int                                                         visit_reinit_count;
         int                                                         visit_touch_count;
-
+    
     private:
-
+        
         mutable size_t                                              ref_count;
     };
 
 }
 
 #endif
+
