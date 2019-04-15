b67e2e5ba37ec3b3192d25d0418c5fe42b7d18ea
Dominik Schrempf <dominik.schrempf@gmail.com>
2019-04-15 17:06:50 +0200

    whitespace

--- src/revlanguage/distributions/phylogenetics/tree/Dist_DuplicationLoss.h
+++ src/revlanguage/distributions/phylogenetics/tree/Dist_DuplicationLoss.h
@@ -33,12 +33,10 @@ namespace RevLanguage {
         const TypeSpec&                                     getTypeSpec(void) const;                                                                //!< Get the type spec of the instance
         const MemberRules&                                  getParameterRules(void) const;                                                          //!< Get member rules (const)
 
-
         // Distribution functions you have to override
         RevBayesCore::DuplicationLossProcess*               createDistribution(void) const;
 
     protected:
-
         std::vector<std::string>                            getHelpAuthor(void) const;                                                              //!< Get the author(s) of this function
         std::string                                         getHelpDescription(void) const;                                                         //!< Get the description for this function
         std::string                                         getHelpDetails(void) const;                                                             //!< Get the more detailed description of the function
@@ -47,19 +45,15 @@ namespace RevLanguage {
         std::vector<std::string>                            getHelpSeeAlso(void) const;                                                             //!< Get suggested other functions
         std::string                                         getHelpTitle(void) const;                                                               //!< Get the title of this help entry
 
-
         void                                                setConstParameter(const std::string& name, const RevPtr<const RevVariable> &var);       //!< Set member variable
 
-
     private:
-
         RevPtr<const RevVariable>                           lambda;                                                                                 //!< The population size
         RevPtr<const RevVariable>                           mu;                                                                                     //!< The population size
         RevPtr<const RevVariable>                           origin;                                                                                 //!< The population size
         RevPtr<const RevVariable>                           individual_tree;                                                                        //!< The species tree
         RevPtr<const RevVariable>                           taxa;                                                                                   //!< The taxons
         RevPtr<const RevVariable>                           condition;                                                                              //!< The taxons
-
 
     };
 
