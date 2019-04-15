16aaaf16b7b6c0578c987bdc0313bd42bdd96034
Dominik Schrempf <dominik.schrempf@gmail.com>
2019-04-15 14:46:24 +0200

    cosmetic changes

--- src/revlanguage/distributions/phylogenetics/tree/Dist_DuplicationLoss.h
+++ src/revlanguage/distributions/phylogenetics/tree/Dist_DuplicationLoss.h
@@ -6,7 +6,7 @@
 #include "RlTypedDistribution.h"
 
 namespace RevLanguage {
-    
+
     /**
      * The RevLanguage wrapper of the Duplication-Loss process
      *
@@ -21,10 +21,10 @@ namespace RevLanguage {
      *
      */
     class Dist_DuplicationLoss : public  TypedDistribution<TimeTree> {
-        
+
     public:
         Dist_DuplicationLoss( void );
-        
+
         // Basic utility functions
         Dist_DuplicationLoss*                               clone(void) const;                                                                      //!< Clone the object
         static const std::string&                           getClassType(void);                                                                     //!< Get Rev type
@@ -32,13 +32,13 @@ namespace RevLanguage {
         std::string                                         getDistributionFunctionName(void) const;                                                //!< Get the Rev-name for this distribution.
         const TypeSpec&                                     getTypeSpec(void) const;                                                                //!< Get the type spec of the instance
         const MemberRules&                                  getParameterRules(void) const;                                                          //!< Get member rules (const)
-        
-        
+
+
         // Distribution functions you have to override
         RevBayesCore::DuplicationLossProcess*               createDistribution(void) const;
-        
+
     protected:
-        
+
         std::vector<std::string>                            getHelpAuthor(void) const;                                                              //!< Get the author(s) of this function
         std::string                                         getHelpDescription(void) const;                                                         //!< Get the description for this function
         std::string                                         getHelpDetails(void) const;                                                             //!< Get the more detailed description of the function
@@ -46,23 +46,23 @@ namespace RevLanguage {
         std::vector<RevBayesCore::RbHelpReference>          getHelpReferences(void) const;                                                          //!< Get some references/citations for this function
         std::vector<std::string>                            getHelpSeeAlso(void) const;                                                             //!< Get suggested other functions
         std::string                                         getHelpTitle(void) const;                                                               //!< Get the title of this help entry
-        
-        
-        void                                                setConstParameter(const std::string& name, const RevPtr<const RevVariable> &var);       //!< Set member variable
-        
-        
-    private:
-        
-        RevPtr<const RevVariable>                           lambda;                                                                                     //!< The population size
-        RevPtr<const RevVariable>                           mu;                                                                                     //!< The population size
-        RevPtr<const RevVariable>                           origin;                                                                                     //!< The population size
-        RevPtr<const RevVariable>                           individual_tree;                                                                            //!< The species tree
-        RevPtr<const RevVariable>                           taxa;                                                                                   //!< The taxons
-        RevPtr<const RevVariable>                           condition;                                                                                   //!< The taxons
 
-        
+
+        void                                                setConstParameter(const std::string& name, const RevPtr<const RevVariable> &var);       //!< Set member variable
+
+
+    private:
+
+        RevPtr<const RevVariable>                           lambda;                                                                                 //!< The population size
+        RevPtr<const RevVariable>                           mu;                                                                                     //!< The population size
+        RevPtr<const RevVariable>                           origin;                                                                                 //!< The population size
+        RevPtr<const RevVariable>                           individual_tree;                                                                        //!< The species tree
+        RevPtr<const RevVariable>                           taxa;                                                                                   //!< The taxons
+        RevPtr<const RevVariable>                           condition;                                                                              //!< The taxons
+
+
     };
-    
+
 }
 
 #endif
