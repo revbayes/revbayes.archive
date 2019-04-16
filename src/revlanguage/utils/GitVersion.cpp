6c3b0b29c093cc3f019beb879cb4f3e77ec8fe0c
Dominik Schrempf <dominik.schrempf@gmail.com>
2019-04-16 16:34:00 +0200

    gitversion ...

--- src/revlanguage/utils/GitVersion.cpp
+++ src/revlanguage/utils/GitVersion.cpp
@@ -1,4 +1,4 @@
 #include "GitVersion.h"
-const char *build_git_sha = "abb983";
-const char *build_date = "Tue Apr 16 12:09:06 CEST 2019";
+const char *build_git_sha = "f5649b";
+const char *build_date = "Tue Apr 16 14:58:25 CEST 2019";
 const char *build_git_branch = "duploss";
