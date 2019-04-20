c41d83373188a2a7ae34313eb850733de3b96227
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2019-04-20 12:29:41 +0200

    Fixing a problem with initializing the prob terms in the BDST process

--- src/core/distributions/phylogenetics/tree/birthdeath/EpisodicBirthDeathSamplingTreatmentProcess.cpp
+++ src/core/distributions/phylogenetics/tree/birthdeath/EpisodicBirthDeathSamplingTreatmentProcess.cpp
@@ -895,7 +895,8 @@ void EpisodicBirthDeathSamplingTreatmentProcess::prepareProbComputation( void ) 
     B_i[0] /= A_i[0];
 
     // E_{i-1}(0) = 1, and our E(i,t) function requires i >= 0, so we hard-code this explicitly
-    E_i_t_i[0] = 1.0;
+    // Sebastian: This should be the probability of going extinct, which is in this case the probability of non-sampling.
+    E_i_t_i[0] = (1 - phi_event[0]);
 
     for (size_t i=1; i<timeline.size(); ++i)
     {
