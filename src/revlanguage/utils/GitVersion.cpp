a520bb6ed30db7df59bfcc095fc3c002b76e0fb3
Michael Landis <mlandis@gmail.com>
2019-05-24 10:43:20 -0400

    set regenerate.sh as +x

--- src/revlanguage/utils/GitVersion.cpp
+++ src/revlanguage/utils/GitVersion.cpp
@@ -1,4 +1,4 @@
 #include "GitVersion.h"
-const char *build_git_sha = "4c49a1";
-const char *build_date = "Tue May 14 12:44:54 EDT 2019";
+const char *build_git_sha = "438536";
+const char *build_date = "Fri May 24 10:42:50 EDT 2019";
 const char *build_git_branch = "development";
