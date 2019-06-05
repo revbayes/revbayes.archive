134b04744fc914bde47feea24505a59e260546ca
Allison Hsiang <allison.hsiang@gmail.com>
2019-06-05 16:04:55 +0200

    Check that origin time is older than age of gene trees

--- src/revlanguage/utils/GitVersion.cpp
+++ src/revlanguage/utils/GitVersion.cpp
@@ -1,4 +1,4 @@
 #include "GitVersion.h"
-const char *build_git_sha = "1f5864";
-const char *build_date = "Tue 07 May 2019 03:44:29 PM CEST";
+const char *build_git_sha = "71e51c";
+const char *build_date = "Wed Jun  5 16:02:06 CEST 2019";
 const char *build_git_branch = "duploss";
