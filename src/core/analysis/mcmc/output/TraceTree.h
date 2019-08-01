e5e36590eb323ee5667aa9e181e65380eea5afde
Andy Magee <afmagee@uw.edu>
2019-07-31 22:59:05 -0700

    Fixes to bugs in commits 14e2397 and 3e386f8

--- src/core/analysis/mcmc/output/TraceTree.h
+++ src/core/analysis/mcmc/output/TraceTree.h
@@ -40,7 +40,7 @@ namespace RevBayesCore {
         };
 
     public:
-        
+
         /*
          * This struct determines which annotations are reported in the summary tree
          */
@@ -56,6 +56,7 @@ namespace RevBayesCore {
             double node_ages_HPD;
             bool sampled_ancestor_probs;
             bool force_positive_branch_lengths;
+            bool use_outgroup;
             
             AnnotationReport();
         };
@@ -66,7 +67,7 @@ namespace RevBayesCore {
          */
         TraceTree( bool c = true );
         virtual ~TraceTree(){}
-        
+
         TraceTree*                                 clone(void) const;
         void                                       annotateTree(Tree &inputTree, AnnotationReport report, bool verbose );
         double                                     cladeProbability(const Clade &c, bool verbose);
